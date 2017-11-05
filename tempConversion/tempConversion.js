c14e4628ec6cd1d3ac032c34b4c53173b4da2f8e
Cody Loyd <codyloyd@gmail.com>
2017-11-04 23:03:44 -0500

    add solutions to first exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-var ftoc = function() {
-  
+var ftoc = function(f) {
+ return Math.round((f - 32) * (5/9) * 10) / 10
 }
 
-var ctof = function() {
-  
+var ctof = function(c) {
+  return Math.round(((c * 9/5) + 32) * 10) / 10 
 }
 
 module.exports = {
--- tempConversion/tempConversion.spec.js
+++ tempConversion/tempConversion.spec.js
@@ -4,22 +4,22 @@ describe('ftoc', function() {
   it('works', function() {
     expect(ftoc(32)).toEqual(0);
   });
-  xit('rounds to 1 decimal', function() {
+  it('rounds to 1 decimal', function() {
     expect(ftoc(100)).toEqual(37.8);
   });
-  xit('works with negatives', function() {
+  it('works with negatives', function() {
     expect(ftoc(-100)).toEqual(-73.3);
   });
 });
 
 describe('ctof', function() {
-  xit('works', function() {
+  it('works', function() {
     expect(ctof(0)).toEqual(32);
   });
-  xit('rounds to 1 decimal', function() {
+  it('rounds to 1 decimal', function() {
     expect(ctof(73.2)).toEqual(163.8);
   });
-  xit('works with negatives', function() {
+  it('works with negatives', function() {
     expect(ctof(-10)).toEqual(14);
   });
 });
