35ed3d7b442c401ab02e3252842a17cc7bd0ddc1
Cody Loyd <codyloyd@gmail.com>
2017-12-15 13:43:38 -0600

    sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-var sumAll = function() {
+const sumAll = function(min, max) {
+  if (min < 0 || max < 0) return "ERROR";
+  if (typeof min !== "number" || typeof max !== "number") return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
+    sum += i;
+  }
+  return sum;
+};
 
-}
-
-module.exports = sumAll
+module.exports = sumAll;
