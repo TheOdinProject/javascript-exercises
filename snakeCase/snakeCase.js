02d2ca43f41ead591c77c928de889e6b0db9b84d
Cody Loyd <codyloyd@gmail.com>
2017-12-15 13:26:05 -0600

    solutions

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,19 @@
-var snakeCase = function() {
+const snakeCase = function(string) {
+  // wtf case
+  string = string.replace(/\.\./g, " ");
 
-}
+  // this splits up camelcase IF there are no spaces in the word
+  if (string.indexOf(" ") < 0) {
+    string = string.replace(/([A-Z])/g, " $1");
+  }
 
-module.exports = snakeCase
+  return string
+    .trim()
+    .toLowerCase()
+    .replace(/[,\?\.]/g, "")
+    .replace(/\-/g, " ")
+    .split(" ")
+    .join("_");
+};
+
+module.exports = snakeCase;
