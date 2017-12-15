d42eebaab872f276ce13cdc50add0ab7a4166aaf
Cody Loyd <codyloyd@gmail.com>
2017-12-15 11:01:24 -0600

    solutions

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-var fibonacci = function() {
+const fibonacci = function(count) {
+  if (count < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
