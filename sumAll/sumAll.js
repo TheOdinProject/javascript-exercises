afeaa66f8140ceba1924ffa056848384b8c43e0f
Kira <kiramas@gmail.com>
2018-08-22 00:31:01 +0000

    solutions to multiple exercises

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  const argsToArray = Array.from(arguments);
+  argsToArray.sort(function(a, b){return a-b});
+  let firstNum = argsToArray[0];
+  let secondNum = argsToArray[1];
+  let sum = 0;
 
+  var isNegative = (element) => element < 0 
+  var isNotNumber = (element) => typeof element != 'number'
+
+  if (argsToArray.some(isNegative)) {
+    return "ERROR";
+  } else if (argsToArray.some(isNotNumber)) {
+    return "ERROR";
+  } else {
+    while (firstNum <= secondNum) {
+      sum += firstNum++;
+    }
+  return sum;
+  }
 }
 
 module.exports = sumAll
