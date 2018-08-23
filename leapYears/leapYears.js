1b8d812b784cd9014766943feee1961e15e288d4
Kira <kiramas@gmail.com>
2018-08-23 16:13:25 +0000

    Functioning code for leapYear exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(date) {
+  if (date % 4 === 0) {
+    if (date % 100 === 0 && date % 400 !== 0) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = leapYears
