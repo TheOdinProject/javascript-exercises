3bbe72b2c021f7b33776afd210cd058daa19d5f9
Kira <kiramas@gmail.com>
2018-08-27 18:35:29 +0000

    Wrote solution to tempConversion exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,13 +1,5 @@
-const leapYears = function(date) {
-  if (date % 4 === 0) {
-    if (date % 100 === 0 && date % 400 !== 0) {
-      return false;
-    } else {
-      return true;
-    }
-  } else {
-    return false;
-  }
+const  leapYears = function(year) {
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 == 0)
 }
 
 module.exports = leapYears
