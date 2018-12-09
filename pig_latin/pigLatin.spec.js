4a70d985b43f41b92d5c741642125b219e2ff392
Leila Alderman <42787645+leila-alderman@users.noreply.github.com>
2018-12-08 19:38:09 -0500

    Change all but the first spec to pending

--- pig_latin/pigLatin.spec.js
+++ pig_latin/pigLatin.spec.js
@@ -23,41 +23,41 @@ describe('#translate', function() {
 		expect(s).toEqual('appleay');
 	});
 
-	it('translates a word beginning with a consonant', function() {
+	xit('translates a word beginning with a consonant', function() {
 		s = pigLatin.translate("banana");
 		expect(s).toEqual("ananabay");
 	});
 
-	it('translates a word beginning with two consonants', function() {
+	xit('translates a word beginning with two consonants', function() {
 		s = pigLatin.translate("cherry");
 		expect(s).toEqual('errychay');
 	});
 
-	it('translates two words', function() {
+	xit('translates two words', function() {
 		s = pigLatin.translate("eat pie");
 		expect(s).toEqual('eatay iepay');
 	});
 
-	it('translates a word beginning with three consonants', function() {
+	xit('translates a word beginning with three consonants', function() {
 		expect(pigLatin.translate("three")).toEqual("eethray");
 	});
 
-	it('counts "sch" as a single phoneme', function() {
+	xit('counts "sch" as a single phoneme', function() {
 		s = pigLatin.translate("school");
 		expect(s).toEqual("oolschay");
 	});
 
-	it('counts "qu" as a single phoneme', function() {
+	xit('counts "qu" as a single phoneme', function() {
 		s = pigLatin.translate("quiet");
 		expect(s).toEqual("ietquay");
 	});
 
-	it('counts "qu" as a consonant even when its preceded by a consonant', function() {
+	xit('counts "qu" as a consonant even when its preceded by a consonant', function() {
 		s = pigLatin.translate("square");
 		expect(s).toEqual("aresquay");
 	});
 
-	it('translates many words', function() {
+	xit('translates many words', function() {
 		s = pigLatin.translate("the quick brown fox");
 		expect(s).toEqual("ethay ickquay ownbray oxfay");
 	});
