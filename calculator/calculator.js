200e4c157b8a6ed1fca34724c7abb2f1ece9b848
jaspreet-singh-sahota <55361440+jaspreet-singh-sahota@users.noreply.github.com>
2019-10-01 19:45:39 +0530

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,28 @@
-function add () {
-	
+function add(a, b) {
+  return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+  return a - b;
 }
 
-function sum () {
-	
+function sum(array) {
+  return array.reduce((current, total) => total + current, 0);
 }
 
-function multiply () {
-	
+function multiply(array) {
+    return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
 }
 
-function power() {
-	
+function power(a, b) {
+  return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(n) {
+  if (n === 0) {return 1};
+  return n * factorial(n -1)	
 }
 
 module.exports = {
