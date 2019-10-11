0d7c05485bf8618bee76066e7a79cc3209cc1519
Bradley Peterson <bradlypetrsn1991@gmail.com>
2019-10-11 11:46:17 -0500

    Edited fibonacci.js
    Added another possible solution that doesn't use a loop.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -10,4 +10,15 @@ const fibonacci = function(count) {
   return b;
 };
 
+/* Another possible solution
+
+  const fibonacci = function(num) {
+  const goldenRatio = (1 + Math.sqrt(5)) / 2;
+  if(num < 0) return "OOPS";
+  return Math.floor((goldenRatio**num) / Math.sqrt(5) + 0.5);
+}
+*/
+module.exports = fibonacci
+
+
 module.exports = fibonacci;
