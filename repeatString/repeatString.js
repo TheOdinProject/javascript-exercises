b0400f7f33babad4b8cacd06762fdc45ad807d0e
Carlos Leon, Jr <tradercljr@gmail.com>
2019-10-13 17:47:40 -0700

    modrepeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(stringOne, n) {
+	let output = '';
+	if ( n < 0 ) {
+		return ('ERROR');
+		}
+  
+	for(let i=0; i < n; i++) {
+		output += stringOne;
+		
+	}
+	return(output);
 }
+repeatString('hey',3);
+repeatString('hey',10);
+repeatString('hey',1);
+repeatString('hey',0);
+repeatString('hey',-1);
 
-module.exports = repeatString
+module.exports = repeatString;
+
