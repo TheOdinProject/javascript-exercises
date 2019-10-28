056e386b165fd86f3c5b2b5ff5e3645a6f94c73c
billalp <billal.patel@rightmove.co.uk>
2019-10-28 09:24:12 +0000

    First test working

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,24 @@
-const caesar = function() {
+const caesar = function(value, addBy) {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+	const newVal = value.toString().toLowerCase();
 
+	if (alphabet.includes(newVal) === true) {
+		var letterIndex = alphabet.indexOf(newVal);
+		var finalIndex = letterIndex + addBy;
+
+		return alphabet[finalIndex].toUpperCase();
+	} else {
+		return 'Value is not a part of the alphabet';
+	}
 }
 
-module.exports = caesar
+module.exports = caesar;
+
+/* Pseudocode
+
+
+
+
+
+
+*/ 
