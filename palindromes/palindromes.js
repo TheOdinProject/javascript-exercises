c7d61f82e986688b557768ffb50f6ccdb458ce3f
billalp <billal.patel@rightmove.co.uk>
2019-11-01 12:33:00 +0000

    Working function

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function(word) {
+	const wordArr = word.split('');
+	const reversedArray = wordArr.reverse();
 
+	newArray = [];
+
+	for (var i = 0; i < wordArr.length; i++) {
+		if (wordArr[i] === reversedArray[i]) {
+			newArray.push(wordArr[i])
+		} else {
+			newArray = ['Not a palindrome'];
+		}
+	}
+	return true ? newArray.join('') === word : false;
 }
 
 module.exports = palindromes
