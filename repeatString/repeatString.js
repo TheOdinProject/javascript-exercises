3d5549c76522de89d7328497e5f611f354b3c0b3
billalp <billal.patel@rightmove.co.uk>
2019-11-04 21:54:47 +0000

    Firt part is working

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
 const repeatString = function(word, times) {
+	let arr = word.split(' ');
 	
+	for (let i = 1; i < times; i++) {
+		arr.push(word);
+	}
+
+	return arr.join('');
 }
 
 module.exports = repeatString
