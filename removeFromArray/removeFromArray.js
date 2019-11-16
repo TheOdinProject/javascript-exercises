941e2e8dc185335b9327f9746f72374289abd250
billalp <billal.patel@rightmove.co.uk>
2019-11-16 22:49:50 +0000

    Made a start

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+	const filteredArray = [];
+	// const array = args;
+	const array = [3];
 
+	for (let i = 0; i < array.length; i++) {
+		if (arr.includes(array[i])) {
+			// filteredArray.push(5);
+		} else if (arr.includes(array[0])) {			
+			// continue;
+		}
+	}
+
+	return filteredArray;
 }
 
 module.exports = removeFromArray
