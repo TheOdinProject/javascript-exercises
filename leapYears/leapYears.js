902914250e555a55d0ca2d0541508b1ec41c073e
Carlos Leon, Jr <tradercljr@gmail.com>
2019-11-17 17:31:58 -0800

    leapYears.js

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+return ( (year % 4 == 0) && (year % 100 !== 0) || ( year % 400 == 0) ); 
+	
+ 
+   
+  
+
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 }
-
 module.exports = leapYears
