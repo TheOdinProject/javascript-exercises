57425a290f3aacff20ccbb57fe216d141b9943d0
Carlos Leon, Jr <tradercljr@gmail.com>
2019-11-19 11:52:57 -0800

    reverseString.js

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(reverseString) {
 
+var phrase = reverseString.split("");
+var backward = phrase.reverse();
+var taskComplete = backward.join("");
+
+return taskComplete;
 }
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc!');
 
 module.exports = reverseString
