daeb889802c8077df91dc5f1256d69b232e44d94
Denzel <denzeltlee@gmail.com>
2019-12-31 13:45:35 +0800

    finished repeatString.js exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	let repeatedString = "";
+	if (num < 0) {
+		repeatedString = "ERROR";
+	} else {
+		for (let i = 0; i < num; i++) {
+			repeatedString += string;
+		}
+	}
+	return repeatedString;
 }
 
 module.exports = repeatString
--- repeatString/repeatString.spec.js
+++ repeatString/repeatString.spec.js
@@ -4,16 +4,16 @@ describe('repeatString', function() {
   it('repeats the string', function() {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  xit('repeats the string many times', function() {
+  it('repeats the string many times', function() {
     expect(repeatString('hey', 10)).toEqual('heyheyheyheyheyheyheyheyheyhey');
   });
-  xit('repeats the string 1 times', function() {
+  it('repeats the string 1 times', function() {
     expect(repeatString('hey', 1)).toEqual('hey');
   });
-  xit('repeats the string 0 times', function() {
+  it('repeats the string 0 times', function() {
     expect(repeatString('hey', 0)).toEqual('');
   });
-  xit('returns ERROR with negative numbers', function() {
+  it('returns ERROR with negative numbers', function() {
     expect(repeatString('hey', -1)).toEqual('ERROR');
   });
 });
