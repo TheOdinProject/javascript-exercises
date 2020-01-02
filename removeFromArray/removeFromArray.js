300930e15ae11139aebc8f542f7c4fffde06dcd2
Denzel <denzeltlee@gmail.com>
2020-01-02 15:07:10 +0800

    finished removeFromArray.js exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+	items.forEach(item => {
+		if (array.indexOf(item) >= 0) {
+			const index = array.indexOf(item);
+			array.splice(index, 1);
+		} else {
+			return
+		}
+	})
+	console.log(array);
+	return array;
 }
 
 module.exports = removeFromArray
