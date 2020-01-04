1a7921ed569ca481f9c33a7d042154b73b002dc6
Denzel <denzeltlee@gmail.com>
2020-01-04 11:40:03 +0800

    finished sumAll.js exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	if (Math.sign(num1) === 1 && Math.sign(num2) === 1) {
+		if (Number.isInteger(num1) && Number.isInteger(num2)) {
+			let total = 0;
+			for (let i = num1; i < num2 + 1; i++) {
+			total += i;
+			}
+			return total;
+		} else {
+			return "ERROR";
+		}
+	} else {
+		return "ERROR";
+	}
 }
 
 module.exports = sumAll
