467c5f7a1d053c9eaae188b6aa44f5241df906ee
Lei Huang <54651756+lhuang61@users.noreply.github.com>
2020-02-12 13:36:51 +1100

    Use function recursion for cleaner code.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,14 +1,13 @@
-const fibonacci = function(count) {
-  if (count < 0) return "OOPS";
-  if (count == 0) return 0;
-  let a = 0;
-  let b = 1;
-  for (let i = 1; i < count; i++) {
-    const temp = b;
-    b = a + b;
-    a = temp;
-  }
-  return b;
-};
+const fibonacci = function(index) {
+    const n = Number(index);
+    
+    if (n === 1 || n === 2) {
+        return 1;
+    } else if (n > 2) {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    } else if (n <= 0) {
+        return "OOPS";
+    }
+}
 
 module.exports = fibonacci;
