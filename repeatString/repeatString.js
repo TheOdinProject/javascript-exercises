54447a31dc563a472b5c790ab0524c2cfc1356ab
Andres Garcia <garciaagudelod@gmail.com>
2020-03-28 18:47:22 -0500

    Solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let repeat = '';
+    if(number < 0){
+        return("ERROR");
+    } else if(number == 0){
+        return repeat;
+    } else {
+        for (let index = 0; index < number; index++) {
+             repeat = repeat + string;        
+        }
+        return repeat;
+    }
 }
 
 module.exports = repeatString
