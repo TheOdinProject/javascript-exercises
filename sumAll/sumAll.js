3d513bfbc39ac54ddd0aa192df2f7bf664ff0540
Andres Garcia <garciaagudelod@gmail.com>
2020-03-28 18:50:05 -0500

    Solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let temporal;
+    let message = "ERROR";
+    let sum = 0;
+    if(min < 0 || max < 0){
+        return message;
+    }else if(typeof min !== "number" || typeof max !== "number"){
+        return message;
+    }if(max < min ){
+        temporal = max;
+        max = min;
+        min = temporal;
+    }
 
-}
+    for (let index = min; index < max + 1; index++) {
+        sum += index;
+    }
+    return sum;
+};
 
 module.exports = sumAll
