abb5900348a7559562234208bb4a647f1cc13912
Harprit Singh <Singhhs@users.noreply.github.com>
2020-04-26 15:16:00 +0200

    removed x

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatString = ""
+    for (let i = 1; i <= num; i++) {
+        repeatString += str 
+    } if ( num < 0){
+        repeatString = 'ERROR'; }
+return repeatString 
 }
 
 module.exports = repeatString
