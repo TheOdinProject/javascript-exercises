19f79acf724994570269e4b87605613f8a6de4d7
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-02 11:16:48 +0300

    passed reverses single word

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let stringLength = inputString.length;
+    let revString = ''
+
+    for (let i = stringLength; i >= 0; i--) {
+        if (revString === 'undefined') {
+            revString = ''
+        }
+        revString = revString + inputString[i];
+    }
+
+    return revString;
 
 }
 
