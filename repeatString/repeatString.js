f9b8d2d40345c26e57940244852b6920f505eff9
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-02 09:27:26 +0300

    Solved problem 1

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(sourceString, stringCount) {
+    //Declare variable/s
+    let resultString = '';
+
+
+    // Create a for loop to repeat the string
+    for (let i = 0; i < stringCount; i++) {
+        resultString = resultString + sourceString;
+    }
+
+    return resultString;
+
 
 }
 
