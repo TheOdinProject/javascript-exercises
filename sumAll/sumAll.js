5481761795385120f4b03d3de4be9c38f894d05c
Karem Quiroz <58605593+codekacode@users.noreply.github.com>
2020-07-01 19:43:35 -0500

    sumAll Solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(nmin,nmax) {
+  if(nmin < 0 || nmax < 0) return 'ERROR';
+  if(typeOf nmin!== "number" || typeOf nmax!== "number") return 'ERROR';
+  
+  if(nmin > nmax){
+    const tem = nmin;
+    nmin = nmax;
+    nmax = tem;    
+  }
+  let sum = 0;
+  for( let i = nmin ; i <= nmax ; i++){
+    sum+= i
+  }
+  return sum;
 
 }
 
