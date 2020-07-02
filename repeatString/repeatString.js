6cf3662f55aeedac59883be763b602d5a3a5cdc4
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-02 09:41:16 +0300

    passed returns ERROR with negative numbers

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -2,6 +2,10 @@ const repeatString = function(sourceString, stringCount) {
     //Declare variable/s
     let resultString = '';
 
+    //Return error if stringCounter is negative
+    if (stringCount < 0) {
+        return 'ERROR';
+    }
 
     // Create a for loop to repeat the string
     for (let i = 0; i < stringCount; i++) {
--- repeatString/repeatString.spec.js
+++ repeatString/repeatString.spec.js
@@ -13,7 +13,7 @@ describe('repeatString', function() {
     it('repeats the string 0 times', function() {
         expect(repeatString('hey', 0)).toEqual('');
     });
-    xit('returns ERROR with negative numbers', function() {
+    it('returns ERROR with negative numbers', function() {
         expect(repeatString('hey', -1)).toEqual('ERROR');
     });
     xit('repeats the string a random amount of times', function() {
