cd63cf311bc3507f273d545ea2163887efafcb5c
Karem Quiroz <58605593+codekacode@users.noreply.github.com>
2020-07-01 19:08:43 -0500

    RemoveFromArray Solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...parameters) {
+  const thisArray = parameters[0];
+  const newArray = [];
+  
+  thisArray.forEach((e) => {
+    if (!parameters.includes(e)){
+      newArray.push(e);
+    }
+  });
+  return newArray;
+};
 
+const removeFromArray = function(...arg){
+  const array = arg[0];
+  return array.filter(val => !arg.includes(val));
 }
 
 module.exports = removeFromArray
