3699f94da99551710869307ba99602a3441b85b9
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-15 18:06:28 +0300

    Created code to remove a single value

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+
+    let myArray = args[0];
+    let pos = 0;
+
+    for (let i = 1; i < args.length; i++) {
+        pos = myArray.indexOf(args[i]);
+        myArray.splice(pos, 1);
+    }
+
+    return myArray;
 
 }
 
