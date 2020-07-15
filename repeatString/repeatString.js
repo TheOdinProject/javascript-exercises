ffc0b766c85a0298bf90432bdbf74b0884ab9319
Salih <salihyildirimnorge82@gmail.com>
2020-07-16 00:38:14 +0200

    solved repeatString exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+    let addString = '';
+    if(times>=0){
+        for (let i = 0; i < times; i++) {
+            addString += str;
+        }
+    }
+    else{
+        addString = 'ERROR';
+    }
+    return addString;
 }
 
 module.exports = repeatString
