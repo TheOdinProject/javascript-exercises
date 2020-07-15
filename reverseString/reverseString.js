18d7b040d6b789c21100e8b3eaad90e8829af3f1
Salih <salihyildirimnorge82@gmail.com>
2020-07-16 00:50:36 +0200

    solved reverseString exercises

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rts='';
+    for (let index = str.length-1; index>=0; index--) {
+        rts=rts + str.substr(index,1);        
+    }
+    return rts;
 }
 
 module.exports = reverseString
