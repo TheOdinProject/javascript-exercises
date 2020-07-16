aa4447b15daa96d25f596435c4010a9bffd844a6
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-16 10:19:15 +0300

    works with blank strings

--- reverseString/reverseString.spec.js
+++ reverseString/reverseString.spec.js
@@ -12,7 +12,7 @@ describe('reverseString', function() {
     it('works with numbers and punctuation', function() {
         expect(reverseString('123! abc!')).toEqual('!cba !321')
     })
-    xit('works with blank strings', function() {
+    it('works with blank strings', function() {
         expect(reverseString('')).toEqual('')
     })
 });
