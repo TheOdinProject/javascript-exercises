2a41f84c1a5d496dab1b8af56e4f7ef4e56196b8
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-16 08:54:02 +0300

    Passed test 1

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+
+    let result = '';
+
+    for (let i = 0; i < number; i++) {
+        if (number < 0) {
+            return 'ERROR';
+        } else {
+            result += string;
+        }
+
+    }
+
+    return result;
 
 }
 
