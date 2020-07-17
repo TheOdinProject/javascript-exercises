607fcec7ca4a0ef7afd51d527e8923459a0ed1ad
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-17 10:51:35 +0300

    Passed works with larger number first

--- sumAll.spec.js
+++ sumAll.spec.js
@@ -7,7 +7,7 @@ describe('sumAll', function() {
     it('works with large numbers', function() {
         expect(sumAll(1, 4000)).toEqual(8002000);
     });
-    xit('works with larger number first', function() {
+    it('works with larger number first', function() {
         expect(sumAll(123, 1)).toEqual(7626);
     });
     xit('returns ERROR with negative numbers', function() {
