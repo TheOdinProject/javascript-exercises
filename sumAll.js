0871cf551434f420cc6bccd60a89493f565e9d7c
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-17 10:49:54 +0300

    Passed sums numbers within the range

--- sumAll.js
+++ sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(...args) {
 
+    let totalOfNumbers = 0;
+    let firstNumber = args[0];
+    let lastNumber = args[args.length - 1];
+
+    /* If either is not a number, return ERROR */
+    if (typeof firstNumber != "number" || typeof lastNumber != "number") {
+        return 'ERROR';
+    }
+
+    /* If first number is greater, switch numbers */
+    if (firstNumber > lastNumber) {
+        let tempNumber = firstNumber;
+        firstNumber = lastNumber;
+        lastNumber = tempNumber;
+    }
+
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        totalOfNumbers += i;
+    }
+
+    return totalOfNumbers;
 }
 
 module.exports = sumAll
