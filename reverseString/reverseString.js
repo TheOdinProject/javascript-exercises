e8cdd37d955fd1590ab3f0c7ef6c5eada53ead8a
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-18 14:25:18 +0300

    reverseString All done

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+
+    let revString = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        revString += string.charAt(i);
+    }
+
+    return revString;
 }
 
 module.exports = reverseString
