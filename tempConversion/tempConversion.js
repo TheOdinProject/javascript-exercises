da6a5737cb788d3566d894ea6d145ee57d47e195
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-18 17:58:27 +0300

    tempConversion all done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,24 @@
-const ftoc = function() {
+const ftoc = function(fValue) {
 
+    let result = 0.0;
+
+    //Convert Farenheit to Celsius
+    result = Math.round(((fValue - 32) * 5 / 9) * 10) / 10;
+
+    return result;
 }
 
-const ctof = function() {
+const ctof = function(cValue) {
+    let result = 0.0;
+
+    //Convert Celsius to Farenheit
+    result = Math.round(((cValue * 9 / 5) + 32) * 10) / 10;
+
+    return result;
 
 }
 
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 }
