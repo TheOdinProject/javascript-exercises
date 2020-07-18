7ab3796032f80f89ed750c756374ef9f3e81af85
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-18 14:17:50 +0300

    repeatString All done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+
+    let result = '';
+
+    if (number < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < number; i++) {
+        result += string;
+    }
+
+    return result;
 
 }
 
