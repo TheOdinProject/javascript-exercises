8bc9e3dc90255dddb5a56917410424da14496f07
Mohammed Nabeel <mohdnabeel13@gmail.com>
2020-07-18 17:08:58 +0300

    Leap Years done

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(yearToCheck) {
+
+    let isALeapYear = true;
+
+    if (yearToCheck % 4 === 0 && yearToCheck % 400 === 0) {
+        return true;
+    }
+
+    if (yearToCheck % 4 === 0 && yearToCheck % 100 != 0) {
+        return true;
+    }
+
+
+
+    return false;
 
 }
 
