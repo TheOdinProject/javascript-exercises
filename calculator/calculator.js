e75a7da978870053ce9b301a3be7550e93d2611c
Mayar-Hassanin98 <48419750+Mayar-Hassanin98@users.noreply.github.com>
2020-07-25 16:44:00 +0200

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,36 @@
-function add () {
+function add (a,b) {
+	return a+b;
+}
+
+function subtract (a,b) {
+	return a-b;
+}
+
+function sum (a) {
+	let sum=0
+	if(a){
+         sum+=a;
+	}
+	return sum;
+}
+
+function multiply (a,b) {
+	return a*b;
+}
+
+function power(a,b) {
+	return Math.pow(a,b)
 	
 }
 
-function subtract () {
-	
-}
-
-function sum () {
-	
-}
-
-function multiply () {
-	
-}
-
-function power() {
-	
-}
-
-function factorial() {
-	
+function factorial(a) {
+	if(a==o)return;
+	let fact=1;
+	for(let i=n ;i<0 ;i--)
+	{ 
+	  fact*=1;
+	}
+	return fact;
 }
 
 module.exports = {
