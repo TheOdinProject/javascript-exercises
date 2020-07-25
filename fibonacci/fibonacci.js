0bde29db9ecb2616276003414620ac0676505fb2
Mayar-Hassanin98 <48419750+Mayar-Hassanin98@users.noreply.github.com>
2020-07-25 17:25:31 +0200

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let a=0 ,b=1,fib=0;
+    for(let i=2;i<=n;i++){
+      fib=a+b;
+      a=b;
+      b=fib;
+    }
+   return fib;
 }
 
 module.exports = fibonacci
