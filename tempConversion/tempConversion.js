3b9c8b9515fe872a070cbc89db24583f555f7a2b
Nidhish1407 <nidhishp07@gmail.com>
2020-08-27 18:07:42 +0530

    Added solution for tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f-32)*(5/9);
+  let celcius = +c.toFixed(1);
+  return celcius;
 }
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * (9/5) + 32;
+  let fahrenheit = +f.toFixed(1);
+  return fahrenheit;
 }
 
 module.exports = {
