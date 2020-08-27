3faf02d030c6d63e81149ec3be36b81feb9cd3f8
Nidhish1407 <nidhishp07@gmail.com>
2020-08-27 16:43:18 +0530

    Added Solution for summAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b))
+        return 'ERROR';
 
+    if (a < 0 || b < 0)
+        return 'ERROR';
+
+    if (a > b) {
+        let temp = b;
+        b = a;
+        a = temp;
+    }
+    let ans = 0;
+    for (let i = a; i <= b; i++)
+        ans += i;
+
+    return ans;
 }
 
 module.exports = sumAll
