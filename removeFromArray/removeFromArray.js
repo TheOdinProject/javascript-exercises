30b8f44cd1b0efe54f4bf8c196f860d0accf769a
Nidhish1407 <nidhishp07@gmail.com>
2020-08-27 16:12:33 +0530

    Added solution for removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+   //[...args]
+    let newArr = [];
+    arr.forEach(element => {
+        if(!args.includes(element))
+        newArr.push(element);
+    });
+    // console.log(newArr);
+    return newArr;
 }
 
 module.exports = removeFromArray
