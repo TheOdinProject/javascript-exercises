798d62f0e2635b956bfb0e94e23bbdcc36775906
Bohdan Mukha <by.muha@gmail.com>
2020-10-04 02:39:22 +0300

    calculator done

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,35 @@
-function add () {
-	
+function add(value1, value2) {
+  return value1 + value2;
 }
 
-function subtract () {
-	
+function subtract(value1, value2) {
+  return value1 - value2;
 }
 
-function sum () {
-	
+function sum(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
 }
 
-function multiply () {
-	
+function multiply(arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1);
 }
 
-function power() {
-	
+function power(value1, value2) {
+  return Math.pow(value1, value2);
 }
 
-function factorial() {
-	
+function factorial(value) {
+  if (value == 0) {
+    return 1;
+  } else {
+    return value * factorial(value - 1);
+  }
 }
-
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
