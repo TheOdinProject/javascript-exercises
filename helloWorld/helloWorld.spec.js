b8b23686e3175694c4fe68eeb6431bd5c6b70e30
Michael Frank <mfra109@gmail.com>
2021-03-03 15:13:24 +1300

    Added jest

--- helloWorld/helloWorld.spec.js
+++ helloWorld/helloWorld.spec.js
@@ -1,7 +1,12 @@
+const { expect } = require('@jest/globals');
 const helloWorld = require('./helloWorld');
 
-describe('Hello World', function() {
-  it('says hello world', function() {
-    expect(helloWorld()).toEqual('Hello, World!');
-  });
+// describe('Hello World', function() {
+//   it('says hello world', function() {
+//     expect(helloWorld()).toEqual('Hello, World!');
+//   });
+// });
+
+test('says "Hello, World!"', () => {
+  expect(helloWorld()).toBe("Hello, World!");
 });
