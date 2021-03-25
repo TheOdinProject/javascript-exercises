750f0bb91dde2f1fecb9e194307bb7137e3b6727
msafdaar <71539665+msafdaar@users.noreply.github.com>
2021-03-25 18:38:31 +0500

    Solved LeapYears challenge

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year){
+  if (year%4 == 0){
+      if (year % 100 !=0){
+          return "Leap Year"
+      }
+      else if (year%400 == 0){
+          return "Leap Year"
+      }
+      else{
+          return "Not a Leap Year"
+      }
+  }
+  else {
+      return "Not a Leap Year"
+  }
 }
 
 module.exports = leapYears
