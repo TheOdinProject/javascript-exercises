7d76ea8058cbeaec319d8463bcc348a811a97235
Smyrna <81999424+MagentaSpruce@users.noreply.github.com>
2021-04-15 20:18:26 -0400

    Update tempConversion.js
    Proposed solution to empty answer template on The Odin Project's Test Driven Development section for tempConversion.js.

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenhite) {
+  const fTemp = fahrenhite;
+  const fToCel = Number(((fTemp - 32) * 5/9).toFixed(1));
+  return fToCel;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const cTemp = celsius;
+  const cToFahr = Number((cTemp * 9/5 + 32).toFixed(1));
+  return cToFahr;
 }
 
 module.exports = {
