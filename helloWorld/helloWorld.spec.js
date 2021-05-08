ffb5d83187db7dbf7699ddf480356aff011801e5
Tatiana <hello@tatianacodes.com>
2021-05-08 11:33:11 -0700

    Revert "Added jest"
    This reverts commit b8b23686e3175694c4fe68eeb6431bd5c6b70e30.

--- helloWorld/helloWorld.spec.js
+++ helloWorld/helloWorld.spec.js
@@ -1,12 +1,7 @@
-const { expect } = require('@jest/globals');
 const helloWorld = require('./helloWorld');
 
-// describe('Hello World', function() {
-//   it('says hello world', function() {
-//     expect(helloWorld()).toEqual('Hello, World!');
-//   });
-// });
-
-test('says "Hello, World!"', () => {
-  expect(helloWorld()).toBe("Hello, World!");
+describe('Hello World', function() {
+  it('says hello world', function() {
+    expect(helloWorld()).toEqual('Hello, World!');
+  });
 });
