34731c04f4df66c2b4b41d44820c04bd5a49bc54
Justin O'Reilly <joreilly25@gmail.com>
2021-05-21 18:49:49 -0400

    Added Jest

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (numStart, numEnd) {
+  // final sum variable
+  let sum = 0;
+  if (numStart < 0 || numEnd < 0 || typeof numStart != "number" || typeof numEnd != "number") {
+    return "ERROR";
+  } else if (numStart > numEnd) {
+    [numStart, numEnd] = [numEnd, numStart];
+  }
+  for (let i = numStart; i <= numEnd; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 module.exports = sumAll;
