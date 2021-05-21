513646cebfd75bf3a4bc9063633538116653e6d9
Justin O'Reilly <joreilly25@gmail.com>
2021-05-21 18:41:06 -0400

    changes to jest

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (checkLeapYear) {
+  if (checkLeapYear % 100 === 0 && checkLeapYear % 400 === 0) {
+    return true;
+  } else if (checkLeapYear % 4 === 0 && checkLeapYear % 100 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 module.exports = leapYears;
