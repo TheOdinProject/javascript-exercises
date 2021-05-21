bbdfafe5d4d748e7d695ad5798c9750fb00ff30b
Justin O'Reilly <joreilly25@gmail.com>
2021-05-21 18:46:07 -0400

    updated with Jest

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function (toCelsius) {
+  toCelsius = ((toCelsius - 32) * 5) / 9;
+  return Math.round(toCelsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (toFahr) {
+  toFahr = (toFahr * 9) / 5 + 32;
+  return Math.round(toFahr * 10) / 10;
 };
 
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
