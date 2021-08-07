61f38bf60cedaf210ddd067c624a086e0f2c7a71
Benjo Kho <benjokho@gmail.com>
2021-08-07 14:52:11 +0800

    Rename folders with numbers and underscores

--- 03 reverseString/reverseString.js
+++
@@ -1,5 +0,0 @@
-const reverseString = function() {
-
-};
-
-module.exports = reverseString;
---
+++ 03_reverseString/reverseString.js
@@ -0,0 +1,5 @@
+const reverseString = function(str) {
+    let newStr = str.split("").reverse().join("");
+};
+
+module.exports = reverseString;
