3236ae156ccc536c34435b134e6232d1f930bc3c
Benjo Kho <benjokho@gmail.com>
2021-08-07 14:54:14 +0800

    Revert edits

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
 const reverseString = function(str) {
-    let newStr = str.split("").reverse().join("");
+
 };
 
 module.exports = reverseString;
--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -5,14 +5,14 @@ describe('reverseString', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
 
-  test('reverses multiple words', () => {
+  test.skip('reverses multiple words', () => {
     expect(reverseString('hello there')).toEqual('ereht olleh')
   })
 
-  test('works with numbers and punctuation', () => {
+  test.skip('works with numbers and punctuation', () => {
     expect(reverseString('123! abc!')).toEqual('!cba !321')
   })
-  test('works with blank strings', () => {
+  test.skip('works with blank strings', () => {
     expect(reverseString('')).toEqual('')
   })
 });
