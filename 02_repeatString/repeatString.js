7a00112f9a5c65bc580f36189abc3b85d4046ef8
BananaBJones <Ryan_gm@sbcglobal.net>
2021-11-02 15:17:05 -0600

    d

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = '';
 
+    for (i = 0; i < num; i++) {
+        newString += string; 
+    }
+    return newString;
 };
+
+console.log(repeatString('hey', 3))
 
 // Do not edit below this line
 module.exports = repeatString;
--- 02_repeatString/repeatString.spec.js
+++ 02_repeatString/repeatString.spec.js
@@ -4,13 +4,13 @@ describe('repeatString', () => {
   test('repeats the string', () => {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  test.skip('repeats the string many times', () => {
+  test('repeats the string many times', () => {
     expect(repeatString('hey', 10)).toEqual('heyheyheyheyheyheyheyheyheyhey');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hey', 1)).toEqual('hey');
   });
-  test.skip('repeats the string 0 times', () => {
+  test('repeats the string 0 times', () => {
     expect(repeatString('hey', 0)).toEqual('');
   });
   test.skip('returns ERROR with negative numbers', () => {
