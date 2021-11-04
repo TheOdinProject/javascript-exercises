15d5b1c9bc0794d27a99a61404a5aaab9204633f
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2021-11-04 14:44:23 +0100

    Add new test cases for palindromic numbers

--- 09_palindromes/palindromes.spec.js
+++ 09_palindromes/palindromes.spec.js
@@ -19,4 +19,16 @@ describe('palindromes', () => {
   test.skip('doesn\'t just always return true', () => {
     expect(palindromes('ZZZZ car, a man, a maraca.')).toBe(false);
   });
+  test.skip('works with numbers that are palindromes', () => {
+    expect(palindromes('1005001')).toBe(true);
+  });
+  test.skip('works with numbers that are not palindromes', () => {
+    expect(palindromes('12345')).toBe(false);
+  });
+  test.skip('works with datetime stamp using short digits', () => {
+    expect(palindromes('11/11/11 11:11')).toBe(true);
+  });
+  test.skip('works with date stamp using long digits', () => {
+    expect(palindromes('02/02/2020.')).toBe(true);
+  });
 });
