c51b7c2c61c32950ab2251c6e8970a5eaf96fdd4
pedro-petrov <petrovonrails@gmail.com>
2022-01-28 19:59:32 +0000

    commit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return (num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(array1) {
+	var mySum = 0;
+  for (var i = 0; i < array1.length; i++) {
+    mySum = mySum + array1[i];
+  }
+  return mySum;
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  var myNum = 1;
+  for (var num in nums) {
+    console.log(`total: ${myNum}, index: ${num}, value: ${nums[num]}, is number ${typeof myNum}`);
+    myNum = myNum * nums[num];
+  }
+return myNum;
 };
 
-const power = function() {
-	
+var test1 = multiply(2, 4, 6, 8, 10, 12, 14);
+console.log(test1);
+
+const power = function(num1, num2) {
+	return (num1 ^ num2);
 };
 
-const factorial = function() {
+const factorial = function(num1) {
 	
 };
 
