e2738642a732826babee4d038967187494e95a8b
Morthiner <88753185+Morthiner@users.noreply.github.com>
2022-02-01 09:10:28 -0400

    Fix the for loop
    Changing the for loop's condition so that it can fit the tests

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -7,7 +7,7 @@ const sumAll = function(min, max) {
     max = temp;
   }
   let sum = 0;
-  for (let i = min; i < max + 1; i++) {
+  for (let i = min; i <= max + 1; i++) {
     sum += i;
   }
   return sum;
