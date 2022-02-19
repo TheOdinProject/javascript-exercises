9b1f10068ab4309e6737ecdab482ab671e0c4685
borobor <bornajurisa@gmail.com>
2022-02-19 23:04:07 +0100

    Write a completely new function that passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,28 @@
-const removeFromArray = function(arr, item) {
-	let removedArr = arr.splice((item - 1), 1);
-	return arr;
+//rest parameters! => ...Args
+
+const removeFromArray = function(...input) {
+	let array = input[0];
+	let newArray = [];
+
+	array.forEach((item) => {
+		if (!input.includes(item)) newArray.push(item);
+	})
+
+	return newArray;
 };
+
+//let's try the same but with .filter()!
+// const removeFromArray = function (...input) {
+// 	array = input[0];
+
+// 	newArray = array.filter((item) => {
+// 		if (!input.includes(item)) return true;
+// 	})
+
+// 	return newArray;
+// };
+
+console.log(removeFromArray([1, 2, 3, 4], 2, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
