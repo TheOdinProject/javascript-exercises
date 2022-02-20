fb1a2db8d734e813dad8ede3783ea6c290239083
thatblindgeye <thatblindgeye@gmail.com>
2022-02-20 14:07:44 -0500

    Add solution directories for exercises

---
+++ 07_tempConversion/solution/tempConversion-solution.js
@@ -0,0 +1,12 @@
+const ftoc = function (f) {
+    return Math.round((f - 32) * (5 / 9) * 10) / 10;
+};
+
+const ctof = function (c) {
+    return Math.round(((c * 9) / 5 + 32) * 10) / 10;
+};
+
+module.exports = {
+    ftoc,
+    ctof,
+};
