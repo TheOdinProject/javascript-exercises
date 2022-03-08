180c199870f4e8ccc64933b71f72fb9c4e7cd05b
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-08 11:50:07 -0800

    loop solution for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+	let backString = '';
 
+	for (i = string.length - 1; i >= 0; i--) {
+		backString += string[i];
+	}
+	console.log(backString);
+	return backString;
 };
+reverseString('Howdy');
 
 // Do not edit below this line
 module.exports = reverseString;
