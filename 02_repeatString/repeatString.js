6a4d5f7e11d115d31b8ba0299352fe5741bbb84d
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-08 10:35:41 -0800

    posting solutions exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+	let string = '';
+    if (num < 0) {
+        return ERROR
+    }
+	for (i = 0; i < num; i++) string += str;
 
+	console.log(string);
+	return string;
+	i++;
 };
-
+repeatString('howdy', 3);
+repeatString('howdy', 3);
 // Do not edit below this line
 module.exports = repeatString;
