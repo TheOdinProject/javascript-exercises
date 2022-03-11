0721e9d02263524957e0d06416ff28a118c3ae5b
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-11 09:29:22 -0800

    solution exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    let a = [];
+    let b = [];
+    if (num1 < num2) {
+        a = num1;
+        b = num2;
+}else {
+        a = num2;
+        b = num1;
+    }
+    if (a <0 || typeof a == 'string' || typeof b == 'string' || typeof a == 'object' || typeof b == 'object'){
+      console.log('ERROR');
+      return 'ERROR'
+    
+    }
+    for (i = a; i <= b; i++){
+        total += i;
+    
+    }
+ // console.log (a)
+    console.log(total)
+    return total
+    };
+    sumAll(5, 7)
 // Do not edit below this line
 module.exports = sumAll;
