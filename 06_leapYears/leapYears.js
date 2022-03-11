a07441c9d3166df83fa47a5381213ba86f03551f
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-11 11:30:30 -0800

    6 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
+        console.log ('true')
+        return (true)
+    }else if (year % 100 === 0 && year % 400 === 0){
+      console.log ('true')
+      return (true)
+    }else {
+     console.log ('false')
+      return (false)
+    }
+    }; 
+    leapYears(1984)
 // Do not edit below this line
 module.exports = leapYears;
