ae2ab746beddd2306533887dbde328d9d9338909
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-15 11:05:02 -0700

    07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+	let cel = (f - 32) * 5 / 9;
+	cel = cel.toFixed(1);
+	console.log(parseFloat(cel));
+	return parseFloat(cel);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+	let faren = c * 9 / 5 + 32;
+	faren = faren.toFixed(1);
+	console.log(parseFloat(faren));
+	return parseFloat(faren);
 };
 
+ftoc(-21);
+ctof(-10);
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof
 };
