612360bded6655953c3f8eefbe098ea3d0ce4b4c
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-18 11:25:38 -0700

    09 Solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    step1 = str.toLowerCase().replace(/[^a-zA-Z0-9s+]/g, "");
+    result = step1.split('').reverse().join('');
+    if (result == step1){
+    console.log(result);
+    return true
+    }else {console.log('False')}
+    return false
+    
+    };
+   // palindromes('123, 321');
 
 // Do not edit below this line
 module.exports = palindromes;
