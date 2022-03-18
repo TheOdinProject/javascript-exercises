0afea275eeaf539e83fe820c8ad3b74c0a50f00a
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-18 11:27:30 -0700

    09 format correction

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,17 @@
-const palindromes = function (str) {
-    step1 = str.toLowerCase().replace(/[^a-zA-Z0-9s+]/g, "");
-    result = step1.split('').reverse().join('');
-    if (result == step1){
-    console.log(result);
-    return true
-    }else {console.log('False')}
-    return false
-    
-    };
-   // palindromes('123, 321');
+const palindromes = function(str) {
+	step1 = str
+		.toLowerCase()
+		.replace(/[^a-zA-Z0-9s+]/g, '');
+	result = step1.split('').reverse().join('');
+	if (result == step1) {
+		console.log(result);
+		return true;
+	} else {
+		console.log('False');
+	}
+	return false;
+};
+// palindromes('123, 321');
 
 // Do not edit below this line
 module.exports = palindromes;
