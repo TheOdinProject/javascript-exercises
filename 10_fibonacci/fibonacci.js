ad8597d5ad65b451cdff41a1110e9b8929722941
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-03-18 13:41:54 -0700

    10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    const nbr = [0,1]
+    if (num < 0){
+    console.log ('OOPS');
+    return 'OOPS';
+    }else{
+    for (var i = 2; i <= num; i++) 
+    nbr.push(nbr[i-1] + nbr[i-2]);
+    }
+    console.log(nbr[i-1])
+    return nbr[i-1]
+    };
+  
+    //fibonacci(10)
 // Do not edit below this line
 module.exports = fibonacci;
