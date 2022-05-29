4452ca004025ea5cc8dec1a281e854590f3be46e
Endritibra <Endrit.ibraimimax@live.com>
2022-05-30 00:14:19 +0200

    Add files via upload

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringi,times) {
+	if(times<0){
+	return 'ERROR'}
+	else{
+	let i;
+	let res='';	
+for(i=0;i<times;i++){
+	res+=stringi;}
+return res;
+	}	
 };
 
 // Do not edit below this line
