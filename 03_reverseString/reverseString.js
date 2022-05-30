4e0fa06dcbe75e81afef504b11ab7975b61fdb9c
endritibra <endrit.ibraimimax@live.com>
2022-05-30 13:41:52 +0200

    -3 ex done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(stringi) {
+	let test="";
+	let len=stringi.length;
+	console.log(len)
+	for(i=len-1;i>=0;i--){
+	test+=stringi[i];
+	 
+	}
+	return test;
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
