5225770c5de5d91c89b05abee1bc03c2a2488a70
endritibra <endrit.ibraimimax@live.com>
2022-06-01 19:59:59 +0200

    Ex6 Done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears= function(year){
+    
+    if(year%4==0 && (year % 100 !== 0 || year % 400 === 0)){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+}
 
-};
 
 // Do not edit below this line
 module.exports = leapYears;
