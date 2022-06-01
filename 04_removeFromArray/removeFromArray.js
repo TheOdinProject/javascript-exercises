c48dc30ec796aac2ec9e305ccadc6c25d9604972
endritibra <endrit.ibraimimax@live.com>
2022-06-01 03:05:21 +0200

    New ex.4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
 
-};
+
+function removeFromArray(...args) {
+
+
+const array=args[0];
+const newArray=[];
+
+array.forEach((item)=>{
+   
+if(!args.includes(item)){
+newArray.push(item);}
+});
+return newArray;};
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+
+
