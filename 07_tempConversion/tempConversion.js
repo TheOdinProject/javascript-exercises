e0275b086ebe4cefad481a153b7d57ac5a2512e1
endritibra <endrit.ibraimimax@live.com>
2022-06-02 19:10:06 +0200

    Nas Album Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+	return Math.round((5/9 * (fah-32))*10)/10;
 };
 
-const ctof = function() {
+const ctof = function(c) {
+     return Math.round(((c * 9/5) + 32)*10)/10;
 
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
