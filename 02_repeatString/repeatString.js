c1a82aa53ece617e29b3f09c2a02687bee99d306
Don <ggarbnod@hotmail.com>
2022-06-22 08:48:14 -0400

    Completed helloWorld and repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = ''
+    if (num < 0) {return "ERROR"}
+    for (let i = 0; i < num; i++) {
+        repeatedString += string
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
