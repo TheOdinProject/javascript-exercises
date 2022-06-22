f74860d12d8e3933571e08e45d01c4d7f7883a12
Don <ggarbnod@hotmail.com>
2022-06-22 09:41:50 -0400

    Finished the reverseString activity

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversedArray = []
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedArray.push(string[i])
+    }
+    return reversedArray.join('')
 };
 
 // Do not edit below this line
