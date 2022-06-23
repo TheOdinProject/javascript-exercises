b8913a605eab3bcf1538af5c8d8d1193e77fd6c6
Don <ggarbnod@hotmail.com>
2022-06-23 15:26:25 -0400

    Merge branch 'master' of https://github.com/nard0g/javascript-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-    //
+const removeFromArray = function(...args) {
+    let origArray = args[0]
+    let newArr = []
+    origArray.forEach((arg) => {
+        if (!args.includes(arg)) {
+            newArr.push(arg)
+        }
+    })
+
+    return newArr
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+removeFromArray([1, 2, 3, 4], 3)
