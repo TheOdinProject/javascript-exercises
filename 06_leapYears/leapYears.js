242936c0467839d0345304a5109bb494349c4997
Don <ggarbnod@hotmail.com>
2022-06-23 15:32:25 -0400

    Completed leapYears and tempConversion exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true
+            }
+            return false
+        }
+        return true
+    }
+    return false
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  return Number(((fTemp - 32) * (5 / 9)).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  return Number(((cTemp * (9 / 5)) + 32).toFixed(1))
 };
 
 // Do not edit below this line
