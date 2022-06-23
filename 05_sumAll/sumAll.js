f7aa76e904d4b600896b325e791605525ab67028
Don <ggarbnod@hotmail.com>
2022-06-23 15:28:30 -0400

    Completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (x < 0 || y < 0) {
+        return "ERROR"
+    } else if (typeof x != "number" || typeof y != "number") {
+        return "ERROR"
+    }
+    
+    let diff = x - y
+    if (diff < 0) {
+        diff *= -1
+    }
+    
+    let sum = 0
+    let min = x
 
+    if (x > y) {
+        min = y
+    }
+
+    for (let i = 0; i <= diff; i++) {
+        sum += min
+        min++
+    }
+    return sum
 };
 
 // Do not edit below this line
