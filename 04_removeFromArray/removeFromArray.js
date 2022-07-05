ae22d1c5dea5917177895c9880857fdf116951a2
Maganra <brennan.l.olson@gmail.com>
2022-07-05 14:41:26 -0500

    finished 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
+    /*** cool fancy way ***/
+    // return array.filter(x => !args.includes(x))
+
+    /*** simpler way ***/
+    const output = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            output.push(item);
+        }
+    })
+    return output;
 };
 
 // Do not edit below this line
