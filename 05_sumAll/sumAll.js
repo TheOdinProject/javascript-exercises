398236dd86ded98d2ada74c2475689263c2dc2ce
Maganra <brennan.l.olson@gmail.com>
2022-07-05 15:07:58 -0500

    Finished 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    let sum = 0;
+    if (typeof(arg1) != 'number' || typeof(arg2) != 'number' || arg1 < 0 || arg2 < 0) {
+        return "ERROR";
+    }
+    let small, big;
+    if (arg1 < arg2) {
+        small = arg1;
+        big = arg2;
+    } else {
+        small = arg2;
+        big = arg1;
+    }
+    for (let i = small; i <= big; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
