96b8fafe7eb2141c4873a8758d2a093a1c91221d
Maganra <brennan.l.olson@gmail.com>
2022-07-05 14:03:38 -0500

    Finished JavaScript Excersises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (i = 1; i <= num; i++) {
+        output += string;
+    }
+    return output;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
