6b172adb5923576a0ee0c71aceed0e446ae469de
Maganra <brennan.l.olson@gmail.com>
2022-07-05 15:36:18 -0500

    Finished 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fnum) {
+  ctemp = Math.round(((fnum - 32) * (5/9)) * 10) / 10;
+  return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(cnum) {
+  ftemp = Math.round(((cnum * (9/5)) + 32) * 10) / 10;
+  return ftemp;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -4,22 +4,22 @@ describe('ftoc', () => {
   test('works', () => {
     expect(ftoc(32)).toEqual(0);
   });
-  test.skip('rounds to 1 decimal', () => {
+  test('rounds to 1 decimal', () => {
     expect(ftoc(100)).toEqual(37.8);
   });
-  test.skip('works with negatives', () => {
+  test('works with negatives', () => {
     expect(ftoc(-100)).toEqual(-73.3);
   });
 });
 
 describe('ctof', () => {
-  test.skip('works', () => {
+  test('works', () => {
     expect(ctof(0)).toEqual(32);
   });
-  test.skip('rounds to 1 decimal', () => {
+  test('rounds to 1 decimal', () => {
     expect(ctof(73.2)).toEqual(163.8);
   });
-  test.skip('works with negatives', () => {
+  test('works with negatives', () => {
     expect(ctof(-10)).toEqual(14);
   });
 });
