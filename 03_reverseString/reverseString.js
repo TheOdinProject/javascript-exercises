ca4ae67a3249c29bce373bfea5a9107d556ae716
Maganra <brennan.l.olson@gmail.com>
2022-07-05 14:21:18 -0500

    Finished 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse = '';
+    for (let i = string.length; i > 0; i--) {
+        reverse += string.charAt(i-1);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
