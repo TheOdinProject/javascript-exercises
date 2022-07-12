14947e81ab5daf172dda184b3ac9fd486b46f8f6
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-12 09:32:31 +0800

    initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if( num > 0 ){
+    let repeated = string.repeat(num);
+    return repeated;
+    } else if( num === 0 ){
+     return "";
+    }else if ( num < 0 ){
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
