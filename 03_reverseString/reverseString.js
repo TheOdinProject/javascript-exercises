fcf255662a9eb6edcc04d42ecbde039c080dbcc4
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-12 11:47:16 +0800

    added answers

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+for(let i = string.length - 1; i >= 0; i--){
+    reversed+=string[i];
+}
+return reversed;
 };
 
 // Do not edit below this line
