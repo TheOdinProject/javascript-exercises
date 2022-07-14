f529df4e26596dfe70e712605746eb2e1de4dd26
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-15 07:41:15 +0800

    initial commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
 const removeFromArray = function(arr, remove) {
- 
-};
-
+    if(arr.includes(remove)){
+       let removedValue = arr.splice(arr[remove] - 2, 1);
+       let clean = arr.filter(( para ) => {
+       return para != removedValue;
+       });
+       return clean;
+    }
+   };
+   
+   console.log(removeFromArray([1,2,3,4], 3));
 // Do not edit below this line
 module.exports = removeFromArray;
