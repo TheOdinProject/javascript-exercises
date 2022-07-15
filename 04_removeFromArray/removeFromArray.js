5b8f2c93c8ffa0f118245838137b7e8208bede54
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-15 09:00:24 +0800

    added answers

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,8 @@
-const removeFromArray = function(arr, remove) {
-    if(arr.includes(remove)){
-       let removedValue = arr.splice(arr[remove] - 2, 1);
-       let clean = arr.filter(( para ) => {
-       return para != removedValue;
-       });
-       return clean;
-    }
-   };
-   
-   console.log(removeFromArray([1,2,3,4], 3));
+const removeFromArray = function(...args) {
+ const array = args[0];
+ return array .filter( val => !args.includes(val));
+};
+
 // Do not edit below this line
 module.exports = removeFromArray;
+
