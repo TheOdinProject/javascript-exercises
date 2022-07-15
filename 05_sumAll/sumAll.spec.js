0cf0c04a5e8e641a6490a87fdfa920ff3e7bc7db
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-15 09:08:46 +0800

    removed a .skip

--- 05_sumAll/sumAll.spec.js
+++ 05_sumAll/sumAll.spec.js
@@ -4,7 +4,7 @@ describe('sumAll', () => {
   test('sums numbers within the range', () => {
     expect(sumAll(1, 4)).toEqual(10);
   });
-  test.skip('works with large numbers', () => {
+  test('works with large numbers', () => {
     expect(sumAll(1, 4000)).toEqual(8002000);
   });
   test.skip('works with larger number first', () => {
