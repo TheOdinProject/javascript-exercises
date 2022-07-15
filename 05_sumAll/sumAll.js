367e9405dd067656830fd0a2df9db6c209d56360
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-15 13:06:26 +0800

    removed redundant comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,22 +1,8 @@
-/*function greater(num1, num2){
-   if(num1 > num2){
-    return num1;
-   } else if(num2 > num1){
-   return num2;
-   }
-  }
-  function least(num1, num2){
-   if(num1 < num2){
-    return num1;
-   } else if(num2 < num1){
-   return num2;
-   }
-  }
-*/
-
 const sumAll = function(num1, num2) {
     let sum = 0;
+
       if( Math.sign(num1 || num2) !== -1 && typeof(num1 && num2) === "number" ){
+
     const least = (num1,num2) =>{
       if(num1 < num2){
       return num1;
@@ -31,7 +17,7 @@ const sumAll = function(num1, num2) {
         } else if(num2 > num1){
          return num2;
         }
-     }
+   }
      
  for( let i = least(num1,num2); i <= greater(num1,num2); i++){
     sum+=i;
@@ -40,6 +26,8 @@ const sumAll = function(num1, num2) {
 } else{
    return "ERROR";
 }};
+
 console.log(sumAll(1, 4));
+
 // Do not edit below this line
 module.exports = sumAll;
