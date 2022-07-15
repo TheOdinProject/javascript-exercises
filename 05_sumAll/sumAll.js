9921be17def068e93790e18e948c0b4c3611f711
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-15 09:07:07 +0800

    added loop that iterates num1 to num2 and add its value

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+ for( let i = num1; i <= num2; i++){
+    sum+=i;
+ }
+ return sum;
 };
-
+sumAll(1, 4);
 // Do not edit below this line
 module.exports = sumAll;
