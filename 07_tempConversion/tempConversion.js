19eb17bce00b5d86c75d5a5cdebba485527a2fbf
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-07-16 10:32:21 +0800

    added answers

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+ //let converted = parseInt(fahr); 
+ let converted = (fahr - 32) * 5/9;
+ return Math.round(converted * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  //let converted = parseInt(cel);
+  let converted = (cel * 9/5) + 32;
+ return Math.round(converted * 10) / 10;
 };
 
 // Do not edit below this line
