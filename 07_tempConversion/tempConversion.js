4ff7b6611fbd93061c6608fd61082f2325ce3cdd
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2022-07-23 14:20:28 +0200

    solved 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let converted = (temp - 32) * (5 / 9);
+  return Math.round((converted + Number.EPSILON) * 10) / 10
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+  let converted = temp * (9 / 5) + 32;
+  return Math.round((converted + Number.EPSILON) * 10) / 10
 
 };
 
