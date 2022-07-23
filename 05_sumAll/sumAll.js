bd85c011fe48d7ba4a3e760a0b51b0f4912c0722
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2022-07-23 12:59:20 +0200

    solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function () {
+    if (typeof(arguments[0]) === 'number' && typeof(arguments[1]) === 'number' && arguments[0] >= 0 && arguments[1] >= 0) {
+        let biggerNum = 0, smallerNum = 0;
+        if (arguments[0] > arguments[1]) {
+            biggerNum = arguments[0];
+            smallerNum = arguments[1];
+        } else {
+            biggerNum = arguments[1];
+            smallerNum = arguments[0];
+        }
 
+        let result = 0;
+
+        for (let i = smallerNum; i <= biggerNum; i++) {
+            result += i;
+        }
+
+        return result;
+    }
+
+    return 'ERROR';
 };
 
 // Do not edit below this line
