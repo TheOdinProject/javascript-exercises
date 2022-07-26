707a5890090da813161d69e29d7a3edc00ef49e4
Don <ggarbnod@hotmail.com>
2022-07-26 15:59:26 -0400

    completed Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(firstVariable, secondVariable) {
+	return firstVariable + secondVariable;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstVariable, secondVariable) {
+	return firstVariable - secondVariable;
 };
 
-const sum = function() {
-	
+const sum = function(myArray) {
+	return myArray.reduce((runningTotal, myVariable) => runningTotal + myVariable, 0);
 };
 
-const multiply = function() {
+const multiply = function(myArray) {
+  return myArray.reduce((runningTotal, myVariable) => runningTotal * myVariable, 1);
 
 };
 
-const power = function() {
-	
+const power = function(baseNum, power) {
+	return baseNum ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(myVariable) {
+	if (myVariable < 0) {
+    return -1;
+  } else if (myVariable === 0) {
+    return 1;
+  } else {
+    return (myVariable * factorial(myVariable - 1))
+  }
 };
 
 // Do not edit below this line
