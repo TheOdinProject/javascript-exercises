f7ac5a2f92b55e6fa9148c84d874315f14c17bea
Don <ggarbnod@hotmail.com>
2022-07-28 11:21:24 -0400

    Completed palindromes and fibonacci exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+    const strippedString = myString.replace(/[^a-z0-9]/gi, '').toUpperCase();
+    const reversedString = strippedString.split("").reverse().join("").toUpperCase();
+//    console.log(strippedString)
+//    console.log(reversedString)
+    return strippedString === reversedString ? true: false;
 };
+
+palindromes("racecar!")
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(fibLength) {
 
+    if (fibLength < 0) {
+        return "OOPS";
+    }
+
+    if (fibLength === 0) {
+        return 0;
+    }
+    
+    let valueA = 0;
+    let valueB = 1;
+    
+    for (i = 1; i < fibLength; i++) {
+        let tempValue = valueB;
+        valueB += valueA;
+        valueA = tempValue;
+    }
+    return valueB;
 };
+
+fibonacci('1')
 
 // Do not edit below this line
 module.exports = fibonacci;
