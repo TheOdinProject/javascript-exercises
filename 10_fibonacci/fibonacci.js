52e5b59c9183ac51c5a51535db588500001945a6
Abdelhakim <elouakouakabdelhakim@gmail.com>
2022-08-06 20:35:22 +0100

    add: solution to the fibonacci exercice using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if(n<0) return 'OOPS';
 
+    if(n == 1 || n == 2){
+        return 1;
+    }
+    return(fibonacci(n-1)+fibonacci(n-2));
 };
 
 // Do not edit below this line
