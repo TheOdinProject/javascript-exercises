8766dc629b756bcbf05263509578a4a0e26d2a9c
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-08-10 21:39:21 +0600

    Updated sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = (n1,n2)=>{
+    return (typeof(n1 && n2) === 'number'? (n1 < n2 ? firstNumberGreater(n1,n2):lastNumberGreater(n1,n2)) : 'ERROR')
+};
 
+const firstNumberGreater = (first_num,last_num)=>{
+    let i;
+    let new_num;
+    Math.sign(first_num) === 1 && Math.sign(last_num) === 1?(i = first_num - 1 , new_num = first_num -1):(i = first_num - 1, new_num = 0);
+    while(i < last_num){
+        i++;
+        new_num += i;
+    }
+    return new_num;
+};
+const lastNumberGreater = (first_num,last_num)=>{
+    let i;
+    let new_num;
+    Math.sign(first_num) === 1 && Math.sign(last_num) === 1?(i = last_num - 1 , new_num = last_num -1):(i = last_num - 1, new_num = 0);
+    while(i < first_num){
+        i++;
+        new_num += i;
+    }
+    return new_num;
 };
 
 // Do not edit below this line
