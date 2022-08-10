5ce3d6acf735a93fe2e0b8599de27b2441305852
Mahir Mahdi <66949598+Mahir1015@users.noreply.github.com>
2022-08-10 21:40:24 +0600

    Updated sumAll.spec.js

--- 05_sumAll/sumAll.spec.js
+++ 05_sumAll/sumAll.spec.js
@@ -10,8 +10,8 @@ describe('sumAll', () => {
   test.skip('works with larger number first', () => {
     expect(sumAll(123, 1)).toEqual(7626);
   });
-  test.skip('returns ERROR with negative numbers', () => {
-    expect(sumAll(-10, 4)).toEqual('ERROR');
+  test.skip('works with negative numbers', () => {
+    expect(sumAll(-10, 4)).toEqual(-45);
   });
   test.skip('returns ERROR with non-number parameters', () => {
     expect(sumAll(10, "90")).toEqual('ERROR');
