ea3e8617996f454772631039f54e6e1e42aa6ed6
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-08-14 09:52:30 +0800

    added answers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const punctuationsAndWordBreaks = /[!"@#$%^&*()_+-<>?/.,\s|`~]/g;
+    let pureString = word.toLowerCase();
+    pureString = pureString.replace(punctuationsAndWordBreaks, "");
 
+    let result = "";
+    for(let i = pureString.length - 1; i >= 0; i--){
+        result += pureString[i];
+    }
+    if( result === pureString ){
+     return true;
+    } else{
+     return false;
+    }
 };
+
+console.log(palindromes('Racecar!')); //should return true
+console.log(palindromes('A car, a man, a maraca.')); //should return true
+console.log(palindromes('Animal loots foliated detail of stool lamina.')); //should return true
 
 // Do not edit below this line
 module.exports = palindromes;
