b9ca198809b0a479224cb934f99b5d43634bd0e4
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-08-14 12:06:59 +0800

    added answers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let stringToNumber = +num;
 
+    let val1 = 0;
+    let val2 = 1;
+    let result = stringToNumber;
+
+    if( result > 0 ){
+    for(let i = 2; i <= num; i++){
+        result = val1 + val2;
+        val1 = val2;
+        val2= result;
+    }
+
+    return result;
+ } else{
+    return "OOPS";
+ }
 };
 
 // Do not edit below this line
