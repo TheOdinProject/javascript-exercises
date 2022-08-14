e93ee203967ba3df193f8809ab75930ed23ec64c
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-08-14 12:41:11 +0800

    added answers

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
-};
+function getTheTitles(book){
+    const result = book.map(getTitle => getTitle.title);
+    return result;
+} 
 
 // Do not edit below this line
 module.exports = getTheTitles;
