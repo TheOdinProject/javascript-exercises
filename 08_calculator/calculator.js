2ed3dee244f3d7d8a2f0c72488ac326d139b8193
Jared Ramon Elizan <elizanjaredr@gmail.com>
2022-08-14 08:47:33 +0800

    added answers

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,36 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(a, b) {
-	return a + b
+const sum = function(num) {
+	let sum = 0;
+  for( let i = 0; i < num.length; i++){
+    sum += num[i];
+  }
+  return sum;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(num) {
+  let product = 1;
+  for(let i = 0; i < num.length; i++){
+  product = product * num[i];
+  }
+  return product;
 };
 
 const power = function(a, b) {
 	return Math.pow(a, b);
 };
 
-const factorial = function(a, b) {
-	return a / b;
+const factorial = function(num) {
+	let result = 1;
+  if( num === 0 || num === 1 ){
+    return result;
+  } else{
+    for(let i = num; i >= 1; i--){
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
