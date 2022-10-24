a4c8df3240d34732b80fa55896996a6ba0ed1d73
octopusGarden <zebraspider@protonmail.com>
2022-10-24 16:18:16 -0400

    Completed the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+//Convert String to Array
+//Reverse Array
+    return string.split('').reverse().join('').toString();
 
+//Join & Convert to String
 };
 
 // Do not edit below this line
