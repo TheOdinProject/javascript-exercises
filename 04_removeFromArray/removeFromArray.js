49743d45eedf638cdf49443baec6460f7e0d9090
octopusGarden <zebraspider@protonmail.com>
2022-10-24 16:58:25 -0400

    Completed fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list) {
+//Remove third element from array
+    let remove = list.splice(2, 1);
+    return list;
 };
 
 // Do not edit below this line
