765dc7031af350607184c1569fe7d2a36bfa315e
octopusGarden <zebraspider@protonmail.com>
2022-10-25 14:30:50 -0400

    Completed seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fnum) {
+  //formula to convert f to c
+  let cnum = (fnum - 32) * 5 % 9;
+  //round to one decimal
+  let roundCnum = parseInt(cnum.toFixed(1));
+  return roundCnum;
 };
 
 const ctof = function() {
-
+  //formula to convert c to f
+  let fnum = (cnum * 9/5) + 32;
+  //round to one decimal
+  let roundFnum = parseInt(fnum.toFixed(0));
+  return roundFnum;
 };
 
 // Do not edit below this line
