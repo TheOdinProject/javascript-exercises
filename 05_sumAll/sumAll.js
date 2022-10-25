8800dd93ae8dd0c5dc948cb8cdf81e8678456fee
octopusGarden <zebraspider@protonmail.com>
2022-10-25 13:21:06 -0400

    Completed the fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+            
+    for (let i = num1; i <= num2; i++) {
+        sum += parseInt(i);
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
