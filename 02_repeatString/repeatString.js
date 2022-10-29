8ec38c4b573fe89ccf343f70a7003a1f5d61866b
Isah Jacob <Isahjacob0@gmail.com>
2022-10-29 11:05:02 +0100

    created a variable that hold the string. loop through the number of times the string is repeated. assigned the number time the string is repeated to the variable created

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    var repeatedWord = "";
+    while (num >  0){
+        repeatedWord += string;
+        num--;
+    }
+    return repeatedWord;
 };
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString('hey', -1)
 
 // Do not edit below this line
 module.exports = repeatString;
