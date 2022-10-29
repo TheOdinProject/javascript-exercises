746d12289678a226e67a0e03d34bc0dfdbf8f72d
Isah Jacob <Isahjacob0@gmail.com>
2022-10-29 11:35:46 +0100

    added a parameter to the function I used the .split, .reverse and .join method to be able to reverse the string created a variable that holds the reversed string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let enteredWord = string.split("").reverse().join("");
+    return enteredWord
 };
-
+reverseString("hello")
+reverseString("hello there")
+reverseString('!abc 123!')
+reverseString('')
 // Do not edit below this line
 module.exports = reverseString;
