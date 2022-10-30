34e89ed634c32c2d911b16df7571465fd8c171c5
Isah Jacob <Isahjacob0@gmail.com>
2022-10-30 23:46:42 +0100

    An if statement that check if leapYear is divisible by 4 will equal 0 and also check if leapYear divisible by 100 will not equal 0 and compare also if leapYear is divisible by 400 and will equal 0

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    return leapYear % 4 === 0 && (leapYear % 100 !== 0 || leapYear % 400 == 0)
 };
-
+leapYears(1996)
+leapYears(1997)
+leapYears(34992)
+leapYears(1900)
+leapYears(1600)
+leapYears(700)
 // Do not edit below this line
 module.exports = leapYears;
