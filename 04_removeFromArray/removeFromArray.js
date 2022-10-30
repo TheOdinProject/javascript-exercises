32bea92581f732753d0330cfc06964e76b9dc193
Isah Jacob <Isahjacob0@gmail.com>
2022-10-30 19:05:13 +0100

    I assigned the first element in the list to array created an empty array and I use forEach to loop through the element to assign the element to the newArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...arrayOfNumbers){
+    const array = arrayOfNumbers[0];
+    const newArray = [];
+    array.forEach((element) => {
+      if (!arrayOfNumbers.includes(element)){
+        newArray.push(element)
+      }  
+    });
+    return  newArray;
+    
 };
-
+removeFromArray([1,2,3,4], 3)
+removeFromArray([1, 2, 3, 4], 3, 2)
+removeFromArray([1, 2, 3, 4], 7,)
+removeFromArray([1, 2, 3, 4], 7, 2)
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3)
+removeFromArray([1, 2, 3], "1", 3)
 // Do not edit below this line
 module.exports = removeFromArray;
