8c96eb988d5f2145cbf376096f3c6282a068d336
Isah Jacob <Isahjacob0@gmail.com>
2022-10-31 17:23:43 +0100

    modified the test file. running test only on the first test case. the skip method remains on the rest

--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -13,7 +13,7 @@ describe('ftoc', () => {
 });
 
 describe('ctof', () => {
-  test.skip('works', () => {
+  test('works', () => {
     expect(ctof(0)).toEqual(32);
   });
   test.skip('rounds to 1 decimal', () => {
