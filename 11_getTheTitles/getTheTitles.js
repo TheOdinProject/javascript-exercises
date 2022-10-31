879d67a4b28001747c108411a185d036fc6492ae
Isah Jacob <Isahjacob0@gmail.com>
2022-10-31 21:59:13 +0100

    I supplied a parameter called array I use .map method to map the book to the title.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(array) {
+    return array.map(book => book.title)
+  };
+    
+  const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
 // Do not edit below this line
 module.exports = getTheTitles;
