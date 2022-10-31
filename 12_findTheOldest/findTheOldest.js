8f4b3f9ce92b2d01871551f6da30b5b68f98669b
Isah Jacob <Isahjacob0@gmail.com>
2022-10-31 23:30:08 +0100

    modified the functions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+      const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath)
+      const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+      return oldestAge < currentAge ? currentPerson : oldest
+    })
+  };
+  
+  const getAge = function(birth, death) {
+    if (!death) {                             
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
+  
+  
+  
+  const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
 // Do not edit below this line
 module.exports = findTheOldest;
