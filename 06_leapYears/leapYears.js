ddac01b1f40f589d53a9b1af3ee36cbab0f3413b
octopusGarden <zebraspider@protonmail.com>
2022-11-04 14:39:31 -0400

    Fixed leap years to work with centuries

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -4,7 +4,7 @@ const leapYears = function(year) {
     //else false
     if (year % 100 === 0 && year % 400 === 0) {
         return true;
-    } else if (year % 4 === 0) {
+    } else if (year % 4 === 0 && year % 100 !== 0) {
         return true;
     } else {
         return false;
