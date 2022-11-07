85ea76b0a5183120d6ae35cff5d734b9e2a1c14d
reddforman <edwardhamidov@aol.com>
2022-11-07 14:08:49 -0500

    Add files via upload

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let addString = "";
+    for (let i = 0; i < num; i++) {
+        addString += string;
+    }
 
+    if (num < 0) {
+        return "ERROR";
+    } else{
+        return addString;  
+    }
+    
 };
 
 // Do not edit below this line
