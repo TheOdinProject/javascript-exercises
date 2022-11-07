dcbbe8a50edca972425a2cd1a9c433cb35d2be80
octopusGarden <zebraspider@protonmail.com>
2022-11-07 10:51:53 -0500

    Completed factorial and power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,28 +8,47 @@ const subtract = function(num1, num2) {
   return total;
 };
 
-const sum = function() {
+const sum = function(numList) {
 	  let total = 0;
 
-    for (i = 0; i < numList.length; i++) {
+    for (let i = 0; i < numList.length; i++) {
         total += parseInt(numList[i]);
     }
     
     return total;
 };
 
-const multiply = function(num1, num2) {
-  let total = num1 * num2;
+const multiply = function(numList) {
+  let total = 1;
+  
+  for (let i = 0; i < numList.length; i++) {
+    total *= parseInt(numList[i]);
+  }
+
   return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	//empty variable for total
+  let total = 1;
+  //for loop to repeat num by power
+  for (let i = 0; i < power; i++) {
+    //multiply each num with empty variable 
+    total *= num;
+  }
+  return total; 
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(num) {
+  //empty variable for total
+  let total = 1;
+  //for loop to continue until num is reached
+  for (let i = 1; i <= num; i++) {
+    //multiply total with the i variable, which starts at 1, and repeats until num 
+    total *= i;
+  }
+  return total; 
+  };
 
 // Do not edit below this line
 module.exports = {
