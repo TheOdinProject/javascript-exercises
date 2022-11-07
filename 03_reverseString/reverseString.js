6452976a23df67c329303d3bc6a67ad2929917e2
reddforman <edwardhamidov@aol.com>
2022-11-07 15:51:13 -0500

    Add files via upload

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const splitString = string.split("");
+    let reverse = "";
+
+    for (let i = (splitString.length - 1); i >= 0; i--) {
+        const pullString = splitString[i];
+        reverse += pullString;
+    }
+
+    return reverse;
 };
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString; 
