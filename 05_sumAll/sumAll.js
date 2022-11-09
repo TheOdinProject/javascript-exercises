f573853be9c0350ce41c7f2c5ae1f5a1a0dbb28e
reddforman <edwardhamidov@aol.com>
2022-11-09 07:20:44 -0800

    Add files via upload

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        smallNumber = Math.min(num1, num2);
+        bigNumber = Math.max(num1, num2);
+        sum = smallNumber;
+        for (let i = smallNumber; i <= bigNumber; i++) {
+            if (i > smallNumber) {
+                sum = sum + i;
+            }
+        }
+        if (sum >=0) {
+            return sum;
+        } else {
+            return "ERROR";
+        }
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
