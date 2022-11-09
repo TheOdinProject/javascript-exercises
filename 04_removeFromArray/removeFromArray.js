d6682bdb7235376d105ef2c52c35c481b0662989
reddforman <edwardhamidov@aol.com>
2022-11-08 20:00:29 -0500

    Add files via upload

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+/*
+const removeFromArray = function(array, number, ...moreNumber) {
+    const index = array.indexOf(number);
+    const removeArray = array.splice(index, 1);
+    return array;
 
 };
+*/
+
+const removeFromArray = function(array, ...moreNumber) {
+    for (let i = 0; i <= moreNumber.length; i++) {
+        const index = array.indexOf(moreNumber);
+        newArray = array.splice(index, 1);
+    }
+    
+    return array;
+
+};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
