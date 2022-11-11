cb46ea9e2d8bca9dc1f77b86f5738d1c66a4b3d6
reddforman <edwardhamidov@aol.com>
2022-11-11 08:17:09 -0800

    Add files via upload

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  celcius = ((f - 32) * 5) / 9;
+  return Math.round(celcius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  fahrenheit = ((c * 9) / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
