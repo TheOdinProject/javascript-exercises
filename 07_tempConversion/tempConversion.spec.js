374252c3026d7de9765962e29ebf688bdbc9ad49
Asartea <76259120+Asartea@users.noreply.github.com>
2022-11-12 20:49:28 +0100

    Update tempConversion function's naming

--- 07_tempConversion/tempConversion.spec.js
+++ 07_tempConversion/tempConversion.spec.js
@@ -1,25 +1,25 @@
-const {ftoc, ctof} = require('./tempConversion')
+const {convertToCelsius, convertToFahrenheit} = require('./tempConversion')
 
-describe('ftoc', () => {
+describe('convertToCelsius', () => {
   test('works', () => {
-    expect(ftoc(32)).toEqual(0);
+    expect(convertToCelsius(32)).toEqual(0);
   });
-  test.skip('rounds to 1 decimal', () => {
-    expect(ftoc(100)).toEqual(37.8);
+  test('rounds to 1 decimal', () => {
+    expect(convertToCelsius(100)).toEqual(37.8);
   });
-  test.skip('works with negatives', () => {
-    expect(ftoc(-100)).toEqual(-73.3);
+  test('works with negatives', () => {
+    expect(convertToCelsius(-100)).toEqual(-73.3);
   });
 });
 
-describe('ctof', () => {
-  test.skip('works', () => {
-    expect(ctof(0)).toEqual(32);
+describe('convertToFahrenheit', () => {
+  test('works', () => {
+    expect(convertToFahrenheit(0)).toEqual(32);
   });
-  test.skip('rounds to 1 decimal', () => {
-    expect(ctof(73.2)).toEqual(163.8);
+  test('rounds to 1 decimal', () => {
+    expect(convertToFahrenheit(73.2)).toEqual(163.8);
   });
-  test.skip('works with negatives', () => {
-    expect(ctof(-10)).toEqual(14);
+  test('works with negatives', () => {
+    expect(convertToFahrenheit(-10)).toEqual(14);
   });
 });
