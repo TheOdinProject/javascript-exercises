4366f7b039c0a3a59460108dfc847a061856392e
reddforman <edwardhamidov@aol.com>
2022-11-15 11:18:29 -0800

    Add files via upload

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g, '');
+    const array = Array.from(string);
+    let newArray = [];
 
+    for (let i = 1; i <= string.length; i++) {
+        newArray.push(string[string.length - i]);
+    }
+
+    let stringArray = (array.toString()).toLowerCase();
+    let stringNewArray = (newArray.toString()).toLowerCase();
+
+    if (stringArray === stringNewArray) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
