6046cee9cd16bc71ff16b114fad74b1ffd56f140
reddforman <edwardhamidov@aol.com>
2022-11-17 10:16:52 -0800

    Add files via upload

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    bookOne = books[0].title;
+    bookTwo = books[1].title;
 
-};
-
+    const bookArray = [bookOne, bookTwo]
+    return bookArray;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+/*
+const getTheTitles = function(array) {
+  return array.map(book => book.title);
+};
+*/
