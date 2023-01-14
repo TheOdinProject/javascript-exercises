4fba21edb12d59db913b5abbeacd530cf7620ba4
Julian So <37486854+julianchso@users.noreply.github.com>
2023-01-14 11:12:15 -0500

    Update getTheTitles.js
    added solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
-const getTheTitles = function() {
+const getTheTitles = function () {
+  const books = [
+    {
+      title: "Book",
+      author: "Name",
+    },
+    {
+      title: "Book2",
+      author: "Name2",
+    },
+  ];
 
+  let titles = [];
+
+  books.forEach((book) => {
+    titles.push(book.title);
+  });
+  return titles;
 };
 
 // Do not edit below this line
