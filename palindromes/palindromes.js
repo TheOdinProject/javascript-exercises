78c89e9fd4111219e7d57e2db972137d97b20bc3
Hifilo <92943726+Hifilo@users.noreply.github.com>
2023-01-17 13:02:49 -0500

    Update palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,27 +1,27 @@
-//This is a method using Regular Expressions, and is probably the easiest if you know how to use them
+//Regex Method
 
-// const palindromes = function (string) {
-// 	const processedString = string.toLowerCase().replace(/[^a-z]/g, '');
-// 	return processedString.split('').reverse().join('') == processedString;
-// };
-
-//there are many ways of checking the solution for palindromes without using regex. Here is one.
 const palindromes = function (string) {
-	let alphabet = 'abcdefghijklmnopqrstuvwxyz'; //create a variable that holds all the letters of the alphabet
-
-	//Take the input string, convert to lowercase, split, reverse, & filter only letters, before rejoining them together as the constant cleanedString.
-	const cleanedString = string
-		.toLowerCase()
-		.split('')
-		.filter((letter) => alphabet.includes(letter))
-		.join('');
-
-	//make a new const from the cleaned string, and reverse it.
-	const reversedString = cleanedString.split('').reverse().join('');
-
-	//we then finally compare cleanedString & reversedString which returns true/false
-	return cleanedString === reversedString;
+	const processedString = string.toLowerCase().replace(/[^a-z]/g, '');
+	return processedString.split('').reverse().join('') == processedString;
 };
+
+//Method without regex
+// const palindromes = function (string) {
+// 	let alphabet = 'abcdefghijklmnopqrstuvwxyz'; //create a variable that holds all the letters of the alphabet
+
+// 	//Take the input string, convert to lowercase, split, reverse, & filter only letters, before rejoining them together as the constant cleanedString.
+// 	const cleanedString = string
+// 		.toLowerCase()
+// 		.split('')
+// 		.filter((letter) => alphabet.includes(letter))
+// 		.join('');
+
+// 	//make a new const from the cleaned string, and reverse it.
+// 	const reversedString = cleanedString.split('').reverse().join('');
+
+// 	//we then finally compare cleanedString & reversedString which returns true/false
+// 	return cleanedString === reversedString;
+// };
 
 //check with console.log statements below in your editor/
 
