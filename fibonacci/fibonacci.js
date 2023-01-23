cc0badd69c2c27e3e2f7cc5237b8898b9936ae58
Florent-Vasseur <105013649+Florent-V@users.noreply.github.com>
2023-01-23 08:54:06 +0100

    Update fibonacci/fibonacci.js
    top !
    
    Co-authored-by: Eric Olkowski <70952936+thatblindgeye@users.noreply.github.com>

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -4,6 +4,9 @@ const fibonacci = function(count) {
   let a = 0;
   let b = 1;
   for (let i = 1; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
     // A slightly shorter version of the above solution:
     // [a, b] = [b, a + b];
   }
