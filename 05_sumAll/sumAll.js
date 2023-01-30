fb8d306f085532efe25c3b109a198fdec6ae7715
Jaycee Go <itsmejcgo@gmail.com>
2023-01-30 19:31:55 +0800

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    sum = 0;
 
+    if (start > end) {
+        for (let j = end; j <= start; j++) {
+            sum += j;
+        }
+    } else {
+        for (let j = start; j <= end; j++) {
+            sum += j;
+        }
+    }
+    return sum;
 };
 
+console.log(sumAll(-10, [1,4]));
+
 // Do not edit below this line
-module.exports = sumAll;
+// module.exports = sumAll;
