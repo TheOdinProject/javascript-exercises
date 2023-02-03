ca7b05549cad7e5010522c95aa311a6d97b71a51
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-03 15:08:55 +0100

    Write a function that determines whether or not a given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (input) {
+   let validate =/[\W_]/g;
+   let newInput = input.toLowerCase().replace(validate, "");
+   let reversedInput = newInput.split("").reverse().join("");
+   if (reversedInput == newInput) {
+     return true;
+   }else{  
+     return false;
+   }
+   };
 
-};
+
 
 // Do not edit below this line
 module.exports = palindromes;
