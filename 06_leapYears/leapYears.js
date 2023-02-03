4204350a10e89b71a33b304913d4550c1b44fb82
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-03 15:01:26 +0100

    Create a function that determines whether or not a given year is a leap year. Leap years are determined by the following rules

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(input) {
+if (input % 4 != 0) {
+    return false;
+}
+if(input % 4 == 0 && input % 400 == 0 || input % 100 != 0){
+    return true
+}
+if (input % 100 == 0 && input % 400 != 0) {
+    return false;
+}
 
 };
 
