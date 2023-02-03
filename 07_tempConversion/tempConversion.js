f40d0db982b685813e9a7ce947d6792798110ec7
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-03 15:02:35 +0100

    Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(input) {
+  let celcius = `${(input - 32) * 5/9}`;
+  let newNumber = parseFloat(Number(celcius).toFixed(1));
+  return newNumber;
+}
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(input) {
+  let fahrenheit = `${(input * 9/5) + 32}`;
+  let newFahrenheit = parseFloat(Number(fahrenheit).toFixed(1));
+  return newFahrenheit;
 };
 
 // Do not edit below this line
