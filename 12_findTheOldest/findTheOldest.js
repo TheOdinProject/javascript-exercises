bea7415d00877828cdba0a67c3adee818b164e9d
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-03 23:47:51 +0100

    Given an array of objects representing people with a birth and death year, return the oldest person.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(){ 
+    const people = [
+        {
+          name: "Carly",
+          yearOfBirth: 1942,
+          yearOfDeath: 1970,
+        },
+        {
+          name: "Ray",
+          yearOfBirth: 1962,
+          yearOfDeath: 2011,
+        },
+        {
+          name: "Jane",
+          yearOfBirth: 1912,
+          yearOfDeath: 1941,
+        },
+      ]
+        const age = (x) => (x.yearOfDeath || new Date().getFullYear()) - x.yearOfBirth;
+        const oldest = people.sort((a, b) =>
+        age(a) > age(b) ? -1 : 1
+        );
+        return oldest[0];
 
-};
+        
+    
+    }   
+      
+
+          
 
 // Do not edit below this line
 module.exports = findTheOldest;
