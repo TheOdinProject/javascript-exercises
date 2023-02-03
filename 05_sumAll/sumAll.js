fe6db15bb30c67aee49ff0da54349707dd2c87c8
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-03 14:56:42 +0100

    Implement a function that takes 2 integers and returns the sum of every number between(and including) them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end){
+  if(end<0 || start<0 || typeof(start) !== "number" || typeof(end) !== "number"){
+    return "ERROR"
+  }else{
+    if(end < start){
+      end1 = end
+      end2 = start
+    }else{
+      end1 = start
+      end2 = end
+    }
+    return (end1 + end2)*(end2/2)}
+  };
 
-};
+
+//   let total = 0;
+//   for (let i = start; i <= end; i++) {
+//    total = total + i;
+  
+//    }
+//    return total;
+// };
+ 
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
