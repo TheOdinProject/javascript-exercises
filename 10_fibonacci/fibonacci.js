ca453ad649c33bdbafe6fe5c393cfd05f991f0b3
Akutsang <mymac@MYs-MacBook-Pro.local>
2023-02-04 10:49:10 +0100

    Create a function that returns a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,24 @@
 const fibonacci = function(n) {
-    if (n <= 1) {
-        return Number(n);
+    if (n == 0) {
+     return Number(n);
     }
-      if (n < 0) {
-        return "opps"
-      }
+    if (n < 0) {
+     return "OOPS";
+    }
+    if (n == 1) {
+      return Number(n);
+    }
+      return fibonacci(n - 1) + fibonacci(n - 2)
+    // n = 3
+    // function(2) + function(1)
+    // function(1) + function(0) + function(0)  + function(-1)
+    // function(0) + function(-1) + 0 + 0 +  
     
-return fibonacci(n - 1) + fibonacci(n - 2)
+    // n = 1
+    // function(0) + function(-1)
+    // 0 + OPPS
+    
+
 
 };
 
