dbcd660b474c101dc8921f8ed482e87bc7a88744
Saddist <74913287+Saddist@users.noreply.github.com>
2023-04-05 23:03:25 +0530

    REPEAT STRING SOLVED

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num ===0){
+        return "";
+    }
+    else if(num<0){
+        return "ERROR";
+    }
+    else if(num ===1){
+        return string;
+    }
+    else {
+        let result = string;
+        while(num>1){
+            result = result+ string;
+            num--;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
