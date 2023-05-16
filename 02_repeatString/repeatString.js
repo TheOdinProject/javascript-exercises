42824f0cfe33d0cd89ab533537f673dffcc44fbf
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-16 22:00:44 +0200

    passed first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
 
-};
 
+const repeatString = function repeatString (string, num) {
+    let repeatedString = ''
+    for (let num = 0; num <=2; num ++) {
+      repeatedString += string;
+    }
+    return repeatedString;
+  }
+  
 // Do not edit below this line
 module.exports = repeatString;
