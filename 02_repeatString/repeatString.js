1ca489e68b567506ca2f8406c6fb80e19840612d
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-16 22:29:32 +0200

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,20 @@
 
 
 const repeatString = function repeatString (string, num) {
+    
+    if (num < 0) {return("ERROR")}
+    
     let repeatedString = ''
-    for (let num = 0; num <=2; num ++) {
+    
+    for (let i = 0; i < num; i ++) {
       repeatedString += string;
     }
     return repeatedString;
-  }
-  
+
+        }
+    
+
+
+
 // Do not edit below this line
 module.exports = repeatString;
