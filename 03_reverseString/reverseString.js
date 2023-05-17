fb96215bc67c6bd769f9fa3dfb68fc03a26a1915
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-17 22:00:23 +0200

    PASSED ALL TESTS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,17 +1,14 @@
 const reverseString = function(string) {
     let num = string.length;
     let lastLetter = string.slice(-1);
-    let secondLastLetter = string.slice(-2, -1);
-    let thirdLastLetter = string.slice(-3, -2);
-    let fourthLastLetter = string.slice(-4, -3);
-    let fifthLastLetter = string.slice(-5, -4);
+    for (let i = 1; i <= num; i++) {
 
-    /* for (let i = 0; i <= num; i++) { */
-        return(lastLetter + secondLastLetter + thirdLastLetter + fourthLastLetter + fifthLastLetter)
+        lastLetter += string.slice(((-i)-1), (-i));
 
+    }
+    return lastLetter;
 
-
-};
+}
 
 // Do not edit below this line
 module.exports = reverseString;
