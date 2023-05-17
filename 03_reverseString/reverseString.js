70985047e300a539d0e4d9109c8b810ca12f4c40
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-17 21:23:53 +0200

    passed first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let num = string.length;
+    let lastLetter = string.slice(-1);
+    let secondLastLetter = string.slice(-2, -1);
+    let thirdLastLetter = string.slice(-3, -2);
+    let fourthLastLetter = string.slice(-4, -3);
+    let fifthLastLetter = string.slice(-5, -4);
+
+    /* for (let i = 0; i <= num; i++) { */
+        return(lastLetter + secondLastLetter + thirdLastLetter + fourthLastLetter + fifthLastLetter)
+
+
 
 };
 
