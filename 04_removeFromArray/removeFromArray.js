7e377a42aa62949da2dbc7f73501f981508ef580
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-18 22:26:56 +0200

    pseudocode for next iteration

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -9,6 +9,13 @@ const removeFromArray = function([array], args) {
 
   };
 
+// NEXT need to find way to compare the two arrays
+// i.e. compare "array" and "args"
+// and check for duplicates..
+// and remove the duplicate from the first array
+// and finally return the new version of the first array
+// check for methods here: https://dev.to/soyleninjs/3-ways-to-remove-duplicates-in-an-array-in-javascript-259o
+
 
 // const removeFromArray = function() {};
 
