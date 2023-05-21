a15eafb5647e0bf501e75f407f491fa00530ac06
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-21 21:23:37 +0200

    passes 5/6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,23 @@
+// Need to figure out how to deal with opposite order (largest first)
+// and non-integer error with Number.isInteger()
+
 const sumAll = function(x, y) {
     let theSum;
   
         if (x < 0 || y < 0) {return('ERROR')}
+
+        else if (Number.isInteger(x) == false || Number.isInteger(y) == false) {return('ERROR')}
          
         else  
+
         {
 
         for (let i = x+1; i <=y; i++) {
         theSum = x += i 
-
-
-    
-      
-      }
+        }
+        
         return theSum
-    }
+        }
   
 };
 
