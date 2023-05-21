15d07a232b1024400f52ff79e67422a682748910
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-21 22:33:26 +0200

    passed all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,12 @@
 const leapYears = function(year) {
 
-    if (year % 4 == 0) {return true}
-    if (year % 4 != 0) {return false}
+    if (
+        year % 4 == 0 
+        && year % 100 != 0 
+        || year % 400 == 0
+        ) 
+        return true
+        else return false 
 };
 
 // Do not edit below this line
