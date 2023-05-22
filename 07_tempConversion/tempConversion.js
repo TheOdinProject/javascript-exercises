c06595fb3dc59e8684fa5ae0f9900dfd3d417de9
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-22 11:02:53 +0200

    passed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  let temp = ((fahrenheitInput - 32) * (5/9))
+  return Math.round(temp * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusInput) {
+  let temp = ((celciusInput * (9/5)) + 32)
+  return Math.round(temp * 10) / 10
+
 };
 
 // Do not edit below this line
