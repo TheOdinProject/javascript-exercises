c77a70db58a5aba745ebe7acbbbafbc4d61a310d
Roberra0 <roberra.aklilu@gmail.com>
2023-06-17 15:54:58 -0700

    Completed exercise with unit tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeater) {
+    if(repeater<0){
+        return "ERROR";
+    }
+    let repeated_string ='';
+    for ( i = 0; i < repeater; i++ ){
+        repeated_string += string;
+    }
+    return repeated_string;
 };
 
+// repeatString("hey",3);
+// console.log('hello');
 // Do not edit below this line
 module.exports = repeatString;
