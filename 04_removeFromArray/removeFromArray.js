3eb5d4e3ee526c0e8f4387f81d1fd3edc0e84d4a
Roberra0 <roberra.aklilu@gmail.com>
2023-06-18 11:57:02 -0700

    completed assignments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(localArray, ...args) {
+    for (let i of args){
+        indexRemove = localArray.indexOf(i);
+        if(indexRemove != -1){
+            localArray.splice(indexRemove,1);
+        }
+    }
+    return localArray;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+// Dont use summation formula
 
+const sumAll = function(start, end) {
+    let total = 0;
+    const min = Math.min(start, end);
+    const max = Math.max(start, end);
+
+    if (start < 0 || typeof start !== "number"){
+        return "ERROR";
+    }
+    if (end < 0 ||  typeof end !== "number"){
+        return "ERROR";
+    }
+
+    for(let i=0; i < max-min+1; i++){
+        total += min+i;
+    }
+    return total;
 };
 
+console.log(sumAll(10,"90"));
 // Do not edit below this line
 module.exports = sumAll;
