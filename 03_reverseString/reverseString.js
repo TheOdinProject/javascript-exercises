b7591b11ff0362344c16580ddba40e9ae06b26d7
Roberra0 <roberra.aklilu@gmail.com>
2023-06-18 10:28:26 -0700

    Completed exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,22 @@
-const reverseString = function() {
+// Take in a word
+// Split it into an array
+// Pop array and store in a new array
+// Continue until array is done
 
+
+const reverseString = function(word) {
+    // Split it into an array
+    const ogStringArray = word.split("");
+    const length_ogStringArray = ogStringArray.length;
+    const reverseStringArray = [];
+    // console.log(charArray);
+    for (let i=0; i < length_ogStringArray; i++){
+        reverseStringArray.push(ogStringArray.pop());
+    }
+    return(reverseStringArray.join(""));
 };
+
+// reverseString("Roberra Aklilu");
 
 // Do not edit below this line
 module.exports = reverseString;
