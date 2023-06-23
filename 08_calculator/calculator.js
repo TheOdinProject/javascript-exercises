02a649e3140ddbe648e1a26b0767c5c4445ad5b6
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 16:31:58 +1000

    tests: :white_check_mark: pass calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+	return numOne + numTwo
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+  return numOne - numTwo
 };
 
-const sum = function() {
-	
+const sum = function (arraySum) {
+  resultSum = 0
+  arraySum.forEach(number => resultSum += number);
+  return resultSum
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  resultMul = 1
+  arrayMul = [...args]
+  arrayMul.forEach(number => resultMul *= number)
+  return resultMul
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  powSum = 1
+  for (i = 0; i < pow; i++) {
+    powSum *= num
+  }
+  return powSum
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  factorialSum = 1; 
+
+  if (factorial === 0) {
+    return 1;
+  }
+  for (i = factorial; i > 0; i--) {
+    factorialSum *= i
+  }
+  return factorialSum
 };
 
 // Do not edit below this line
