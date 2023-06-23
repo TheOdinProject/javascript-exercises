b5b3a35ff251cd543fdeb2f3bc282ab4d4a93ae1
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 17:04:26 +1000

    tests: :white_check_mark: pass palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
+    "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+    normalString = Array.from(string.toLowerCase())
+        .filter(function (letter) {
+            return allLetters.includes(letter)
+        })
+        .join(",")
+    reverseString = Array.from(string.toLowerCase())
+        .filter(function (letter) {
+            return allLetters.includes(letter)
+        })
+        .reverse()
+        .join(",")
+    
+    console.log(normalString)
+    console.log(reverseString)
+    
+    
+    return normalString === reverseString ? true : false 
 };
 
 // Do not edit below this line
