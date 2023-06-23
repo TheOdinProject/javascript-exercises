7e9ec5f434384fe9ea8f59928bd1c496adcf2c8a
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 15:47:19 +1000

    tests :white_check_mark: pass removeArray, [solution looked]

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
 
+    /* 
+    - Array is used to filter over each number
+    - Each number is compared to the ...args Array
+    - If there is no match between Number and args Array, we push that number
+    [I checked solution: did not know includes excisted, and re-ording of it]
+    */
+
+    var newArray = [];
+    array.filter(function (number) {
+        if (!args.includes(number)) {
+            newArray.push(number)
+        }
+    })
+
+    return newArray
 };
 
 // Do not edit below this line
