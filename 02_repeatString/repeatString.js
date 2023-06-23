f5d662406d45d7e5282c0ba68a9dd70ec14f7d44
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 13:47:57 +1000

    tests :white_check_mark: pass repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
 
+    var localString = "";
+
+    for (i = 0; i < num; i++) {
+        localString = localString + string;
+    }
+    return localString;
 };
 
 // Do not edit below this line
