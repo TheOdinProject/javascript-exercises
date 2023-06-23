b151f4e87f5af8feb79ececd9caa5ddfabe4ae55
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 15:29:53 +1000

    tests :white_check_mark: pass sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+    
+    /* Checks */
+    if (typeof numOne !== "number" || numOne < 0 || typeof numTwo !== "number" || numTwo < 0) {
+        return "ERROR";
+    }
 
+    if (numOne > numTwo) {
+        var placeHold = numTwo
+        numTwo = numOne;
+        numOne = placeHold; 
+    }
+
+    var sum = 0;
+    for (i = numOne; i <= numTwo; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
