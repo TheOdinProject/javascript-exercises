80a1fe5c6cb8fb367e84e343c2ab4b191e1a5e84
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 13:58:53 +1000

    tests :white_check_mark: pass reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const newArray = Array.from(string).reverse();
+    var returnString = ""
 
+    for (i = 0; i < newArray.length; i++) {
+        returnString += newArray[i];
+    }
+
+
+    return returnString;
 };
 
 // Do not edit below this line
