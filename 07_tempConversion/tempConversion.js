1834e8d146790b5703ad78d8774f447664db81bf
abduldoesramen <abdulrehman821337@gmail.com>
2023-06-23 16:06:43 +1000

    tests :white_check_mark: pass tempConversions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenTemp) {
+  
+  /* Override Bodmas to match Conversion Formulas*/
+  const resultCelc = (fahrenTemp - 32) * 5 / 9
+  return Math.round(resultCelc * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcTemp) {
+  
+  const resultFahr = (celcTemp * 9 / 5 + 32)
+  return Math.round(resultFahr * 10) / 10
+
 };
 
 // Do not edit below this line
