454d897d0e5e7425962ac5389850a78bfb8f1501
Roberra0 <roberra.aklilu@gmail.com>
2023-07-06 15:37:00 -0700

    Completed calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = (nums)=>{
+  let total=0;
+	for(let i in nums){
+    total += nums[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = (...args) => {
+  let total=1;
+  let i=0;
+  for(let i in args){
+    // console.log("i: "+i+" args[i]: "+args[i]);
+    total *= args[i];
+    i++;
+  }
+  console.log(total);
+  return total;
 
 };
 
-const power = function() {
-	
+const power = (a,b)=> {
+	let total=1;
+  for(let i = 0; i<b; i++){
+    total *= a;
+  }
+  return total; 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let multplyNums =[];
+  for(i=a; i>0; i--){
+    multplyNums.push(i);
+  }
+  return(
+  multplyNums.reduce((result,num)=> result *= num,1));
 };
 
 // Do not edit below this line
