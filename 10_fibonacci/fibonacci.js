0e16261d324085eee2b45bd1dfb4932e95410941
abduldoesramen <abdulrehman821337@gmail.com>
2023-07-07 10:56:36 +1000

    :white_check_mark: fibonacci all test passing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (findNum) {
 
+    /* Perform checks to validate input */
+    if (findNum === 0 || findNum === 1) {
+        return 1;
+    } else if (findNum < 0) {
+        return "OOPS";
+    } else if (typeof findNum === "string") {
+        findNum = Number(findNum)
+    }
+
+    const fibArray = [1, 1];
+    for (i = 2; i < findNum; i++) {
+        fibArray.push(fibArray[i-1] + fibArray[i-2])
+    }
+    return fibArray[findNum - 1]
 };
 
 // Do not edit below this line
