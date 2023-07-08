d538ffeb27b58e6708daf0ca4b9bacc44fd8cc0e
Roberra0 <roberra.aklilu@gmail.com>
2023-07-07 19:15:27 -0700

    completed final exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people){
+    peopleSorted = people.sort((a, b)=>
+        (getAge(a) > getAge(b)) ?
+            -1:
+            1
+    );
+    return(peopleSorted[0]);
+}
 
-};
-
+function getAge(person){
+    let death = person.yearOfDeath;
+    if(!death){
+        death = new Date().getFullYear();
+    };
+    return death - person.yearOfBirth;
+}
 // Do not edit below this line
 module.exports = findTheOldest;
