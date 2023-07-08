30ac06d4097ee1b556e4d2ff328f9885921d448f
Roberra0 <roberra.aklilu@gmail.com>
2023-07-07 18:33:58 -0700

    Title exercise complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+   return books.map(book=>book.title);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
