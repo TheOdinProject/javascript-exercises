f5f6efae9bf53a57db07544b8068b9c8a02d1955
Will <59489624+cats256@users.noreply.github.com>
2023-07-29 15:40:07 -0500

    Update sumAll-solution.js

--- 05_sumAll/solution/sumAll-solution.js
+++ 05_sumAll/solution/sumAll-solution.js
@@ -1,7 +1,7 @@
 const sumAll = function (min, max) {
   if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
   if (min < 0 || max < 0) return "ERROR";
-    if (min > max) {
+  if (min > max) {
     const temp = min;
     min = max;
     max = temp;
