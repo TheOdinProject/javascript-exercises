9ae464672a5b7502bd126d2d7a854e1f49c6b29e
Archybella <128797928+Archybella@users.noreply.github.com>
2023-08-08 15:18:17 +0300

    Update repeatString.js
    repeat string function completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+  let result = '';
+  for (let i = 0; i < times; i++) {
+    result += word;
+  }
+  return result;
 };
 
+// Test the repeatString function
+const repeatedWord = repeatString('apple', 3);
+console.log(repeatedWord); // Output: "appleappleapple"
 // Do not edit below this line
 module.exports = repeatString;
