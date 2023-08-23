e46b84fd0fc5313e02851ced73c685f52a566879
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-23 21:03:03 +0530

    test 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 
+    //solution 2 :
+    // let array = string.split("");
+    // let n = string.length;
+    // for(let i = 0 ;i < n/2 ;i++){
+    //     let temp = array[n - 1 - i];
+    //     array[n - 1 - i] = array[i];
+    //     array[i] = temp;
+    // }
+    // return array.join('');
+
+    // solution 3 :
+    // let array = string.split("");
+    // let n = string.length;
+    // let res = "";
+    // for(let i = n - 1 ; i >= 0 ;i--){
+    //     res += array[i];
+    // }
+    // return res;
 };
 
 // Do not edit below this line
