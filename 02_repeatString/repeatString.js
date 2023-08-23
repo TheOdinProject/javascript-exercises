d8849236862c15a68dcdb69c61d368af6bd13342
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-23 20:39:18 +0530

    test 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text , num) {
+ if(num < 0) return "ERROR";
+ let res = "";
+ for(let i = 1 ;i <= num ; i++){
+    res += text;
+ }
+ return res;
 };
 
 // Do not edit below this line
