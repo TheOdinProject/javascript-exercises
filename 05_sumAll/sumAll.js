034caa5dbcae64e47b2c4637b2255452343ae536
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-24 13:11:15 +0530

    test 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(n1 , n2) {
+    let a = 1;
+    if(n1 < 0 || n2 < 0 || typeof(n1) != typeof(a) || typeof(n2) != typeof(a))
+        return 'ERROR';
+    let sum = 0
+    if(n2 < n1){
+        let temp = n1;
+        n1 = n2;
+        n2 = temp;
+    }
+    while(n1 <= n2){
+        sum += n1;
+        n1++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
