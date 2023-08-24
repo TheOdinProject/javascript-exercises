abf474e1fc2e88b30c955c46e93f36bdde665604
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-24 13:22:32 +0530

    test 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 === 0 && (year % 100 != 0 || year % 400 === 0);
+    // if(year % 4 == 0){
+    //     if(year % 25 == 0){
+    //         if(year % 400 == 0) return true;
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    // return false;
 };
 
 // Do not edit below this line
