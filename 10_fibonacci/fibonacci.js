e5a927b7eb3e6cdf9907367f60140603995d7b17
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-25 11:17:22 +0530

    test 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    if(n === 0) return 0;
+    if(n <= 2) return 1;
+    let myArray = new Array(n);
+    myArray[0] = 1;
+    myArray[1] = 1;
+    for(let i = 2 ; i < n ;i++){
+        myArray[i] = myArray[i - 1] + myArray[i - 2];
+    }
+    return myArray[n - 1];
 };
 
 // Do not edit below this line
