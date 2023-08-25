1fd8505b2623ef1327f7d833403b1cedd7209392
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-25 11:53:10 +0530

    test 12 completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let res;
+    let maxAge = 0;
+    let n = people.length;
+    for(let i = 0 ;i < n ; i++){
+        let birth = people[i].yearOfBirth;
+        let death;
+        if(people[i].yearOfDeath ==  undefined){
+            const d = new Date() ;
+            death =  d.getFullYear();
+        }
+        else {
+            death = people[i].yearOfDeath;
+        }
+        let age = death - birth;
+        if(age > maxAge){
+            maxAge = age;
+            res = people[i];
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
