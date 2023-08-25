62ea370b9423ee48ad589a61888de0c021cc9818
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-25 11:31:56 +0530

    test 11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    //Solution 1:
+    //return books.map((book) => book.title);
 
+    // Solution 3:
+    return books.map(myFunction);
+    function myFunction(book){
+        return book.title;
+    }
+
+    // Solution 2:
+    // let res = [];
+    // let n = books.length;
+    // for(let i = 0 ; i< n ;i++){
+    //     res[i] = books[i].title;
+    // }
+    // return res;
 };
 
 // Do not edit below this line
