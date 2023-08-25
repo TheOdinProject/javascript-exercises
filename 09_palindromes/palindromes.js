799fe56a92cbb3b08d5dd6f714619611b44a43df
rajroushan6714 <rajubhai6714x@gmail.com>
2023-08-25 11:02:55 +0530

    test 9 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // replace(str1 , str2) replaces all str1 with str2.
+    let processedString = string.toLowerCase().replace(/[^a-z0-9]/g,"");
+    return processedString == processedString.split('').reverse().join('');
 
+    // Solution 2 : 
+
+    // let str = string.toLowerCase();
+    // let myArray = str.split('');
+    // let temp = "";
+    // let n = myArray.length;
+    // for(let i = 0 ;i < n ;i++){
+    //     if(myArray[i]>= 'a' && myArray[i] <= 'z' || myArray[i] <= '9' && myArray[i] >= '0'){
+    //         temp += myArray[i];
+    //     }
+    // }
+    // return temp == temp.split('').reverse().join('');
 };
 
 // Do not edit below this line
