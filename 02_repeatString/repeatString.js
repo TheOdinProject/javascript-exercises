0e8b5f3db33b7bf09c9155b3021adc6d7e2e06c8
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-10-11 09:08:20 +1300

    exercise 01 and 02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+	if (num < 0) return 'ERROR';
 
+	let phrase = '';
+	for (let i = 0; i < num; i++) {
+		phrase += string;
+	}
+	return phrase;
 };
 
 // Do not edit below this line
