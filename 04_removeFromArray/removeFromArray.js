e974761c99aa947392a85402e36de37f64a7365b
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-10-11 05:41:57 +0000

    Completed exercises 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const newArr = [];
 
+    arr.forEach((item) => {
+        if(!args.includes(item)) {
+            newArr.push(item);
+        }
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    const arr = [];
 
+    if(!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR';
+    if(num1 < 0 || num2 < 0) return 'ERROR';
+
+    for(let i = 1; i <= Math.max(num1, num2); i++) {
+        arr.push(i);
+    }
+
+    arr.forEach(num => {
+        sum += num;
+    })
+
+    return sum;
 };
 
 // Do not edit below this line
