4741f6b23cb8ba91ae226372f796b3cf0004b302
terga17 <terglav10@gmail.com>
2023-10-12 22:16:03 +0200

    Koncan do 7 vaje

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    let sum = 0;
+    if(from > to){
+        [from, to] = [to, from];
+    }
+    if(from < 0 || to < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(from) || !Number.isInteger(to)){
+        return "ERROR";
+    }    
+        for(from; from <= to; from++){
+            sum += from;
+        }
+    return sum;
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 };
+
+// Leap years are years divisible by four (like 1984 and 2004). However, 
+//years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
 
 // Do not edit below this line
 module.exports = leapYears;
