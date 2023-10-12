9a171b12c88242e8e03c077943876caee37ca98e
okoro <mi.fracri91@gmail.com>
2023-10-12 15:21:28 +0100

    passed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(allArr, ...removeArr) {
+    let totalArr = [];
+for (const arr of allArr){
+    if (removeArr.includes(arr) ){
+        continue;
+    } 
+      totalArr.push(arr);
+}
+return totalArr;
 };
 
 // Do not edit below this line
