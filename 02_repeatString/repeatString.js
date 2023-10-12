400cff7caa1eeee42471ae69778b09a6546abf90
okoro <mi.fracri91@gmail.com>
2023-10-12 10:13:27 +0100

    passed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let str = "";
+for (let i = 0; i < num; i++){
+    str += string;
+}
+return str;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
