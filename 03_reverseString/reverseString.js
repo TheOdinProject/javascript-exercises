6797f0c8582133e66fbcc4a95af30472826ad6e7
okoro <mi.fracri91@gmail.com>
2023-10-12 10:43:22 +0100

    passed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let splitString = string.split("");
+let revString = splitString.reverse();
+let joinString = revString.join("");
+return joinString;
 };
 
 // Do not edit below this line
