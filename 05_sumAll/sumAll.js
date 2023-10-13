8aff243c3bd43936d94bce43a6a24e8eb4319781
okoro <mi.fracri91@gmail.com>
2023-10-13 01:06:05 +0100

    passed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
+    let totalSum = 0;
+    if (num1 < num2) {
+        let i = num1;
+        while (i <= num2) {
+            totalSum += i;
+            i++;
+        }
+    } else {
+        let i = num2;
+        while (i <= num1) {
+            totalSum += i;
+            i++;
+        }
+    }
+    return totalSum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
