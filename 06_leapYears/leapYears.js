7466d836586890783d53d5dcc014abd8d28f8268
okoro <mi.fracri91@gmail.com>
2023-10-14 02:58:13 +0100

    passed exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+if (year % 4 === 0){
+    if(year % 100 === 0 && year % 400 === 0){
+        return true
+    }else if (year % 4 === 0 && year % 100 !== 0){
+        return true
+    }else{
+        return false
+    }
+}else{
+    return false
+}
 };
 
 // Do not edit below this line
