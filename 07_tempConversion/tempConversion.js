217e8f985d5fa907f915488204c157ba79690512
okoro <mi.fracri91@gmail.com>
2023-10-14 07:49:56 +0100

    passed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+   let celsius = (fahrenheit - 32) * 5 / 9;
+   return celsius = Number(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+let fahrenheit = (celsius * (9 / 5)) + 32;
+return fahrenheit = Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
