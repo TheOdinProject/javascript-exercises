33e627c4ba0d050c715eedee93f571d1fb24a204
MortezaTavasoly <vector.1475225963@gmail.com>
2023-10-23 05:06:00 -0800

    second commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  if (number === 0) return 0;
 
+  let prev1 = 1;
+  let prev2 = 0;
+
+  for (let i = 2; i <= number; i++) {
+    let current = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = current;
+  }
+
+  return prev1;
 };
 
 // Do not edit below this line
