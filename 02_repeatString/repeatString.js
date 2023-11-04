1e55ec634bc0764e9c3b178389f587e2ac28d395
Eric Holland <hollanderic@yahoo.com>
2023-11-03 21:40:38 -0400

    Finished second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    output_str = '';
+    for (i = 0; i < repeats; i++) {
+        output_str = output_str + string;
+    }
+    return output_str;
 };
 
 // Do not edit below this line
