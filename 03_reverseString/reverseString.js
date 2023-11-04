88fc7f3aed2379f16cc9a285629b5b68a0b88b90
Eric Holland <hollanderic@yahoo.com>
2023-11-03 21:50:06 -0400

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    idxLenString = string.length - 1;
+    new_str = '';
+    for (i = idxLenString; i >= 0; i--) {
+        new_str = new_str + string[i]
+    }
+    return new_str;
 };
 
 // Do not edit below this line
