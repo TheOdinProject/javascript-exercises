d13b907660d443bb2641748c0e670021ffb3c566
Eric Holland <hollanderic@yahoo.com>
2023-11-04 20:26:28 -0400

    Added html file to debug in browser console

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const removalChecks = [];
+    for (i=0; i<arguments.length; ++i) {
+        if (i !== 0) {
+            removalChecks.push(arguments[i]);
+        }
+    }
+    console.log(removalChecks)
+    lenArray = array.length-1;
+    for (i=0; i<removalChecks.length; i++) {
+        valToCheck = removalChecks[i]
+        for (i=lenArray; i>=0; --i) {
+            if (array[i] == valToCheck) {
+                array.splice(i, 1)
+            }
+        }
+    }
 
+    return array
 };
 
 // Do not edit below this line
