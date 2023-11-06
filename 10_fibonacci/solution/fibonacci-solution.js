908c4ed26ea00142da5e1a2f2a2f8e58c721d0da
Luis Leiva <103515231+Luislev@users.noreply.github.com>
2023-11-05 23:12:41 -0500

    Update fibonacci-solution.js

--- 10_fibonacci/solution/fibonacci-solution.js
+++ 10_fibonacci/solution/fibonacci-solution.js
@@ -1,17 +1,12 @@
 const fibonacci = function(count) {
     if (count < 0) return "OOPS";
     if (count === 0) return 0;
-
-    let firstPrev = 1;
-    let secondPrev = 0;
     
+    const fib = [0, 1];
     for (let i = 2; i <= count; i++) {
-        let current = firstPrev + secondPrev;
-        secondPrev = firstPrev;
-        firstPrev = current;
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
-
-    return firstPrev;
+    return fib[count];
 };
 
 module.exports = fibonacci;
