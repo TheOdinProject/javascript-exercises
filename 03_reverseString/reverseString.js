b887bfcd10b80949ef36a877123c4f3a0369baf2
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-11-09 19:01:06 +0800

    Resolve failing tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    // approach 1.
+    return input.split('').reverse().join('');
+};
 
+const reverseStringFaster = function(input) {
+    // approach 2.
+    let result = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        result += input[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
