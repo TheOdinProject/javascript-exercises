e7847b0ac16034baffbda4dea19ec812d9181df8
Max Hillebrand <max.hillebrand@yahoo.de>
2023-11-17 08:59:10 +0100

    solves 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let addedStrings = "";
 
+  if (num < 0) {
+    return (addedStrings = "ERROR");
+  } else {
+    for (num; num > 0; num--) {
+      addedStrings += string;
+    }
+  }
+  return addedStrings;
 };
 
 // Do not edit below this line
