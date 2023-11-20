154c9e9f91a4323da09735d931c4edaeb2479a38
okoro <mi.fracri91@gmail.com>
2023-11-20 16:39:09 +0100

    exercise 10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(nums) {
+if(nums <= 0) return "OOPS";
 
+let firstcount = 1;
+let secondcount = 0;
+
+for(let i = 2; i <= nums; i++){
+    let current = firstcount + secondcount;
+    secondcount = firstcount;
+    firstcount =  current;
+}
+
+return firstcount;
 };
 
 // Do not edit below this line
