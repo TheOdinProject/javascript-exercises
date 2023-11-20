fce60582f9f7f3659774b81e458182ea28b547c6
okoro <mi.fracri91@gmail.com>
2023-11-20 16:05:30 +0100

    completed exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const changedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+return changedString.split("").reverse().join("") == changedString;
 };
 
 // Do not edit below this line
