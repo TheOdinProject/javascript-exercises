9cf9fcff1e55e285ddbab899b252a8405309b684
okoro <mi.fracri91@gmail.com>
2023-11-20 19:41:09 +0100

    all task completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+        const oldestPerson = people.reduce((oldest, currentPerson) => {
+          const currentAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : new Date().getFullYear() - currentPerson.yearOfBirth;
+          const oldestAge = oldest ? (oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth) : -Infinity;
+      
+          return currentAge > oldestAge ? currentPerson : oldest;
+        }, null);
+      
+        return oldestPerson;
+      
 };
 
 // Do not edit below this line
