1dce6620191d591491b1028760356aae559a1498
Adelina Moroaca <moroacaadelina@gmail.com>
2023-12-23 16:12:05 +0200

    Add palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+    let strL = str.toString().toLowerCase().toUpperCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/[\W_]+/g, "");
+     let newStr = '';
+    for(i = strL.length; i >= 0; i--){
+        newStr += strL.charAt(i);
+        // console.log(newStr)
+    }
+    if (strL === newStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+palindromes('lid off a daffodil') // true
+palindromes('tacos') // false
 // Do not edit below this line
 module.exports = palindromes;
