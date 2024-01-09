4045f847e53dcfe647663d453aef2f23ea696e58
Yago <yagovr.rocha@gmail.com>
2024-01-09 00:17:44 -0300

    Excercise 05 OK!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0 ) return "ERROR";
+    if (min > max){
+        const i = min;
+        min = max;
+        max = i;
+    }
 
+    let sum = 0;
+    for (let i = min; i <= max; i++ ){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
