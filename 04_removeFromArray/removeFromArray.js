fe113303557212a1f45f99e44d2055386b6e88ab
Yago <yagovr.rocha@gmail.com>
2024-01-09 00:24:27 -0300

    04 Exercise OK!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+    return array.filter(number => !args.includes(number));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
