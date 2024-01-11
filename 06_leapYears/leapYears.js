dce986af4757eb02afbb129fcf62c7000fb26e17
Yago <yagovr.rocha@gmail.com>
2024-01-10 23:53:27 -0300

    Excercise 06 OK!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if ((years % 4 == 0 && years % 100 != 0) || (years % 4 == 0 && years % 400 == 0)){
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
