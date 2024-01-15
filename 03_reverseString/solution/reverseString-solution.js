dc2b23b0fefbe9681cc30c645b79bcc1a62e45ee
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2024-01-15 10:28:13 +0800

    Add alternative solution for learner's consideration

--- 03_reverseString/solution/reverseString-solution.js
+++ 03_reverseString/solution/reverseString-solution.js
@@ -2,4 +2,15 @@ const reverseString = function (string) {
   return string.split("").reverse().join("");
 };
 
+// alternatively, you may consider the following solution 
+// that uses only basic syntax as oppose to string methods.
+
+// const reverseString = function(input) {
+//   let result = "";
+//   for (let i = input.length - 1; i >= 0; i--) {
+//       result += input[i];
+//   }
+//   return result;
+// };
+
 module.exports = reverseString;
