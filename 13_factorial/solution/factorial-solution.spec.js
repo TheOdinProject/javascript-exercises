ee05ca925aa60a5472f4f15bd1c75e30ae428197
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-22 18:58:48 +0000

    feat| spec file fixed

--- 13_factorial/solution/factorial-solution.spec.js
+++ 13_factorial/solution/factorial-solution.spec.js
@@ -4,37 +4,37 @@ describe('factorial', () => {
   test('4th factorial number is 24', () => {
     expect(factorial(4)).toBe(24);
   });
-  test.skip('6th factorial number is 720', () => {
+  test('6th factorial number is 720', () => {
     expect(factorial(6)).toBe(720);
   });
-  test.skip('10th factorial number is 3628800', () => {
+  test('10th factorial number is 3628800', () => {
     expect(factorial(10)).toBe(3628800);
   });
-  test.skip('15th factorial number is 1.3076744e+12', () => {
-    expect(factorial(15)).toBe(1.3076744e12);
+  test('15th factorial number is 1307674368000', () => {
+    expect(factorial(15)).toBe(1307674368000);
   });
-  test.skip('25th factorial number is 1.551121e+25', () => {
-    expect(factorial(25)).toBe(1.551121e25);
+  test('25th factorial number is 1.5511210043330986e+255', () => {
+    expect(factorial(25)).toBe(1.5511210043330986e+25);
   });
-  test.skip('0th factorial number is 1', () => {
+  test('0th factorial number is 1', () => {
     expect(factorial(0)).toBe(1);
   });
-  test.skip('doesn\'t accept negatives', () => {
+  test('doesn\'t accept negatives', () => {
     expect(factorial(-25)).toBe(undefined);
   });
-  test.skip('doesn\'t accept floats', () => {
+  test('doesn\'t accept floats', () => {
     expect(factorial(5.4)).toBe(undefined);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(factorial("0")).toBe(1);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(factorial("1")).toBe(1);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(factorial("2")).toBe(2);
   });
-  test.skip('DOES accept strings', () => {
+  test('DOES accept strings', () => {
     expect(factorial("8")).toBe(40320);
   });
 });
