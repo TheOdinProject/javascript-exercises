897b2c167afdbcec7f491ac3f059ebd27b8ee2ed
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-23 19:21:46 +0000

    feat/ create tests

--- 14_contains/solution/contains-solution.spec.js
+++ 14_contains/solution/contains-solution.spec.js
@@ -1,15 +1,45 @@
 const contains = require('./contains-solution');
 
 describe('contains', () => {
-  test('First test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
+  const nestedObject = {
+    data: {
+      banana: NaN,
+      info: {
+          magicNumber: 44,
+          stuff: {
+              thing: {
+                banana: NaN,
+                moreStuff: {
+                    something: 'foo',
+                    answer: [42]
+                }
+              }
+          }
+      }
+    }
+  }
 
-    expect(contains()).toBe('');
+  test('true if property is number', () => {
+    expect(contains(nestedObject, 44)).toBe(true);
   });
-  
-  test('Second test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
 
-    expect(contains()).toBe('');
+  test('true if property is string', () => {
+    expect(contains(nestedObject, 'foo')).toBe(true);
+  });
+
+  test('does not do type conversions', () => {
+    expect(contains(nestedObject, '44')).toBe(false);
+  });
+
+  test('false if property is not in object', () => {
+    expect(contains(nestedObject, 'bar')).toBe(false);
+  });
+
+  test('true if property is duplicated', () => {
+    expect(contains(nestedObject, NaN)).toBe(true);
+  });
+
+  test('false if property exists but is not primitive', () => {
+    expect(contains(nestedObject, [42])).toBe(false);
   });
 });
