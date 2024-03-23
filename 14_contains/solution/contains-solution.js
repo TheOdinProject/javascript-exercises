59c67684456cebb409f87379c71cd225cbaa4a1e
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-23 19:22:29 +0000

    feat/ create solution

--- 14_contains/solution/contains-solution.js
+++ 14_contains/solution/contains-solution.js
@@ -1,5 +1,11 @@
-const contains = function() {
-  // Replace this comment with the solution code
+const contains = function(current, search) {
+  if (Object.values(current).includes(search)) return true;
+
+  for (const nestedObject of Object.values(current).filter((value) => typeof value === 'object')) {
+    return contains(nestedObject, search)
+  }
+  
+  return false
 };
   
 // Do not edit below this line
