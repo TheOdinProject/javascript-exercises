7146b0ddd179b1316e3e47c0e613a7e12e0a95fa
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-24 10:49:21 +0000

    feat/ improve wording

--- 14_contains/solution/contains-solution.spec.js
+++ 14_contains/solution/contains-solution.spec.js
@@ -3,8 +3,9 @@ const contains = require('./contains-solution');
 describe('contains', () => {
   const object = {
     data: {
-      banana: NaN,
+      duplicate: 'e',
       info: {
+          duplicate: 'e',
           magicNumber: 44,
           stuff: {
               thing: {
@@ -19,27 +20,31 @@ describe('contains', () => {
     }
   }
 
-  test('true if property is number', () => {
-    expect(contains(nestedObject, 44)).toBe(true);
+  test('true if the provided number is a value within the object', () => {
+    expect(contains(object, 44)).toBe(true);
   });
 
-  test('true if property is string', () => {
-    expect(contains(nestedObject, 'foo')).toBe(true);
+  test('true if the provided string is a value within the object', () => {
+    expect(contains(object, 'foo')).toBe(true);
   });
 
-  test('does not do type conversions', () => {
-    expect(contains(nestedObject, '44')).toBe(false);
+  test('does not convert input string into a number when searching for a value within the object', () => {
+    expect(contains(object, '44')).toBe(false);
   });
 
-  test('false if property is not in object', () => {
-    expect(contains(nestedObject, 'bar')).toBe(false);
+  test('false if the provided string is not a value within the object', () => {
+    expect(contains(object, 'bar')).toBe(false);
   });
 
-  test('true if property is duplicated', () => {
-    expect(contains(nestedObject, NaN)).toBe(true);
+  test('true if the provided string is duplicated', () => {
+    expect(contains(object, 'e')).toBe(true);
   });
 
-  test('false if property exists but is not primitive', () => {
-    expect(contains(nestedObject, [42])).toBe(false);
+  test('false if the provided property exists but is not a primitive', () => {
+    expect(contains(object, [42])).toBe(false);
+  });
+
+  test('true if NaN is a property within the object', () => {
+    expect(contains(object, NaN)).toBe(true);
   });
 });
