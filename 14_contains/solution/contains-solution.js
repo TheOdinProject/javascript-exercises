c19c1bc2bf243db001b27ce30b3f9f94b4564cf7
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-24 12:39:47 +0000

    feat/ clarify that we would normally use math.isnan

--- 14_contains/solution/contains-solution.js
+++ 14_contains/solution/contains-solution.js
@@ -1,7 +1,7 @@
 const contains = function(obj, searchValue) {
   const values = Object.values(obj);
 
-  // NaN !== NaN, so we would usually have to do an explicit check to test for it. However, Array.prototype.includes handles that for us
+  // NaN !== NaN, so we would usually have to do an explicit check with Math.isNaN to test for it. However, Array.prototype.includes handles that for us
   if (values.includes(searchValue)) return true;
 
   const nestedObjects = values.filter((value) => typeof value === 'object');
