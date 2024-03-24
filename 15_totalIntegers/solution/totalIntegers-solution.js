99f8a40bc7a22b6ccbffde3613f1b7b2cf93def5
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-24 17:17:24 +0000

    fix/ semicolon

--- 15_totalIntegers/solution/totalIntegers-solution.js
+++ 15_totalIntegers/solution/totalIntegers-solution.js
@@ -2,7 +2,7 @@ const totalIntegers = function(obj, count = 0) {
 
   if (typeof obj !== 'object' || obj === null) return;
 
-  const elements = Array.isArray(obj) ? obj : Object.values(obj)
+  const elements = Array.isArray(obj) ? obj : Object.values(obj);
 
   for (const el of elements) {
     if (Number.isInteger(el)) ++count;
