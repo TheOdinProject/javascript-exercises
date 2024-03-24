73a0f10ec424e85b326f4d27a65a1d9e126db3a8
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-24 14:27:44 +0000

    feat/ add solution

--- 15_totalIntegers/solution/totalIntegers-solution.js
+++ 15_totalIntegers/solution/totalIntegers-solution.js
@@ -1,6 +1,22 @@
-const totalIntegers = function() {
-  // Replace this comment with the solution code
+const isInteger = (number) => {
+  switch (true) {
+    case (Number.isNaN(number)):
+    case (typeof number !== 'number'):
+    case (number % 1 !== 0):
+      return false;
+    default:
+      return true;
+  }
 };
-  
+
+const totalIntegers = function(array, count = 0) {
+  if (!Array.isArray(array)) return;
+  for (const el of array) {
+    if (isInteger(el)) ++count;
+    if (Array.isArray(el)) count += totalIntegers(el);
+  }
+  return count;
+};
+
 // Do not edit below this line
 module.exports = totalIntegers;
