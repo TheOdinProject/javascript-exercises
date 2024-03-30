fc2fc72c1e4680af73fb31b7ead052c4fb4da474
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-30 16:08:01 +0000

    feat: remove mention of IEEE

--- 14_contains/solution/contains-solution.js
+++ 14_contains/solution/contains-solution.js
@@ -1,18 +1,20 @@
-const contains = function(obj, searchValue) {
+const contains = function (obj, searchValue) {
   const values = Object.values(obj);
 
-  // NaN === NaN evaluates to false (as per the IEEE 754 standard)
+  // NaN === NaN evaluates to false
   // Normally, we would have to do an explicit Number.isNaN() check to compare NaN equality
   // However, Array.prototype.includes automatically handles this for us
   if (values.includes(searchValue)) return true;
 
-  const nestedObjects = values.filter((value) => typeof value === 'object' && value !== null);
+  const nestedObjects = values.filter(
+    (value) => typeof value === "object" && value !== null,
+  );
   for (const nestedObject of nestedObjects) {
     return contains(nestedObject, searchValue);
   }
 
   return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
