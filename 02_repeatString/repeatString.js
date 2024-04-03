974ae17ac4ef518cec61c5e3453a73d1274e735e
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-03 15:14:23 -0400

    02 exercise Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num >= 0) {
+        let world = '';
+        for (let index = 0; index < num; index++) {
+            world += str  
+        }
+        return world
+    } else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
