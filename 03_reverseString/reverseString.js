67021dc3a4136bb4eba09bab19a257f7208e4ace
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-03 16:22:53 -0400

    03 exercise Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(world) {
+    let worldSplit = world.split('');
+    let worldReverseSplit = worldSplit.reverse();
+    let worldReverse = worldReverseSplit.join('')
+    return worldReverse
 };
 
 // Do not edit below this line
