01d2ec5699f89179aef4a67a37a39413446fd10d
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-04-04 18:07:01 +0100

    feat: create tests for solution

--- 16_permutations/solution/permutations-solution.spec.js
+++ 16_permutations/solution/permutations-solution.spec.js
@@ -1,15 +1,82 @@
 const permutations = require('./permutations-solution');
 
 describe('permutations', () => {
-  test('First test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
+  function outcome(input, expected) {
+    const actual = permutations(input);
 
-    expect(permutations()).toBe('');
+    // Convert both arrays to strings to compare them later, excluding the order the arrays elements are in
+
+    const sterilise = (input) =>
+      input
+        .map((el) => el.toString())
+        .toSorted()
+        .toString();
+
+    return [sterilise(actual), sterilise(expected)];
+  }
+
+  let actual, expected;
+
+  afterEach(() => {
+    expect(actual).toBe(expected);
   });
-  
-  test('Second test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
 
-    expect(permutations()).toBe('');
+  test('Works for array of size one', () => {
+    [actual, expected] = outcome([1], [1]);
+  });
+  test('Works for array of size two', () => {
+    [actual, expected] = outcome(
+      [1, 2],
+      [
+        [1, 2],
+        [2, 1],
+      ],
+    );
+  });
+  test('Works for array of size three', () => {
+    [actual, expected] = outcome(
+      [1, 2, 3],
+      [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1],
+      ],
+    );
+  });
+  test('Works for array of size four', () => {
+    [actual, expected] = outcome(
+      [1, 2, 3, 4],
+      [
+        [
+          [1, 2, 3, 4],
+          [1, 2, 4, 3],
+          [1, 3, 2, 4],
+          [1, 3, 4, 2],
+          [1, 4, 2, 3],
+          [1, 4, 3, 2],
+          [2, 1, 3, 4],
+          [2, 1, 4, 3],
+          [2, 3, 1, 4],
+          [2, 3, 4, 1],
+          [2, 4, 1, 3],
+          [2, 4, 3, 1],
+          [3, 1, 2, 4],
+          [3, 1, 4, 2],
+          [3, 2, 1, 4],
+          [3, 2, 4, 1],
+          [3, 4, 1, 2],
+          [3, 4, 2, 1],
+          [4, 1, 2, 3],
+          [4, 1, 3, 2],
+          [4, 2, 1, 3],
+          [4, 2, 3, 1],
+          [4, 3, 1, 2],
+          [4, 3, 2, 1],
+        ],
+      ],
+    );
   });
 });
