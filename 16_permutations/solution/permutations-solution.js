1eebb5d6d6cd2ab8978f7be6ae4e299d7ba72d1c
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-04-04 18:07:11 +0100

    feat: create solution to exercise

--- 16_permutations/solution/permutations-solution.js
+++ 16_permutations/solution/permutations-solution.js
@@ -1,6 +1,19 @@
-const permutations = function() {
-  // Replace this comment with the solution code
+const permutations = function (original, currentPermutations) {
+  if (original.length === 1) return original;
+
+  const perms = currentPermutations
+    ? currentPermutations
+    : original.map((el) => [el]);
+
+  const newPerms = [];
+  perms.forEach((el) => {
+    const missing = original.filter((item) => !el.includes(item));
+    missing.forEach((itemMissing) => newPerms.push([...el, itemMissing]));
+  });
+
+  if (newPerms.every((el) => el.length === original.length)) return newPerms;
+  return permutations(original, newPerms);
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
