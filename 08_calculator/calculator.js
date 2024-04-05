1896e179cc69c0e7f06f5eb4e1ba89dc0229187c
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-05 20:27:33 +0000

    08 exercise Done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	if (a > b) return a - b
+  else return b - a;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let totalSum = 0;
+	array.forEach((number) => {
+    totalSum += number
+  });
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplyTotal = 1;
+  array.forEach((number) => {
+    multiplyTotal *= number;
+  });
+  return multiplyTotal;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let totalFact = 1;
+
+  if (number === 1 || number === 0) totalFact;
+  else{
+    for (let factorialNum = number; factorialNum >= 1; factorialNum--) {
+      totalFact *= factorialNum;
+    }
+  }
+  return totalFact;
 };
 
 // Do not edit below this line
