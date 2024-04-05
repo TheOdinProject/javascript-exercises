3a39cd13667c09d32c1d5be13a972f88aaff41c5
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-05 00:02:32 -0400

    04 exercise Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray,...args) {
+    const newArray = []
+    myArray.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item)
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
