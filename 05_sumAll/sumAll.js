90e47852e99a4c0b04423c360a08ec1ecf101ff4
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-04-05 00:23:33 -0400

    05 exercise Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR'
+    } else {
+        let sumTotal = 0;
+        let bigger = 0;
+        let shorter = 0;
+    
+        if (num1 > num2){
+            bigger = num1
+            shorter = num2
+        }else{
+            bigger = num2
+            shorter = num1
+        }
+        
+        for (shorter; shorter <= bigger; shorter++) {
+            sumTotal += shorter
+        }
+        return sumTotal
+    }
 };
 
 // Do not edit below this line
