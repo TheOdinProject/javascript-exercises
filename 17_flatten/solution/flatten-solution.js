07ba41675e79eb9c6693d3bdf2ea559f88cce2b7
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-04-06 15:15:41 +0100

    feat: create test files for exercise

--- 17_flatten/solution/flatten-solution.js
+++ 17_flatten/solution/flatten-solution.js
@@ -1,6 +1,13 @@
-const flatten = function() {
-  // Replace this comment with the solution code
-};
-  
+const flatten = function (array, depth = Infinity) {
+  if (depth === 0 || !array.some(Array.isArray)) return array
+
+  let flattened = []
+  array.forEach(el => {
+   flattened = flattened.concat(el) 
+  });
+
+  return flatten(flattened, depth - 1)
+} 
+
 // Do not edit below this line
 module.exports = flatten;
