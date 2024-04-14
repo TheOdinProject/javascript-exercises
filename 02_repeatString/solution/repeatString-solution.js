ea15fc7e4554256295bd87b3e1652e9734df5f16
Raunak Neupane <rezydev@gmail.com>
2024-04-14 19:20:14 +0545

    Update repeatString-solution.js

--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -8,3 +8,10 @@ const repeatString = function (word, times) {
 };
 
 module.exports = repeatString;
+
+// You can also use the repeat() string method for this solution. However, to understand looping properly, consider the above code as the main solution. Learn more here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+
+// ALTERNATIVE SOLUTION //
+// const repeatString = function (string, num) {
+//   return string.repeat(num);
+// };
