ec4ef9014f21c8374000fc55c2acd630d0aa107d
thoht3 <tho.hotrong@gmail.com>
2024-04-17 15:24:17 +0700

    tao xong bai 5 roi ne!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (
+        typeof(int1) !== 'number'
+        || typeof(int2) !== 'number'
+        || int1 < 0
+        || Math.sign(int2) == -1)  {
+        return 'ERROR'
+    }
+    let sum = 0;
+    if (int1 <= int2) {
+        for (let i = int1; i <= int2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let j = int2; j <= int1; j++) {
+            sum += j;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
