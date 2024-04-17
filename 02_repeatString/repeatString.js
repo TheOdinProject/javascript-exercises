cd12998b5c826a06bf381f851156fea82dfaf21d
thoht3 <tho.hotrong@gmail.com>
2024-04-17 14:58:30 +0700

    tao xong bai 1 2 3 4 goi ne!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function( inputText, inputNumber) {
+    if (inputNumber < 0) {
+        return 'ERROR'
+    }
+    let words = '';
+    for ( let i = 0; i <inputNumber; i++ ) {
+        words += inputText;
+    }
+    return words;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...toRemove) {
+    let removedArray = [];
+    array.forEach((item) => {
+        if (!toRemove.includes(item)) {
+            removedArray.push(item)
+        }
+    })
+    return removedArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
