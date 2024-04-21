64487dedafdae9bedf1f82f13147a568b5a72a42
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 21:22:21 -0400

    06_leapYears finalized

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true
+    }
 
+    if (year % 100 == 0) {
+        return false
+    }
+
+    if (year % 4 == 0) {
+        return true
+    }
+
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
