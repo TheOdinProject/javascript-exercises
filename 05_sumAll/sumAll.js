92789e74442e85e088a19da9ac9acebbd8f651dd
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 21:16:28 -0400

    05_sumAll finalized

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    if (firstNumber < 0 || secondNumber < 0 || 
+        typeof firstNumber != "number" || typeof secondNumber != "number" ) {
+        return 'ERROR'
+    }
 
+    let largestNumber = Math.max(firstNumber, secondNumber)
+    let smallestNumber = Math.min(firstNumber, secondNumber)
+    let sum = 0;
+
+    for (i=smallestNumber; i <= largestNumber; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
