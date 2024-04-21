e4cc3a763bd9bfca959c204ef9a8e7dd924c0fd2
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 20:01:41 -0400

    02_repeatString finalized

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
 
+    let result = '';
+    for (i=0; i < num; i++) {
+        result += string;
+    }
+    return result
 };
 
 // Do not edit below this line
