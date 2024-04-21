56aadcbfbaf97d74687e2afeca8a81e90de3bf24
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 20:20:14 -0400

    03_reverseString finalized

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = '';
+    for (i = (string.length -1); i >= 0; i--) {
+        reversed += string[i];
+    } 
+    
+    return reversed
 };
 
 // Do not edit below this line
