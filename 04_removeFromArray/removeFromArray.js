7321aef81442a257c4a6aa1a24c129686bfb6a07
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 20:55:18 -0400

    04_removeFromArray finalized

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    arrayResult = []
 
+    for (character of array) {
+        let keep = true;
+        for (element of args) {
+            if (character === element) {
+                keep = false
+                break
+            }
+        }
+        if (keep) {
+            arrayResult.push(character)
+        }
+    }
+
+    return arrayResult
 };
 
 // Do not edit below this line
