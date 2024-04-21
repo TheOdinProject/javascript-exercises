fc3171d25e1cd9d3ef6556bf1ade6ced17163ad2
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-04-20 21:48:38 -0400

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farenheit) {
+	celsius = ((farenheit - 32)*5)/9
+
+	if (Number.isInteger(celsius)) {
+		return celsius	
+	}
+
+	return Number(celsius.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+	farenheit = (((celsius * 9)/5) + 32)
+
+	if (Number.isInteger(farenheit)) {
+		return farenheit	
+	}
+
+	return Number(farenheit.toFixed(1))
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit
 };
