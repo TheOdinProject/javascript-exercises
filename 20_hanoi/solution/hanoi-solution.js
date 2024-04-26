d5d8bc8c8062fc215cf9cd8c0911ba251bb75cf2
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-04-26 14:52:48 +0100

    feat(hanoi): create solution

--- 20_hanoi/solution/hanoi-solution.js
+++ 20_hanoi/solution/hanoi-solution.js
@@ -1,6 +1,29 @@
-const hanoi = function() {
-  // Replace this comment with the solution code
+const hanoi = function (
+  n,
+  fromTowerIndex = 0,
+  storageTowerIndex = 1,
+  toTowerIndex = 2,
+  towers = [
+    Array(n)
+      .fill() // Array function creates an array with "empty" slots which map does not iterate over
+      .map((_, i) => n - i),
+    [],
+    [],
+  ],
+  steps = [towers.map((tower) => [...tower])],
+) {
+  if (n === 0) return steps;
+
+  hanoi(n - 1, fromTowerIndex, toTowerIndex, storageTowerIndex, towers, steps);
+
+  towers[toTowerIndex].push(towers[fromTowerIndex].pop());
+
+  steps.push(towers.map((tower) => [...tower]));
+
+  hanoi(n - 1, storageTowerIndex, fromTowerIndex, toTowerIndex, towers, steps);
+
+  return steps;
 };
-  
+
 // Do not edit below this line
 module.exports = hanoi;
