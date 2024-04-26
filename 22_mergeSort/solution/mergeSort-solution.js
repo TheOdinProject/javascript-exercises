b7e9c638d942909dba3d2639ab75a794e9c4edde
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-04-26 20:46:14 +0100

    feat(mergeSort): create solution

--- 22_mergeSort/solution/mergeSort-solution.js
+++ 22_mergeSort/solution/mergeSort-solution.js
@@ -1,6 +1,27 @@
-const mergeSort = function() {
-  // Replace this comment with the solution code
-};
-  
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+
+  const midpoint = Math.floor(array.length / 2);
+  const leftHalf = array.slice(0, midpoint);
+  const rightHalf = array.slice(midpoint);
+
+  return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+}
+
+function merge(left, right, merged = [], leftIndex = 0, rightIndex = 0) {
+  if (leftIndex >= left.length && rightIndex >= right.length) return merged;
+
+  if (
+    leftIndex < left.length &&
+    (rightIndex >= right.length || left[leftIndex] < right[rightIndex])
+  ) {
+    merged.push(left[leftIndex]);
+    return merge(left, right, merged, leftIndex + 1, rightIndex);
+  } else {
+    merged.push(right[rightIndex]);
+    return merge(left, right, merged, leftIndex, rightIndex + 1);
+  }
+}
+
 // Do not edit below this line
 module.exports = mergeSort;
