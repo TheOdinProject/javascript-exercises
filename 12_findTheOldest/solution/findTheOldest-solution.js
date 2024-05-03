2852b5a1ffbfcc5c1b4635c33be2f7a1ad0c3887
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-05-03 16:06:48 +0100

    feat(12): improve explanation of `??=`
    Co-authored-by: MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>

--- 12_findTheOldest/solution/findTheOldest-solution.js
+++ 12_findTheOldest/solution/findTheOldest-solution.js
@@ -1,7 +1,8 @@
 const getAge = function (person) {
   // The nullish coalescing assignment operator
-  // evaluates to the right hand side if the left hand side is undefined
-  // Else it evaluates to the left hand side
+  // only does the assignment if the left side evaluates to undefined or null
+  // if the left side has any other value, no assignment happens
+  // here, we use ??= to set the current year for our subtraction below only if there is no year of death
   person.yearOfDeath ??= new Date().getFullYear();
 
   return person.yearOfDeath - person.yearOfBirth;
