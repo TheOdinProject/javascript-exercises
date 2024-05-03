17ea5f0f2e627423859619a326eb842c33b6555d
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-05-03 02:35:39 +0100

    feat(10): add explanatory comments

--- 10_fibonacci/solution/fibonacci-solution.js
+++ 10_fibonacci/solution/fibonacci-solution.js
@@ -34,6 +34,11 @@ const fibonacci = function(countArg) {
 // An even more advanced way
 // const fibs = (n) =>
 //  Array.from({ length: n }) // create array of size n, filled with "undefined"
-//    .reduce((acc, _, i) => [...acc, acc.at(-1) + acc.at(-2)], [0, 1])[n];
+//    // It is a convention to use "_" as a name for arguments that are not used
+//    // But need to be there in order to access further arguments. _ here would represent the value
+//    // But we don't need the value, we want index, which is the third argument received by the callback function
+//    .reduce((acc, _, i) => 
+//          // The return value of the callback function becomes the *next* value for the accumulator
+//          [...acc, acc.at(-1) + acc.at(-2)], [0, 1])[n]; // since this generates an array of n fibonacci numbers, we just take last value
 
 module.exports = fibonacci;
