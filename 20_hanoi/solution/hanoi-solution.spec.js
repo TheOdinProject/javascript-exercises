509c18d3b97474c4f17296ff9884ba4a45f2b02a
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-05-03 01:18:37 +0100

    feat(hanoi): create tests for exercise

--- 20_hanoi/solution/hanoi-solution.spec.js
+++ 20_hanoi/solution/hanoi-solution.spec.js
@@ -1,15 +1,61 @@
-const hanoi = require('./hanoi-solution');
+const hanoi = require("./hanoi-solution");
 
-describe('hanoi', () => {
-  test('First test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
-
-    expect(hanoi()).toBe('');
+describe("hanoi", () => {
+  test("hanoi(1) should solve the puzzle in 1 move", () => {
+    const result = hanoi(1);
+    expect(result).toEqual([
+      [[1], [], []],
+      [[], [], [1]],
+    ]);
+    expect(result.length).toBe(2);
   });
-  
-  test('Second test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
 
-    expect(hanoi()).toBe('');
+  test("hanoi(2) should solve the puzzle in 3 moves", () => {
+    const result = hanoi(2);
+    expect(result).toEqual([
+      [[2, 1], [], []],
+      [[2], [1], []],
+      [[], [1], [2]],
+      [[], [], [2, 1]],
+    ]);
+    expect(result.length).toBe(4);
+  });
+
+  test("hanoi(3) should solve the puzzle in 7 moves", () => {
+    const result = hanoi(3);
+    expect(result).toEqual([
+      [[3, 2, 1], [], []],
+      [[3, 2], [], [1]],
+      [[3], [2], [1]],
+      [[3], [2, 1], []],
+      [[], [2, 1], [3]],
+      [[1], [2], [3]],
+      [[1], [], [3, 2]],
+      [[], [], [3, 2, 1]],
+    ]);
+    expect(result.length).toBe(8);
+  });
+
+  test("hanoi(4) should solve the puzzle in 15 moves", () => {
+    const result = hanoi(4);
+    const expected = [
+      [[4, 3, 2, 1], [], []],
+      [[4, 3, 2], [1], []], 
+      [[4, 3], [1], [2]],
+      [[4, 3], [], [2, 1]],
+      [[4], [3], [2, 1]],
+      [[4, 1], [3], [2]],
+      [[4, 1], [3, 2], []],
+      [[4], [3, 2, 1], []],
+      [[], [3, 2, 1], [4]],
+      [[], [3, 2], [4, 1]],
+      [[2], [3], [4, 1]],
+      [[2, 1], [3], [4]],
+      [[2, 1], [], [4, 3]],
+      [[2], [1], [4, 3]],
+      [[], [1], [4, 3, 2]],
+      [[], [], [4, 3, 2, 1]],
+    ];
+    expect(result).toEqual(expected);
   });
 });
