692adf197c0056f4140603114e7b5c5747385f87
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-05-03 16:16:36 +0100

    feat(12): include both solutions

--- 12_findTheOldest/solution/findTheOldest-solution.js
+++ 12_findTheOldest/solution/findTheOldest-solution.js
@@ -1,18 +1,39 @@
-const getAge = function (person) {
-  // The nullish coalescing assignment operator
-  // only does the assignment if the left side evaluates to undefined or null
-  // if the left side has any other value, no assignment happens
-  // here, we use ??= to set the current year for our subtraction below only if there is no year of death
-  person.yearOfDeath ??= new Date().getFullYear();
-
-  return person.yearOfDeath - person.yearOfBirth;
-};
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+}
 
 const findTheOldest = function (people) {
-  // Sort the people from oldest to youngest
-  return people.sort(
-    (person, nextPerson) => getAge(nextPerson) - getAge(person),
-  )[0];
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
+  });
 };
+
+// An alternative solution
+// const getAge = function (person) {
+//  // The nullish coalescing assignment operator
+//  // only does the assignment if the left side is "nullish" (evaluates to undefined or null)
+//  // if the left side has any other value, no assignment happens
+//  // here, we use ??= to set the current year for our subtraction below only if there is no year of death
+//  person.yearOfDeath ??= new Date().getFullYear();
+//
+//  return person.yearOfDeath - person.yearOfBirth;
+//};
+
+//const findTheOldest = function (people) {
+//  const peopleOldestToYoungest = people.toSorted(
+//    (person, nextPerson) => getAge(nextPerson) - getAge(person),
+//  );
+//
+//  const oldestPerson = peopleOldestToYoungest[0];
+//  return oldestPerson;
+//};
 
 module.exports = findTheOldest;
