de603998af061d3e7a1ed677610a5c2b90653d3a
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-05-03 01:25:22 +0100

    feat: recursive fibonacci write tests

--- 21_recursiveFibonacci/solution/recursiveFibonacci-solution.spec.js
+++ 21_recursiveFibonacci/solution/recursiveFibonacci-solution.spec.js
@@ -2,42 +2,45 @@ const recursiveFibonacci = require('./recursiveFibonacci-solution');
 
 describe('recursiveFibonacci', () => {
   test('Gets 1 fibonacci number', () => {
-    expect(recursiveFibonacci(1)).toBe([1]);
+    expect(recursiveFibonacci(1)).toEqual([1]);
   });
   
-  test('Gets 2 fibonacci number', () => {
-    expect(recursiveFibonacci(2)).toBe([1, 1]);
+  test('Gets 2 fibonacci numbers', () => {
+    expect(recursiveFibonacci(2)).toEqual([1, 1]);
   });
   
-  test('Gets 3 fibonacci number', () => {
-    expect(recursiveFibonacci(3)).toBe([1, 1]);
+  test('Gets 3 fibonacci numbers', () => {
+    expect(recursiveFibonacci(3)).toEqual([1, 1, 2]);
   });
   
-  test('Gets 4 fibonacci number', () => {
-    expect(recursiveFibonacci(4)).toBe([1, 1]);
+  test('Gets 4 fibonacci numbers', () => {
+    expect(recursiveFibonacci(4)).toEqual([1, 1, 2, 3]);
   });
   
-  test('Gets 6 fibonacci number', () => {
-    expect(recursiveFibonacci(6)).toBe([1, 1]);
+  test('Gets 5 fibonacci numbers', () => {
+    expect(recursiveFibonacci(5)).toEqual([1, 1, 2, 3, 5]);
   });
   
-  test('Gets 10 fibonacci number', () => {
-    expect(recursiveFibonacci(10)).toBe([1, 1]);
+  test('Gets 6 fibonacci numbers', () => {
+    expect(recursiveFibonacci(6)).toEqual([1, 1, 2, 3, 5, 8]);
   });
   
-  test('Gets 12 fibonacci number', () => {
-    expect(recursiveFibonacci(12)).toBe([1, 1]);
+  test('Gets 10 fibonacci numbers', () => {
+    expect(recursiveFibonacci(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
   });
   
-  test('Gets 16 fibonacci number', () => {
-    expect(recursiveFibonacci(16)).toBe([1, 1]);
+  test('Gets 12 fibonacci numbers', () => {
+    expect(recursiveFibonacci(12)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]);
+  });
+  
+  test('Gets 15 fibonacci numbers', () => {
+    expect(recursiveFibonacci(15)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]);
   });
   
   test('Gets 20 fibonacci numbers', () => {
-    expect(recursiveFibonacci(20)).toBe([1, 1]);
-  });
-  
-  test('Gets 22 fibonacci numbers', () => {
-    expect(recursiveFibonacci(22)).toBe([1, 1]);
+    expect(recursiveFibonacci(20)).toEqual([
+      1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765
+    ]);
   });
 });
+
