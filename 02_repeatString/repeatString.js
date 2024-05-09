0b051eba75e4fe8c2f909fc9edaa7c9fa785d9c3
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-09 16:54:30 -0700

    Add environment for Test Driven Development (TDD)
    All JavaScript files:
    
    Run test for init Hello World file
    
    Run test for repeating string n amount of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    //Make variable to store end result
+    let stringHolder = '';
+    // Append current string to end of stringHolder for 'num' many times
+  for(let i=0; i<num; i++)
+        {
+            stringHolder += string;
+        }
+    //Make error handling for negative number values
+    if(num < 0) {
+        return 'ERROR'
+    }
+    else {
+        return stringHolder;
+    }
+    
 };
 
 // Do not edit below this line
