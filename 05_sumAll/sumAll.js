4e0f4a75e26f924dc2aa97d49a2e092bc69587a2
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-09 19:00:02 -0700

    Create test for summation function
    Create function that takes parameters and creates
    an array between the two parameters, in increments of 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(j, k) {
+    const newArray = [];
+    let sum = 0;
+    //Ensuring that the function can pass any order of numbers and this will find the greater of the two
+    if(j > k)
+        {
+            startPoint = k
+            endPoint = j
+        }
+    else {
+        startPoint = j;
+        endPoint = k;
+    }
+    //Ensuring that values cannot be negative for either portion of array declaration
+    if((j > 0 && k > 0) && (typeof(k) != "string"))
+        {
+            for(i = startPoint; i <= endPoint; i++ )
+                {
+                    newArray.push(i);
 
+                    for(j = i; j< i+1; j++)
+                        {
+                            sum += j;
+                        }
+                }
+                return sum;
+        }
+    else {
+        return 'ERROR';
+    }
+    
+        
 };
 
 // Do not edit below this line
