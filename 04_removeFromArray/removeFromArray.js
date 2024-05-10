acbd754ee32cc89411713334a07fa45ba5a616f8
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-09 18:18:49 -0700

    Add new test for removing items from an removeFrom
    Create new function for removing items from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {  
+    //Make blank array object to push new array items to
+    const newArray = [];
+    //For each item in the array that is not included in the args portion of the function, push to the new Array
+    array.forEach( (item) => {
+        if(!args.includes(item)){
+            newArray.push(item);
+        }
+    });
+    //Return the finished array!
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
