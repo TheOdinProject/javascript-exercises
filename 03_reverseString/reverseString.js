82f8f6daa1993f2ea2aa0a6ca0b3fd154dfeb2b2
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-09 17:14:23 -0700

    Test file for reversing string input
    Create function for reversing strings based on input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //Make variable to store reversed string
+  let reverseStringArray = '';
+  //Start at end of old string and push invidual chars to new string container in reverse order
+  for(i=string.length-1; i >= 0; i--)
+    {
+        reverseStringArray += string[i];
+    }
+    return reverseStringArray;
 };
 
 // Do not edit below this line
