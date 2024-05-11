0180e1995fb58e3e9d96e5c9f359b08877c4dcdc
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-11 16:18:44 -0700

    Add functions for conversion of temperatures
    Add function for converting farenheit to celcius
    
    Add function for converting celcius to farenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let convertedNum = Math.round(((num-32)*5/9) * 10)/10
+  return convertedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let convertedNum = Math.round(((num*9/5)+32) * 10)/10
+  return convertedNum;
 };
 
 // Do not edit below this line
