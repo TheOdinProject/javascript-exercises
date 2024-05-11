217555db503f17474b8f37690005cfb1b63b1c67
batchelormaxdb <batchelormaxdb@gmail.com>
2024-05-11 15:49:25 -0700

    Add function for checking if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //Ensure year is divisble by four, not by 100, unless they are also divisible by 400
+    if (year % 4 == 0)
+        {
+          if (year % 100 == 0)
+          {
+            if (year % 400 == 0) 
+              return true;
+            return false;
+          }
+          return true;
+        }
 };
 
 // Do not edit below this line
