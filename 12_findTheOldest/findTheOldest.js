96e4f8ffd12463630e53bd06d3bf45db23b11acd
JdClarkGit <137746621+JdClarkGit@users.noreply.github.com>
2024-05-18 18:43:55 -0500

    put in my solutions

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (birth, death) {
+  let longestLife = 0;
+  let oldestIndex = 0;
 
+  for (let i = 0; i < birth.length; i++) {
+    let lifespan = death[i] - birth[i];
+    if (lifespan > longestLife) {
+      longestLife = lifespan;
+      oldestIndex = i;
+    }
+  }
+  return { birth: birth[oldestIndex], death: death[oldestIndex] };
 };
 
 // Do not edit below this line
