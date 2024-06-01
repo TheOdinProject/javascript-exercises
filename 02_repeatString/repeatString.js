167286bde9f5c7ac3158245a260ab22ce112d729
yasuo055 <anonymousyasuo@gmail.com>
2024-06-01 21:08:27 +0800

    add r

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+let string = 'hey';
 
-};
+const repeatString = function(string, num) {
+    let result = '';
+    for(let i = 0; i < num; i++){
+        result += string;
+    }
+    return result;
+};  
+
+const repeatedString = repeatString(string, 3);
+console.log(repeatedString);
+
 
 // Do not edit below this line
 module.exports = repeatString;
