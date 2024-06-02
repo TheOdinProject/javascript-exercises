247b6134d91d418be0761d1eaa8dadd5472495ad
yasuo055 <anonymousyasuo@gmail.com>
2024-06-02 23:16:55 +0800

    update SA task complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let finalSum = 0;
+  // Check if parameters are numbers and positive integers
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
 
+  if (num1 > num2) {
+    let swap = num1;
+    num1 = num2;
+    num2 = swap;
+  }
+
+  for (let i = num1; i <= num2; i++) {
+    finalSum += i;
+  }
+  return finalSum;
 };
+
+console.log(sumAll(10, [90, 1]));
 
 // Do not edit below this line
 module.exports = sumAll;
