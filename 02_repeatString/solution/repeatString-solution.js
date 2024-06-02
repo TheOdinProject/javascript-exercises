72e00bbd019fa7600976c3b1ccc5e219b83e38db
yasuo055 <anonymousyasuo@gmail.com>
2024-06-02 09:38:47 +0800

    update r

--- 02_repeatString/solution/repeatString-solution.js
+++ 02_repeatString/solution/repeatString-solution.js
@@ -1,11 +1,11 @@
-// const repeatString = function (word, times) {
-//   if (times < 0) return "ERROR";
-//   let string = "";
-//   for (let i = 0; i < times; i++) {
-//     string += word;
-//   }
-//   return string;
-// };
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
+};
 
 console.log(typeof Number);
 
