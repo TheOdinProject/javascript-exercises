e9b57fae700cccb24a76b4d693af5a7fc201f6b4
yasuo055 <anonymousyasuo@gmail.com>
2024-06-02 10:31:02 +0800

    add RFA 1st task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, element) {
+    const index = arr.indexOf(element);
+
+    if(index !== -1){
+        arr.splice(index, 1);
+    }
+    return arr;
+   
 };
+
+let removeElement = removeFromArray([1, 2, 3, 4], 3);
+console.log(removeElement);
 
 // Do not edit below this line
 module.exports = removeFromArray;
