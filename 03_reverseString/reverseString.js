e616cfa6d2739f396f1c4ade17172d88c7567876
yasuo055 <anonymousyasuo@gmail.com>
2024-06-02 09:51:40 +0800

    add 1st task RS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split('').reverse().join('');
 };
+
+
+let isReverse = reverseString("hello there");
+console.log(isReverse)
 
 // Do not edit below this line
 module.exports = reverseString;
