ed7e42156f0992aa9f77ba39590096bf2c2cc964
yasuo055 <anonymousyasuo@gmail.com>
2024-06-03 12:52:55 +0800

    update TC completed task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let result = (Fahrenheit - 32) * 5 / 9;
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let result = (celsius * 9) / 5 + 32;
+  return parseFloat(result.toFixed(1));
 };
+
+
 
 // Do not edit below this line
 module.exports = {
