ed3b3ffa6df463cffd57b2b45450a08103e18a10
yasuo055 <anonymousyasuo@gmail.com>
2024-06-11 19:53:15 +0800

    update fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,18 @@
-const fibonacci = function(input) {
-    const n = parseInt(input);
-    if(isNaN(n)){
-      return NaN; // Return NaN if input is not a number
-    }
-    if(input < 0){
-        return "OOPS";
-    }
-    if(n <= 1){
-     return n;
-    }else{
-     return fibonacci(n - 1) + fibonacci(n - 2);
-    }
-   
+const fibonacci = function (input) {
+  const n = parseInt(input);
+  if (isNaN(n)) {
+    return NaN; // Return NaN if input is not a number
+  }
+
+  if (input < 0) {
+    return "OOPS";
+  }
+
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
 
 // Do not edit below this line
