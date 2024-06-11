5f2583bfb8d14b0271be680beb8777020f9245be
yasuo055 <anonymousyasuo@gmail.com>
2024-06-11 22:21:20 +0800

    update findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  // Function to calculate age
+  const calculateAge = (yearOfBirth, yearOfDeath) => {
+    const currentYear = new Date().getFullYear();
+    return (yearOfDeath || currentYear) - yearOfBirth;
+  };
 
+  // Find the oldest person
+  return array.reduce((oldest, person) => {
+    const personAge = calculateAge(person.yearOfBirth, person.yearOfDeath);
+    const oldestAge = calculateAge(oldest.yearOfBirth, oldest.yearOfDeath);
+
+    return oldestAge > personAge ? oldest : person;
+  });
 };
 
 // Do not edit below this line
