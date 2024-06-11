e42d96f777a26e5166594f148dcc44cad0ff9e5e
yasuo055 <anonymousyasuo@gmail.com>
2024-06-11 21:09:20 +0800

    Update getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function () {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      
+    ];
+
+    return books.map(book => book.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
