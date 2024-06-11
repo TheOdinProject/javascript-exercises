9cafa125e65326ac5f60ad20c07e95903bc2aae4
yasuo055 <anonymousyasuo@gmail.com>
2024-06-11 11:13:45 +0800

    update complete palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleaned = str.toLowerCase().replace(/[\W_]/g, ""); // Normalize the string
 
+  const reversed = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
 };
+
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
