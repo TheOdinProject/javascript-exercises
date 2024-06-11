d8b457c235f454daffc80c5a36fd4f6285ae046f
yasuo055 <anonymousyasuo@gmail.com>
2024-06-11 10:30:55 +0800

    calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++){
+        sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr = []) {
+  if (arr.length === 0) {
+    return 1; // Default to 1 if no arguments are provided
+  }
+  
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+
+  if(n === 0){
+    return 1; // Base case: 0! is 1
+  }
+
+  return n * factorial(n - 1); //Recursive call
+  
 };
 
 // Do not edit below this line
