5a0ee1b51194c80906f83c2fe4ab3ae88b068096
Aaron Pang <aaroncwpang@gmail.com>
2024-06-30 17:31:38 -0700

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(repeatedWord, repeatCount) {
+  let output = ''
+  if (repeatCount < 0) {
+    return 'ERROR';
+  }
+  while (repeatCount > 0) {
+    output += repeatedWord;
+    repeatCount--;
+  }
+  return output;
 };
 
 // Do not edit below this line
