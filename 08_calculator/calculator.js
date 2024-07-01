ac4f2bf0b86df92de1c61a6302d9f64b76111cec
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 16:10:54 -0700

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+  nums.forEach((element) => {
+    sum += element;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  product = 1;
+  nums.forEach((element) => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let product = 1;
+  for (let i = 0; i < power; i++) {
+    product *= base;
+  }
+	return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+	let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
