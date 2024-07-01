e09cfb2c50467885137c5a158e57bcc9cf5a0356
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 14:41:58 -0700

    compelted 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  switch(true) {
+    case !(year % 4 === 0):
+      console.log('hi');
+      break;
+    case ((year % 100 === 0) && !(year % 400 === 0)):
+      break;
+    default: 
+      console.log(year % 4);
+      return true;
+  }
+  return false;
 
 };
 
