18c35a5bc38b3cb00b4dead0bbcc12bc2da77cf7
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 14:48:39 -0700

    completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Math.round(((temp - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Math.round((temp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
