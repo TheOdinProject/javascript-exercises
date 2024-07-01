d731d568690c85d632fb5e5d35425420f0d762b4
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 14:30:30 -0700

    completed 05_sum_All

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+  
+  if ((!Number.isInteger(firstNum)) || (!Number.isInteger(secondNum)) ||
+    (firstNum < 0) || (secondNum < 0)) {
+    return 'ERROR';
+  }
+  let low = 0;
+  let high = 0;
+  
+  if (firstNum > secondNum) {
+    high = firstNum;
+    low = secondNum;
+  }
+  else {
+    high = secondNum;
+    low = firstNum;
+  }
+  let sum = 0;
 
+  for (let i = low; i <= high; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
