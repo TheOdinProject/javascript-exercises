2e4fd4c78b7cdd022da9f855fcf0a8767919db7e
Aaron Pang <aaroncwpang@gmail.com>
2024-06-30 17:39:01 -0700

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+  let array = input.split("");
+  let reverseString = '';
+  for (let j = array.length - 1; j >= 0; j--) {
+    reverseString += array[j];
+  }
+  return reverseString;
 };
 
 // Do not edit below this line
