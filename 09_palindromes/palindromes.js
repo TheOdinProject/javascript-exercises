c5c1a77c00f5f37e3e91c085352df6f3ec83729f
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 16:52:37 -0700

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleanedInput = input.toLowerCase()
+  .split('')
+  .filter((character) => alphanumerical.includes(character))
+  .join('');
+  const backwards = cleanedInput.split('').reverse().join('');
+  return backwards === cleanedInput;
 };
 
 // Do not edit below this line
