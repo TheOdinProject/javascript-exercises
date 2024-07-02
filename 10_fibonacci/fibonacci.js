20fcde8f2e23fde68c91fcc1aeae10d465f23721
Aaron Pang <aaroncwpang@gmail.com>
2024-07-01 17:12:39 -0700

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+  if (input == 0) return 0;
+  if (input < 0) return 'OOPS';
+  if (input == 1 || input == 2) return 1;
+ 
+  return fibonacci(input - 2) + fibonacci(input - 1);
 };
 
 // Do not edit below this line
