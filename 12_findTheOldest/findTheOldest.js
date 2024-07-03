3035685e18adc004a54fb5a123e89f0c651c7a40
Aaron Pang <aaroncwpang@gmail.com>
2024-07-02 17:07:07 -0700

    updated solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,40 @@
-const getAge = function(birth, death) {
-  if (!death || isNaN(death)) {
+const getAge = function (birth, death) {
+  if (!death) {
     death = new Date().getFullYear();
   }
   return death - birth;
 };
 
-const findTheOldest = function(array) {
-  return array.reduce((oldest, current) => {
-    let currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
-    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
-    if (currentAge > oldestAge) {
-      return current;
-    }
-    return oldest;
+const findTheOldest = function (people) {
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+    return oldestAge < currentAge ? currentPerson : oldest;
   });
-}
+};
 
-// Do not edit below this line
+/* ALTERNATIVE SOLUTION
+const getAge = function (person) {
+  // The nullish coalescing assignment operator
+  // only does the assignment if the left side is "nullish" (evaluates to undefined or null)
+  // if the left side has any other value, no assignment happens
+  // here, we use ??= to set the current year for our subtraction below only if there is no year of death
+  person.yearOfDeath ??= new Date().getFullYear();
+
+  return person.yearOfDeath - person.yearOfBirth;
+};
+
+const findTheOldest = function (people) {
+  const peopleOldestToYoungest = people.toSorted(
+    (person, nextPerson) => getAge(nextPerson) - getAge(person),
+  );
+
+  const oldestPerson = peopleOldestToYoungest[0];
+  return oldestPerson;
+};
+*/
+
 module.exports = findTheOldest;
