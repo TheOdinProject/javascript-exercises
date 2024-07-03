842e1b7fb2fb42e0da0fe8e19ead1b4362a2e562
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-07-03 19:49:16 +0100

    refactor: remove redundant `length` tests

--- 20_hanoi/solution/hanoi-solution.spec.js
+++ 20_hanoi/solution/hanoi-solution.spec.js
@@ -3,27 +3,27 @@ const hanoi = require("./hanoi-solution");
 describe("hanoi", () => {
   test("hanoi(1) should solve the puzzle in 1 move", () => {
     const result = hanoi(1);
-    expect(result).toEqual([
+    const expected = [
       [[1], [], []],
       [[], [], [1]],
-    ]);
-    expect(result.length).toBe(2);
+    ];
+    expect(result).toEqual(expected);
   });
 
   test("hanoi(2) should solve the puzzle in 3 moves", () => {
     const result = hanoi(2);
-    expect(result).toEqual([
+    const expected = [
       [[2, 1], [], []],
       [[2], [1], []],
       [[], [1], [2]],
       [[], [], [2, 1]],
-    ]);
-    expect(result.length).toBe(4);
+    ];
+    expect(result).toEqual(expected);
   });
 
   test("hanoi(3) should solve the puzzle in 7 moves", () => {
     const result = hanoi(3);
-    expect(result).toEqual([
+    const expected = [
       [[3, 2, 1], [], []],
       [[3, 2], [], [1]],
       [[3], [2], [1]],
@@ -32,15 +32,15 @@ describe("hanoi", () => {
       [[1], [2], [3]],
       [[1], [], [3, 2]],
       [[], [], [3, 2, 1]],
-    ]);
-    expect(result.length).toBe(8);
+    ];
+    expect(result).toEqual(expected);
   });
 
   test("hanoi(4) should solve the puzzle in 15 moves", () => {
     const result = hanoi(4);
     const expected = [
       [[4, 3, 2, 1], [], []],
-      [[4, 3, 2], [1], []], 
+      [[4, 3, 2], [1], []],
       [[4, 3], [1], [2]],
       [[4, 3], [], [2, 1]],
       [[4], [3], [2, 1]],
