2283f07ca304e533daea60d792b5499ea9b33b3d
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-12 08:48:48 -0700

    done 02 all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let temp = '';
+    if (num<0) {
+        return 'ERROR';
+    }
+    if (num==0||string.lenth==0) {
+        return '';
+    }
+    do{
+        temp+=string;
+        num--;
+    } while(num > 0)
+    return temp;   
 };
 
 // Do not edit below this line
