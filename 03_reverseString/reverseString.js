2112a19a74ccfb86fce3b676f4f7af773478c8f5
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-12 09:26:30 -0700

    finished 03 all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reversedString='';
+    for (let index = text.length; index >= 0; index--) {
+        reversedString+=text.charAt(index);
+    }
+    return reversedString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
