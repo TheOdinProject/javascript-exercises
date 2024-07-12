eeadba04d0edc3a167de50967930f29d0803ab30
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-12 10:26:28 -0700

    gross if ors 04 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,firstRemove,secondRemove, thirdRemove, fourthRemove) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]===firstRemove || arr[i]===secondRemove || arr[i]===thirdRemove || arr[i]===fourthRemove) {
+            arr.splice(i,1);
+            // console.log(arr);
+            removeFromArray(arr,firstRemove,secondRemove, thirdRemove, fourthRemove);
+        } 
+    }
+    return arr;
+
 };
 
 // Do not edit below this line
