6e543b478148c7b45678d495665071cb25effa7e
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 09:27:16 -0700

    06 passed all tests ez pz

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear = false; 
+    if (year%400 == 0) {
+        return isLeapYear= true;
+    }
+    if (year%100 == 0) {
+        return isLeapYear= false;
+    }
+    if (year%4 == 0) {
+        return isLeapYear= true;
+    } 
+    return isLeapYear;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+
+// Leap years are years divisible by four (like 1984 and 2004). 
+// However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+// unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years) 
+
+// leapYears(2000) // is a leap year: returns true
+// leapYears(1985) // is not a leap year: returns false
