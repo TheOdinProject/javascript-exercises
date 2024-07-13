6296c2e6bbb778d9cb4d6d165d4097ef4110f53e
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 09:42:59 -0700

    07 all tests passed ez

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit-32)*(5/9))*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius*(9/5))+32)*10)/10 ;
+  
 };
 
 // Do not edit below this line
@@ -9,3 +12,12 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+// ```
+// convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+// convertToFahrenheit(0) // celsius to fahrenheit, should return 32
+// ```
+
+// Because we are human, we want the result temperature to be rounded to one decimal place
