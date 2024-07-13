837adcc042774f654f49d8df9784c2751c9db4b6
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 08:54:42 -0700

    all tests pass 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(value_x, value_y) {
+    if ( !Number.isInteger(value_x)|| !Number.isInteger(value_y) ){
+        return 'ERROR';
+    }   
+    if (value_x < 0 || value_y < 0 ) {
+        return 'ERROR';
+    }
+    if (value_x === value_y){
+        return 'ERROR';
+    }
 
+    let sumToReturn = 0;
+    let smallNum, bigNum;
+    if (value_x > value_y) {
+        bigNum = value_x;
+        smallNum = value_y;
+    }else{
+        smallNum = value_x;
+        bigNum = value_y;
+    }
+    do {
+        sumToReturn+=smallNum++;
+    } while (smallNum<=bigNum);
+    return sumToReturn;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
