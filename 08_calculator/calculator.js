64ec5867cc75bad44563896b279136465e371a9f
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 10:40:42 -0700

    working on calc 08, finish up in a little

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,18 +1,35 @@
-const add = function() {
-	
+//basic math functions params are primitive num 
+const add = function(value_x, value_y) {
+	return value_x + value_y;
 };
 
-const subtract = function() {
-	
+const subtract = function(value_x, value_y) {
+	return value_x - value_y;
 };
 
-const sum = function() {
-	
+//Param is array
+const sum = function(arr) {
+
+  let sumToReturn = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sumToReturn = sumToReturn + arr[i];   
+  }
+  return sumToReturn;
+  
+};
+const multiply = function(arr) {
+  if(arr === undefined) {
+    return;
+  }
+  let numToReturn = 1 ;
+  for (let i = 0; i < arr.length; i++) {
+    numToReturn = numToReturn * arr[i];   
+  }
+  return numToReturn;
 };
 
-const multiply = function() {
 
-};
+// adanced math functions
 
 const power = function() {
 	
@@ -31,3 +48,12 @@ module.exports = {
   power,
   factorial
 };
+
+
+// The goal for this exercise is to create a calculator that does the following:
+
+// add, subtract, get the sum, multiply, get the power, and find the factorial
+
+// In order to do this please fill out each function with your solution. 
+// Make sure to return the value so you can test it in Jest! To see the expected value
+// take a look at the spec file that houses the Jest test cases.
