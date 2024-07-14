4464bb636c1b053e09bde930d6f7f73e6408218b
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 22:19:21 -0700

    11 passed all tests

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,14 @@
-const getTheTitles = function() {
+// const getTheTitles = function(arr) {
+//     let returnArray =  [] ;
+//     for (let i = 0; i < arr.length; i++) {
+//         returnArray.push(arr[i].title)
+//     }
+//     return returnArray;
+// };
 
-};
+function getTheTitles(arr) {
+    return arr.map( (obj) => obj.title );
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
