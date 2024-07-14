b067f88da1ec831adeece6191c6a74a1c052e658
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-14 09:05:47 -0700

    yay 12 finished all tests passed!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestPerson = arr[0];
+    for (const i in arr) {
+        oldestPerson = setOldest(arr[i], oldestPerson)
+    }
+    return oldestPerson;
 };
+
+function setOldest(person1, person2) {
+    return getAge(person1) > getAge(person2) ? person1: person2;
+}
+// use current year if no death year
+function getAge(obj) {
+    return Object.hasOwn(obj, 'yearOfDeath')? obj.yearOfDeath - obj.yearOfBirth : new Date().getFullYear() - obj.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
