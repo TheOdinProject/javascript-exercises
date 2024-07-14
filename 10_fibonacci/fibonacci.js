b9f14e93e8585b1191a3c659dadda7db53683528
GNouchi <40991860+GNouchi@users.noreply.github.com>
2024-07-13 22:05:14 -0700

    10 passed all tests recursive solution and cached recursive solution commented out

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,36 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) { return "OOPS"; }
+    num = parseInt(num); 
+    if (num == 0 || num == 1) { return num; }
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 2; i < num; i++) {
+        temp = num1 + num2;
+        num1 = num2;
+        num2 = temp;
+    }
+    return num1 + num2;
 };
+
+//cheat answer 
+// function fibonacci(n) {
+//     if (n < 0) { return "OOPS"; }
+//     if (n == 0) { return 0;}
+//     if (n == 1) { return 1;}
+//     return fibonacci(n -1) + fibonacci(n -2 ) 
+// }
+
+// cached version (should be a lot faster)
+// function fibonacci(n, cache) { 
+//     if (n < 0) { return "OOPS"; }
+//     if (n == 0) { return 0;}
+//     cache = cache || [1, 1] 
+//         if (cache[n]) 
+//             return cache[n] 
+//         return cache[n] = fibonacci(n - 1, cache) +  
+//         fibonacci(n - 2, cache); 
+// }
 
 // Do not edit below this line
 module.exports = fibonacci;
+
