9e5041a7bdd8f386747baad27e587e6f67e388ab
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-18 15:59:31 +0100

    feat(permutations): remove unnecessary utility helper function that is replaceable with toEqual

--- 16_permutations/solution/permutations-solution.spec.js
+++ 16_permutations/solution/permutations-solution.spec.js
@@ -1,41 +1,19 @@
-const permutations = require('./permutations-solution');
+const permutations = require("./permutations-solution");
 
-describe('permutations', () => {
-  function outcome(input, expected) {
-    const actual = permutations(input);
-
-    // Convert both arrays to strings to compare them later, excluding the order the arrays elements are in
-
-    const sterilise = (input) =>
-      input
-        .map((el) => el.toString())
-        .toSorted()
-        .toString();
-
-    return [sterilise(actual), sterilise(expected)];
-  }
-
-  let actual, expected;
-
-  afterEach(() => {
-    expect(actual).toBe(expected);
+describe("permutations", () => {
+  test("Works for array of size one", () => {
+    expect(permutations([1]).sort()).toEqual([1].sort());
   });
-
-  test('Works for array of size one', () => {
-    [actual, expected] = outcome([1], [1]);
-  });
-  test('Works for array of size two', () => {
-    [actual, expected] = outcome(
-      [1, 2],
+  test("Works for array of size two", () => {
+    expect(permutations([1, 2]).sort()).toEqual(
       [
         [1, 2],
         [2, 1],
-      ],
+      ].sort(),
     );
   });
-  test('Works for array of size three', () => {
-    [actual, expected] = outcome(
-      [1, 2, 3],
+  test("Works for array of size three", () => {
+    expect(permutations([1, 2, 3]).sort()).toEqual(
       [
         [1, 2, 3],
         [1, 3, 2],
@@ -43,40 +21,37 @@ describe('permutations', () => {
         [2, 3, 1],
         [3, 1, 2],
         [3, 2, 1],
-      ],
+      ].sort(),
     );
   });
-  test('Works for array of size four', () => {
-    [actual, expected] = outcome(
-      [1, 2, 3, 4],
+  test("Works for array of size four", () => {
+    expect(permutations([1, 2, 3, 4]).sort()).toEqual(
       [
-        [
-          [1, 2, 3, 4],
-          [1, 2, 4, 3],
-          [1, 3, 2, 4],
-          [1, 3, 4, 2],
-          [1, 4, 2, 3],
-          [1, 4, 3, 2],
-          [2, 1, 3, 4],
-          [2, 1, 4, 3],
-          [2, 3, 1, 4],
-          [2, 3, 4, 1],
-          [2, 4, 1, 3],
-          [2, 4, 3, 1],
-          [3, 1, 2, 4],
-          [3, 1, 4, 2],
-          [3, 2, 1, 4],
-          [3, 2, 4, 1],
-          [3, 4, 1, 2],
-          [3, 4, 2, 1],
-          [4, 1, 2, 3],
-          [4, 1, 3, 2],
-          [4, 2, 1, 3],
-          [4, 2, 3, 1],
-          [4, 3, 1, 2],
-          [4, 3, 2, 1],
-        ],
-      ],
+        [1, 2, 3, 4],
+        [1, 2, 4, 3],
+        [1, 3, 2, 4],
+        [1, 3, 4, 2],
+        [1, 4, 2, 3],
+        [1, 4, 3, 2],
+        [2, 1, 3, 4],
+        [2, 1, 4, 3],
+        [2, 3, 1, 4],
+        [2, 3, 4, 1],
+        [2, 4, 1, 3],
+        [2, 4, 3, 1],
+        [3, 1, 2, 4],
+        [3, 1, 4, 2],
+        [3, 2, 1, 4],
+        [3, 2, 4, 1],
+        [3, 4, 1, 2],
+        [3, 4, 2, 1],
+        [4, 1, 2, 3],
+        [4, 1, 3, 2],
+        [4, 2, 1, 3],
+        [4, 2, 3, 1],
+        [4, 3, 1, 2],
+        [4, 3, 2, 1],
+      ].sort(),
     );
   });
 });
