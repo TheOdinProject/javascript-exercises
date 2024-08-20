fc1927959685ddb57f3bcf13da60ab19d77f709c
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-20 23:38:10 +0100

    feat(permutations): copy over tests from solution

--- 16_permutations/permutations.spec.js
+++ 16_permutations/permutations.spec.js
@@ -1,15 +1,64 @@
-const permutations = require('./permutations');
+const permutations = require("./permutations");
 
-describe('permutations', () => {
-  test('First test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
-
-    expect(permutations()).toBe('');
+describe("permutations", () => {
+  test("1 possible permutation for a set containing 0 numbers", () => {
+    expect(permutations([]).sort()).toEqual([[]].sort());
   });
-  
-  test.skip('Second test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
 
-    expect(permutations()).toBe('');
+  test.skip("1 possible permutation for a set containing 1 number", () => {
+    expect(permutations([1]).sort()).toEqual([[1]].sort());
+  });
+
+  test.skip("2 possible permutations for a set containing 2 numbers", () => {
+    expect(permutations([1, 2]).sort()).toEqual(
+      [
+        [1, 2],
+        [2, 1],
+      ].sort(),
+    );
+  });
+
+  test.skip("6 possible permutations for a set containing 3 numbers", () => {
+    expect(permutations([1, 2, 3]).sort()).toEqual(
+      [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1],
+      ].sort(),
+    );
+  });
+
+  test.skip("24 possible permutations for a set containing 4 numbers", () => {
+    expect(permutations([1, 2, 3, 4]).sort()).toEqual(
+      [
+        [1, 2, 3, 4],
+        [1, 2, 4, 3],
+        [1, 3, 2, 4],
+        [1, 3, 4, 2],
+        [1, 4, 2, 3],
+        [1, 4, 3, 2],
+        [2, 1, 3, 4],
+        [2, 1, 4, 3],
+        [2, 3, 1, 4],
+        [2, 3, 4, 1],
+        [2, 4, 1, 3],
+        [2, 4, 3, 1],
+        [3, 1, 2, 4],
+        [3, 1, 4, 2],
+        [3, 2, 1, 4],
+        [3, 2, 4, 1],
+        [3, 4, 1, 2],
+        [3, 4, 2, 1],
+        [4, 1, 2, 3],
+        [4, 1, 3, 2],
+        [4, 2, 1, 3],
+        [4, 2, 3, 1],
+        [4, 3, 1, 2],
+        [4, 3, 2, 1],
+      ].sort(),
+    );
   });
 });
