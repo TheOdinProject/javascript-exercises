d64601a8cfbfeecc855c7a7185f44363e279b0e9
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-20 23:38:36 +0100

    fix(pascal): typo
    Co-authored-by: MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>

--- 19_pascal/solution/pascal-solution.js
+++ 19_pascal/solution/pascal-solution.js
@@ -20,7 +20,7 @@ const pascal = function (counter, currentLine = [1]) {
 
   // If a given line of the pascal's triangle has an even amount of elements, two things are true:
   // - the next line will have an odd amount of elements
-  // - the two elements in the middle will always be the same. So we are save to remove one
+  // - the two elements in the middle will always be the same. So it's safe to remove one
   if (currentLine.length % 2 === 0) {
     joined.splice(joined.length / 2, 1);
   }
