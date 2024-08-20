98242fb05b776bcc38a30603d874444dd2840878
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-20 23:41:23 +0100

    feat(hanoi): copy over tests from solution

--- 20_hanoi/hanoi.spec.js
+++ 20_hanoi/hanoi.spec.js
@@ -1,15 +1,65 @@
-const hanoi = require('./hanoi');
+const hanoi = require("./hanoi");
 
-describe('hanoi', () => {
-  test('First test description', () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
-
-    expect(hanoi()).toBe('');
+describe("hanoi", () => {
+  test("hanoi(1) should solve the puzzle in 1 move", () => {
+    const result = hanoi(1);
+    expect(result).toEqual(["Move disc 1 from tower 1 to tower 3"]);
   });
-  
-  test.skip('Second test description', () => {
+
+  test.skip("hanoi(2) should solve the puzzle in 3 moves", () => {
+    const result = hanoi(2);
+    expect(result).toEqual([
+      "Move disc 1 from tower 1 to tower 2",
+      "Move disc 2 from tower 1 to tower 3",
+      "Move disc 1 from tower 2 to tower 3",
+    ]);
+  });
+
+  test.skip("hanoi(3) should solve the puzzle in 7 moves", () => {
+    const result = hanoi(3);
+    expect(result).toEqual([
+      "Move disc 1 from tower 1 to tower 3",
+      "Move disc 2 from tower 1 to tower 2",
+      "Move disc 1 from tower 3 to tower 2",
+      "Move disc 3 from tower 1 to tower 3",
+      "Move disc 1 from tower 2 to tower 1",
+      "Move disc 2 from tower 2 to tower 3",
+      "Move disc 1 from tower 1 to tower 3",
+    ]);
+  });
+
+  test.skip("hanoi(4) should solve the puzzle in 15 moves", () => {
+    const result = hanoi(4);
+    expect(result).toEqual([
+      "Move disc 1 from tower 1 to tower 2",
+      "Move disc 2 from tower 1 to tower 3",
+      "Move disc 1 from tower 2 to tower 3",
+      "Move disc 3 from tower 1 to tower 2",
+      "Move disc 1 from tower 3 to tower 1",
+      "Move disc 2 from tower 3 to tower 2",
+      "Move disc 1 from tower 1 to tower 2",
+      "Move disc 4 from tower 1 to tower 3",
+      "Move disc 1 from tower 2 to tower 3",
+      "Move disc 2 from tower 2 to tower 1",
+      "Move disc 1 from tower 3 to tower 1",
+      "Move disc 3 from tower 2 to tower 3",
+      "Move disc 1 from tower 1 to tower 2",
+      "Move disc 2 from tower 1 to tower 3",
+      "Move disc 1 from tower 2 to tower 3",
+    ]);
+  });
+});
+
+describe("hanoi", () => {
+  test("First test description", () => {
     // Replace this comment with any other necessary code, and update the expect line as necessary
 
-    expect(hanoi()).toBe('');
+    expect(hanoi()).toBe("");
+  });
+
+  test.skip("Second test description", () => {
+    // Replace this comment with any other necessary code, and update the expect line as necessary
+
+    expect(hanoi()).toBe("");
   });
 });
