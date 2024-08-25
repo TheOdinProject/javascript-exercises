7b343a1eec005efeb1a19175e08781c6cffcaaf2
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-08-25 16:56:26 +0100

    feat: remove redundant test
    Co-authored-by: MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>

--- 20_hanoi/hanoi.spec.js
+++ 20_hanoi/hanoi.spec.js
@@ -50,16 +50,3 @@ describe("hanoi", () => {
   });
 });
 
-describe("hanoi", () => {
-  test("First test description", () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
-
-    expect(hanoi()).toBe("");
-  });
-
-  test.skip("Second test description", () => {
-    // Replace this comment with any other necessary code, and update the expect line as necessary
-
-    expect(hanoi()).toBe("");
-  });
-});
