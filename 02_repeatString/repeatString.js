b2f98a00a9bb0c3a6675d28eb68bcf29e5f719ee
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-01 11:48:40 -0400

    Completed scrip for test 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let result = ''
+    for (let i = 0;i<3;i++)
+    {
+        result += string
+    }
+    return result
 };
 
 // Do not edit below this line
