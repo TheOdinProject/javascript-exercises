1621ca51242fb4027db3a845bbe3f8c3c683467d
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-02 10:38:02 -0400

    Done exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    length=string.length
+    let array= string.split('')
+    let reversedArray=[]
+    for (let i=0;i<length;i++)
+    {
+        reversedArray[i]=array.pop()
+    }
+    reversedArray=reversedArray.join('')
+    return reversedArray
 };
 
 // Do not edit below this line
