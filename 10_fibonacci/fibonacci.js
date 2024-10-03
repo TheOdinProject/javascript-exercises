e2855c7497ac6f4872f8a8c3fa112777a7fc6a53
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-03 12:44:26 -0400

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,29 @@
-const fibonacci = function() {
-
+const fibonacci = function(amount) {
+    if(amount === 0 || amount === '0')
+    {
+        return 0
+    }
+    if(amount<0)
+    {
+        return 'OOPS'
+    }
+    let array = []
+    let length = amount
+    let currentNumber = 1
+    for(let i = 0;i< length;i++)
+    {
+        if(i>=2)
+        {
+            array[i] = array[i-2] + array[i-1]
+        }
+        else
+        {
+            array[i] = currentNumber
+        }
+    }
+    return array[length-1]
 };
 
+fibonacci("0")
 // Do not edit below this line
 module.exports = fibonacci;
