1eeca3dbcfae76157f60920af1e098ba00e305dd
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-03 09:56:10 -0400

    completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	let length = numbers.length
+  let result=0
+  for(let i = 0;i<length;i++)
+  {
+    result+=numbers[i]
+  }
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(...numbers) {
+  let length = numbers.length
+  let result=numbers[0]
+  for(let i = 1;i<length;i++)
+  {
+    result-=numbers[i]
+  }
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let length = numbers.length
+  let result=0
+  for(let i = 0;i<length;i++)
+  {
+    result+=numbers[i]
+  }
+  return parseInt(result)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let length = numbers.length
+  let result=numbers[0]
+  for(let i = 1;i<length;i++)
+  {
+    result*=numbers[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(...numbers) {
+  let length = numbers[1]
+  let result=numbers[0]
+  for(let i = 1;i<length;i++)
+  {
+    result*=numbers[0]
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let length = number
+  let result=1
+  for(let i = 1;i<=length;i++)
+  {
+    result*=i
+  }
+  return result
 };
 
 // Do not edit below this line
