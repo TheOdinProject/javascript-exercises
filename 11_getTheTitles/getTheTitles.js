e66f307f99dc683a3112cead5809727686cf21c7
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-03 13:15:23 -0400

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(library) {
+    let length = library.length
+    let titleArray = []
+    for(let i = 0;i< length; i++)
+    {
+        titleArray.push(library[i].title)
+    }
+    return titleArray
 };
-
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
