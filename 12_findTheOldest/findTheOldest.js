c21f24ba716ea1876ff97d4157477e60999bf7ce
Patrick Brochu <patrickbrochu70@gmail.com>
2024-10-04 10:50:38 -0400

    Done exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
 
+const findTheOldest = function(list) {
+    let length = list.length
+    let highestAge=0
+    let oldest;
+    for (person of list)
+    {
+        let age = person.yearOfDeath - person.yearOfBirth
+        if(age > highestAge && person.yearOfDeath != undefined)
+        {
+            highestAge = age
+            oldest = person
+        }
+    }
+    return oldest
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
