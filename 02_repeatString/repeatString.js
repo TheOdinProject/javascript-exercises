99d3a92de9dfeb82ef2d77acbf88494262888baf
Dan <101015168+danstagram@users.noreply.github.com>
2024-10-09 22:58:55 +0200

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,counter) {
+    if(counter < 0)return "ERROR";
+    let text = "";
+    for (let i=0; i < counter; i++){
+        text = text + string;
+    }
+    return text;
 };
 
 // Do not edit below this line
