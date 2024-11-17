40b89c0d0eccecf3505925b01d07759b1334fa62
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-16 21:30:13 -0800

    Test function to ensure numbers are added together

--- 02_troubleshooting/troubleshooting.spec.js
+++ 02_troubleshooting/troubleshooting.spec.js
@@ -1,1 +1,7 @@
 const troubleshooting = require('./troubleshooting')
+
+describe('troubleshooting', () => {
+  test('adds the numbers together', () => {
+    expect(troubleshooting()).toEqual(2);
+  });
+});
