c901a048c4dfff502fb89983669415fa0a07a5b7
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-18 22:00:24 -0800

    Test result for troubleshooting function

---
+++ 02_troubleshooting/solution/troubleshooting-solution.spec.js
@@ -0,0 +1,7 @@
+const troubleshooting = require('./troubleshooting-solution')
+
+describe('troubleshooting', () => {
+  test('adds the numbers together', () => {
+    expect(troubleshooting()).toEqual(2);
+  });
+});
