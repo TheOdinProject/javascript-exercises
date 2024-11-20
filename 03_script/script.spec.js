c7b256d7174ba5b53468d79dce4d3e5042c5e41e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-19 23:27:08 -0800

    Verify proper return value for numbers less than ten

--- 03_script/script.spec.js
+++ 03_script/script.spec.js
@@ -7,4 +7,10 @@ describe('numberChecker', () => {
   test('returns true when number is 10', () => {
     expect(numberChecker(10)).toEqual(true);
   });
+  test('returns false when number is 9', () => {
+    expect(numberChecker(9)).toEqual(false);
+  });
+  test('returns false when number is 6', () => {
+    expect(numberChecker(6)).toEqual(false);
+  });
 });
