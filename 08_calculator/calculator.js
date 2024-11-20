c960840fd52e86228d89bbe434e413745b079b48
lianfrances <leannefrances740@gmail.com>
2024-11-20 19:22:15 +0800

    finished exercise 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+// const { factorial } = require("./solution/calculator-solution");
+
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let sum = arr.reduce((sum, current) => sum + current, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let multiply = arr.reduce((accumulator, current) => accumulator * current, 1);
+  return multiply;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let value = Math.pow(a, b);
+  return value;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) return -1;
+  if (num === 0) return 1;
+  else return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
