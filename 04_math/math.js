fc02aad318ecc5c3b9fd6592b733904168fe144d
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-21 22:30:25 -0800

    Add math variables for the exercise

---
+++ 04_math/math.js
@@ -0,0 +1,8 @@
+const a = 1 - 1
+const b = "one plus eight"
+const c = "22 times three"
+const d = "the *remainder* of 5/4"
+const e = "the variable 'b' minus 17"
+const f = "the sum of the previous five variables"
+
+module.exports = {a, b, c, d, e, f}
