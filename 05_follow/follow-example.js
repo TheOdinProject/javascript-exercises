02ef9e77e8ea61a5fbd8e573bbb3458193b0707a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-22 00:02:58 -0800

    Add example problem

---
+++ 05_follow/follow-example.js
@@ -0,0 +1,16 @@
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+
+console.log(greeting);
+
+module.exports = {
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName
+}
