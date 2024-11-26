45e4d7114ba8bbe3f274ce5a860c4f80c72b7afe
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-26 00:53:13 -0800

    Add boilerplate code required to begin exercise

---
+++ 05_follow/follow.js
@@ -0,0 +1,25 @@
+/*
+ First name: Carlos
+ Last name: Stevenson
+ This year: 1965
+ Birth year: 1947
+
+ The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+*/
+
+// Add your code right below, good luck!
+
+
+
+
+
+// Do not change this
+module.exports = {
+  firstName: typeof firstName === 'undefined' ? null : firstName,
+  lastName: typeof lastName === 'undefined' ? null : lastName,
+  thisYear: typeof thisYear === 'undefined' ? null : thisYear,
+  birthYear: typeof birthYear === 'undefined' ? null : birthYear,
+  greeting: typeof greeting === 'undefined' ? null : greeting,
+  fullName: typeof fullName === 'undefined' ? null : fullName,
+  age: typeof age === 'undefined' ? null : age
+}
