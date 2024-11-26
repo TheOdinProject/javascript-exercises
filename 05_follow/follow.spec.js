f287c3ffbc3d1330ca867c139b4ca2a424d5da4e
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-26 00:53:59 -0800

    Add progressive tests for exercise

---
+++ 05_follow/follow.spec.js
@@ -0,0 +1,25 @@
+const values = require('./follow')
+
+describe('follow', () => {
+  test('firstName is Carlos', () => {
+    expect(values.firstName).toEqual('Carlos');
+  });
+  test.skip('lastName is Stevenson', () => {
+    expect(values.lastName).toEqual('Stevenson');
+  });
+  test.skip('thisYear is 1965', () => {
+    expect(values.thisYear).toEqual(1965);
+  });
+  test.skip('birthYear is 1947', () => {
+    expect(values.birthYear).toEqual(1947);
+  });
+  test.skip('greeting is properly output', () => {
+    expect(values.greeting).toEqual('Hello! My name is Carlos Stevenson and I am 18 years old.');
+  });
+  test.skip('fullName is Carlos Stevenson', () => {
+    expect(values.fullName).toEqual('Carlos Stevenson');
+  });
+  test.skip('age is 18', () => {
+    expect(values.age).toEqual(18);
+  });
+});
