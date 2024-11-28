e1c4523f4323606571ac307a88194c4a5dc6f722
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2024-11-27 22:09:21 -0800

    Add comment as reminder to not edit some of the code

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -5,4 +5,5 @@ const d = "the *remainder* of 5/4"
 const e = "the variable 'b' minus 17"
 const f = "the sum of the previous five variables"
 
+// Do not edit below this line
 module.exports = {a, b, c, d, e, f}
