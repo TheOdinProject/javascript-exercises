af787c61a37ee826a947cfd14822c607a1ba4441
utkarshahu <utkarshsahu180@gmail.com>
2025-01-08 23:42:17 +0530

    Update in helo World.js

--- 01_helloWorld/solution/helloWorld-solution.spec.js
+++ 01_helloWorld/solution/helloWorld-solution.spec.js
@@ -1,4 +1,4 @@
-const helloWorld = require('./helloWorld-solution');
+const helloWorld = require('./helloWorld-solution'); // Import the function
 
 describe('Hello World', function () {
   test('says "Hello, World!"', function () {
