529ef32ae9a6d818018b579a57430432a5eab2fe
larz <139354341+l-fakih@users.noreply.github.com>
2025-02-15 03:09:00 +0200

    return the desired output, where the string repeats based on the argument given

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+if (num < 0){
+    return 'ERROR';
+}
+let repeatedWord = "";  
+for (let i = 0; i < num; i++){
+    repeatedWord += string;
+}
+return repeatedWord;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
