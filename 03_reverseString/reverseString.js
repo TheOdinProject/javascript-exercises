699349239b7665158d853484f87062622fce2cbc
larz <139354341+l-fakih@users.noreply.github.com>
2025-02-15 03:56:15 +0200

    reverse the strings that are inputted

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let newString = "";
+    for (let i = stringArray.length - 1; i >= 0; i--){
+            newString += stringArray[i];
+    }
+    return newString;
 };
 
 // Do not edit below this line
