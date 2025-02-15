bd076a9725bc78fb82d0a961e79c8ced69934a14
larz <139354341+l-fakih@users.noreply.github.com>
2025-02-15 23:58:08 +0200

    Return true or false if the year inputed is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
-
-};
-
-// Do not edit below this line
-module.exports = leapYears;
+const leapYears = function(year) {
+    //Check if the year contains 4 digits:
+    const divisibleByFour = year % 4;
+    const divisibleByHundred = year % 100;
+    const divisibleByFourHundred = year % 400;
+    //If there is a remainder after dividing it by 4, it is not a leap year
+    if (divisibleByFour !== 0){
+        return false;
+    }
+    //If there is a remainder after dividing it by 100, it is a leap year
+    else if (divisibleByHundred !== 0){
+        return true;
+    }
+    //If the year ends in two zeros and there is a remainder after dividing by 400, it is not a leap year
+    else if (divisibleByFourHundred !== 0){
+        return false;
+    }
+    else {
+        return true;
+    }
+    }
+    // Do not edit below this line
+    module.exports = leapYears;
+    
