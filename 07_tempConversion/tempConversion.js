ee9ee6b5630b97a5830737d2ffae1bd07dd02103
larz <139354341+l-fakih@users.noreply.github.com>
2025-02-16 00:14:05 +0200

    Convert celsius to fahrenheit and fahrenheit to celsius using the correct code

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const toCelsius = (fahrenheit - 32) * 5/9;
+  return Math.round(toCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const toFahrenheit = (9/5) * celsius + 32;
+  return Math.round(toFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
