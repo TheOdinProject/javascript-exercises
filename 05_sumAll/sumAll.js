8c921d8a2e9cabd90c04d43b179c5179342ce18e
larz <139354341+l-fakih@users.noreply.github.com>
2025-02-15 21:53:37 +0200

    sum all the integers passed as arguments accordingly

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
-};
+const sumAll = function(firstInteger, secondInteger) {
+    //Create a variable for the sum
+    let sum = 0;
+    //Check if the arguments passed are not float integers, negative, strings
+    //or other objects and shouldnt be in an array
+    if ((!Number.isInteger(firstInteger)) || (!Number.isInteger(secondInteger)) || (firstInteger < 0) || (secondInteger < 0)){
+        return 'ERROR';
+   }
+   //Check if the first argument is less than the second so we can add the sum accordingly
+    else if (firstInteger < secondInteger){
+        for (let i = firstInteger; i <= secondInteger; ++i){
+            sum += i;
+        }
+    }
+    //Check if the second argument is less than the first so we can add the sum accordingly
+    else if (secondInteger < firstInteger){
+        for (let i = secondInteger; i <= firstInteger; ++i){
+            sum += i;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
