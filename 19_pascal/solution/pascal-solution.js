b08da1eace6e45bcc4f0913ace2a16ad155186b0
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-02-16 17:35:28 +0000

    feat: make solution simpler (credit to @JoshDevHub)

--- 19_pascal/solution/pascal-solution.js
+++ 19_pascal/solution/pascal-solution.js
@@ -1,32 +1,17 @@
-const pascal = function (counter, currentLine = [1]) {
+const pascal = function (counter) {
+  const currentLine = [1];
   if (counter === 1) {
     return currentLine;
   }
 
-  const halfOfNextLine = currentLine.reduce(
-    (numbers, currentNumber, index) => {
-      const nextNumber = currentLine[index + 1];
-      if (currentNumber <= nextNumber) {
-        return [...numbers, currentNumber + nextNumber];
-      }
-      return numbers;
-    },
-    [1],
-  );
+  const previousLine = pascal(counter - 1);
+  previousLine.forEach((number, i) => {
+    const rightNeighbor = previousLine[i + 1] ?? 0;
+    currentLine.push(number + rightNeighbor);
+  })
 
-  // Notice that pascal triangle's lines are always palindromic in nature.
-  // We only need the first half to obtain the information to construct the second half
-  const joined = [...halfOfNextLine, ...halfOfNextLine.reverse()];
-
-  // If a given line of the pascal's triangle has an even amount of elements, two things are true:
-  // - the next line will have an odd amount of elements
-  // - the two elements in the middle will always be the same. So it's safe to remove one
-  if (currentLine.length % 2 === 0) {
-    joined.splice(joined.length / 2, 1);
-  }
-
-  return pascal(counter - 1, joined);
-};
+  return currentLine;
+}
 
 // Do not edit below this line
 module.exports = pascal;
