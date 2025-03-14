95662d8d1616fbf065a40b5b01f0bafae6056ad1
cath-ly <takath339@gmail.com>
2025-03-13 20:14:09 -0400

    Reverse String exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (strInput) {
+  const len = strInput.length;
+  let word = "";
+  for (let i = 0; i < len; i++) {
+    word = word + strInput[len - i - 1];
+  }
+  return word;
 };
 
 // Do not edit below this line
