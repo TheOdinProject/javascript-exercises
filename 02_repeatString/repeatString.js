3a557ba11bc0f0487b023beea50a5e631dd36e85
cath-ly <takath339@gmail.com>
2025-03-13 20:13:42 -0400

    Repeat String exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let newStr = "";
+  for (i = 0; i < num; i++) {
+    newStr = newStr + str;
+  }
+  return newStr;
 };
 
 // Do not edit below this line
