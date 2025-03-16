ffe353df15ff1aeba79a55ecfc2368f9172abb9b
cath-ly <takath339@gmail.com>
2025-03-15 20:53:11 -0400

    Leap Year exercise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 100 === 0 && year % 400 !== 0) {
+    return false;
+  }
+  if (year % 4 !== 0) {
+    return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
