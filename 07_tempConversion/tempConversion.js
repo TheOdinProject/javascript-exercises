09068ebc76aae0fee8c0774a8fe74ed747d0b9d8
cath-ly <takath339@gmail.com>
2025-03-15 22:51:45 -0400

    Fundamental Array and Loop Chapter Complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degreeF) {
+  const num = ((degreeF - 32) * 5) / 9;
+  return Number(num.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreeC) {
+  const num = (degreeC * 9) / 5 + 32;
+  return Number(num.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
