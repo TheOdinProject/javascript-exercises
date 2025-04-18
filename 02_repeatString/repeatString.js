9e5b99de7e2ba28af533b177f110a13a279755c1
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-18 11:52:20 -0500

    Implement multiple code changes to enhance functionality and improve performance

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return 'ERROR';
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+    return result;
 
 };
 
