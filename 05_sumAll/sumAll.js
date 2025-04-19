773dbf762ea02a254f02a6ebacfd96bdcc77bf49
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 16:28:50 -0500

    Refactor sumAll function for improved readability and update tests to ensure all cases are covered

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function(firstNum, secondNum) {
-
+const sumAll = (a, b) => {
+    if (
+        typeof a !== "number" || a % 1 !== 0 || a < 0 ||
+        typeof b !== "number" || b % 1 !== 0 || b < 0
+      ) {
+        return "ERROR"
+       } else {
+    let sum = 0
+    for (let i = Math.min(a, b); i<= Math.max(a, b); i +=1) {
+        sum += i;
+    }
+    return sum;
+}
 };
 
 // Do not edit below this line
