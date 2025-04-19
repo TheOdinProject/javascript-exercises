9cb95b92ec77088f3714e4de47cac0e749e673f0
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 12:33:37 -0500

    Remove unused implementation from removeFromArray function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,5 @@
 const repeatString = function(str, num) {
-    if (num < 0) return 'ERROR';
-    let result = '';
-    for (let i = 0; i < num; i++) {
-        result += str;
-    }
-    return result;
+
     
 };
 
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,5 @@
 const removeFromArray = function (arr, num) {
-    var i = 0;
-    while (i < arr.length) {
-        if (arr[i] === num) {
-            arr.splice(i, 1);
-        } else {
-            ++i;
-        }
-    }
-    return arr;
-
+    
 };
 
 // Do not edit below this line
