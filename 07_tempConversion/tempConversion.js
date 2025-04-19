04afa651df9813b21edd9ae586bf1981e0a86870
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 16:55:05 -0500

    Refactor temperature conversion functions to accept parameters and update tests for improved coverage

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(a) {
+  let b = ((a - 32) * 5/9)
+  let bString = b.toFixed(1)
+  let bRounded = +bString
+  return bRounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(a) {
+  let b = ((a * 9/5) + 32)
+  let bString = b.toFixed(1)
+  let bRounded = +bString
+  return bRounded
 };
 
 // Do not edit below this line
