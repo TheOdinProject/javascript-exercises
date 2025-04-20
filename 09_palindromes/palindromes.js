27e04891794ab5b3881992a627682aa8d0d8f72d
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 20:30:00 -0500

    Refactor palindromes function to accept a string parameter and update tests for improved coverage

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
