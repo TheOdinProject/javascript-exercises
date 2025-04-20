5cfec7d30e2cbef86ebb0e86cb84e8b4ec774ed1
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 20:14:09 -0500

    Refactor calculator functions to accept parameters and update tests for improved coverage

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return ( a - b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return ( a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res = 1;
+    for (let i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res
 };
 
 // Do not edit below this line
