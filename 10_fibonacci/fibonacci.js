46d2a2146a5d79e800ac805f62bd3f7440d032f7
Joshua Matte <joshua@bloomfieldhomes.net>
2025-04-19 20:41:29 -0500

    Refactor fibonacci function to accept a parameter and update tests for improved coverage

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(f) {
+    let n = Number(f)
+    if (n < 0) {
+        return "OOPS"
+    } else if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
