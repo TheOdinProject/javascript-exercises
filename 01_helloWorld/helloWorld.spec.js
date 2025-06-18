e2374793b57214c7ed64c5ebb9846abce47e87f0
CalanMoyTeneo <calan@weblite.com.my>
2025-06-18 18:18:11 +0800

    Add test

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,15 +1,14 @@
-//imports code from another file 
-const helloWorld = require('./helloWorld');
+//imports code from another file
+const helloWorld = require("./helloWorld");
 
-describe(
-    //body of the test 
+describe(//body of the test
 
-    'Hello World', function() {
-    test('says "Hello, World!"' //test description
-      , function() {
-      expect(helloWorld()).toEqual('Hello, World 123!');
-    });
-  }
+"Hello World", function () {
+  test('says "Hello, World!"', function () { //test description
+    expect(helloWorld()).toEqual("Hello, World 123!");
+  });
 
-
-);
+  test('does not say meow "', function () { //test description
+    expect(helloWorld()).not().toEqual("meow");
+  });
+});
