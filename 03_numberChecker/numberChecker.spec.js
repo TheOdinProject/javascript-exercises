2df633c27c49f0955db7ac81d112a3f241409117
Jay <jaygiri.gosai@yahoo.com>
2025-07-15 18:06:42 +0530

    Correct the comparison operator

--- 03_numberChecker/numberChecker.spec.js
+++ 03_numberChecker/numberChecker.spec.js
@@ -1,16 +1,16 @@
-const numberChecker = require('./numberChecker')
+const numberChecker = require("./numberChecker");
 
-describe('numberChecker', () => {
-  test('returns true when number is 1000', () => {
+describe("numberChecker", () => {
+  test("returns true when number is 1000", () => {
     expect(numberChecker(1000)).toEqual(true);
   });
-  test.skip('returns true when number is 10', () => {
+  test("returns true when number is 10", () => {
     expect(numberChecker(10)).toEqual(true);
   });
-  test.skip('returns false when number is 9', () => {
+  test("returns false when number is 9", () => {
     expect(numberChecker(9)).toEqual(false);
   });
-  test.skip('returns false when number is 6', () => {
+  test("returns false when number is 6", () => {
     expect(numberChecker(6)).toEqual(false);
   });
 });
