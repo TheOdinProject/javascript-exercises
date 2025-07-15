d70308d78029c7c1fb86647f74efac047d660804
Jay <jaygiri.gosai@yahoo.com>
2025-07-15 18:47:58 +0530

    Loop excercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+      result += string;
+    }
+    return result;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
