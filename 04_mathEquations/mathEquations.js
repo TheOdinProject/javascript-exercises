275c1e72ccb484127236fc95f3deed37beea92b1
Jay <jaygiri.gosai@yahoo.com>
2025-07-15 18:15:07 +0530

    Arithmetic operations

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
-const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
-const b = "one plus eight"
-const c = "22 times three"
-const d = "the *remainder* of 5/4"
-const e = "the variable 'b' minus 17"
-const f = "the sum of the previous five variables"
+const a = 1 - 1; // Freebie!!! This is the answer to "one minus one"
+const b = 1 + 8;
+const c = 22 * 3;
+const d = 5 % 4;
+const e = b - 17;
+const f = a + b + c + d + e;
 
 // Do not edit below this line
-module.exports = {a, b, c, d, e, f}
+module.exports = { a, b, c, d, e, f };
--- 04_mathEquations/mathEquations.spec.js
+++ 04_mathEquations/mathEquations.spec.js
@@ -1,22 +1,22 @@
-const {a, b, c, d, e, f} = require('./mathEquations')
+const { a, b, c, d, e, f } = require("./mathEquations");
 
-describe('math', () => {
-  test('a is 0', () => {
+describe("math", () => {
+  test("a is 0", () => {
     expect(a).toEqual(0);
   });
-  test.skip('b is 9', () => {
+  test("b is 9", () => {
     expect(b).toEqual(9);
   });
-  test.skip('c is 66', () => {
+  test("c is 66", () => {
     expect(c).toEqual(66);
   });
-  test.skip('d is 1', () => {
+  test("d is 1", () => {
     expect(d).toEqual(1);
   });
-  test.skip('e is -8', () => {
+  test("e is -8", () => {
     expect(e).toEqual(-8);
   });
-  test.skip('f is 68', () => {
+  test("f is 68", () => {
     expect(f).toEqual(68);
   });
 });
