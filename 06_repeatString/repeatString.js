6c18d0206eebea9ef0f3f5e8831b1018d18843a2
isi-code <irvinjose.silva@gmail.com>
2025-07-16 15:48:54 -0700

    Complete function that returns words in reverse order

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
+
+    if (num < 0) { 
+        return "ERROR"; 
+    }
+
+    for (let i = 0; i < num; i++){
+        repeatedString += string;
+    }
+
+    return repeatedString
 
 };
 
