b522baa99e020ab419a8e684afdce6a4d8467755
isi-code <irvinjose.silva@gmail.com>
2025-07-16 15:50:25 -0700

    Complete function that returns words in reverse order
    Past commit, I completed a function that repeats a word specified number of times

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+  let reversedWord = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedWord += string[i];
+  }
+  return reversedWord;
+    
 };
 
 // Do not edit below this line
