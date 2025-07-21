b643f8d39f6b1aa83452fe84600f0535c1cebc25
programathor10 <brunojobs@proton.me>
2025-07-21 08:54:27 -0300

    ejercicio 6 y 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+function repeatString(string, num) {
+  if (num < 0) return 'ERROR';
+  if (num === 0) return '';
+  let repeatedString = '';
+  for (let i = 0; i < num; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
