ef09469bd3ed39f44712f81b6ad8a38f78126fad
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2025-07-20 22:18:06 -0700

    practice 01-05 js

--- 05_joinStrings/joinStrings.spec.js
+++ 05_joinStrings/joinStrings.spec.js
@@ -4,25 +4,25 @@ describe('step 2', () => {
   test('firstName is Carlos', () => {
     expect(values.firstName).toEqual('Carlos');
   });
-  test.skip('lastName is Stevenson', () => {
+  test('lastName is Stevenson', () => {
     expect(values.lastName).toEqual('Stevenson');
   });
-  test.skip('thisYear is 1965', () => {
+  test('thisYear is 1965', () => {
     expect(values.thisYear).toEqual(1965);
   });
-  test.skip('birthYear is 1947', () => {
+  test('birthYear is 1947', () => {
     expect(values.birthYear).toEqual(1947);
   });
-  test.skip('greeting is properly output', () => {
+  test('greeting is properly output', () => {
     expect(values.greeting).toEqual('Hello! My name is Carlos Stevenson and I am 18 years old.');
   });
 });
 
 describe('step 3', () => {
-  test.skip('fullName is Carlos Stevenson', () => {
+  test('fullName is Carlos Stevenson', () => {
     expect(values.fullName).toEqual('Carlos Stevenson');
   });
-  test.skip('age is 18', () => {
+  test('age is 18', () => {
     expect(values.age).toEqual(18);
   });
 });
