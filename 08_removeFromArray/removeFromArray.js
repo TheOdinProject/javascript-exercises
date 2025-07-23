d328ec9c0619643558dc82f1227694663e21c475
isi-code <irvinjose.silva@gmail.com>
2025-07-23 16:06:37 -0700

    Remove an element from an array
    I added a line of code that returns a new array that removes any element that matches the item to remove. To complete this exercise, I need to make sure the function removes any additional parameter added after the two required ones.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeThing, ...moreThings) {
+
+    let newArray = array.filter( element => element !== removeThing)
+
+    return newArray
+
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.spec.js
+++ 08_removeFromArray/removeFromArray.spec.js
@@ -7,10 +7,10 @@ describe('removeFromArray', () => {
   test.skip('removes multiple values', () => {
     expect(removeFromArray([1, 2, 3, 4], 3, 2)).toEqual([1, 4]);
   });
-  test.skip('removes multiple of the same value', () => {
+  test('removes multiple of the same value', () => {
     expect(removeFromArray([1, 2, 2, 3], 2)).toEqual([1, 3]);
   });
-  test.skip('ignores non present values', () => {
+  test('ignores non present values', () => {
     expect(removeFromArray([1, 2, 3, 4], 7, "tacos")).toEqual([1, 2, 3, 4]);
   });
   test.skip('ignores non present values, but still works', () => {
