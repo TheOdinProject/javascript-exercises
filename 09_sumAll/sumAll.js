b4793ddaafbbd93befa7ba901aa8d93d0b78de63
isi-code <irvinjose.silva@gmail.com>
2025-07-23 16:11:05 -0700

    sum all numbers within a range
    As of now, my code sums everything within a range of numbers. I still need to add validation to the parameters  like not accepting float numbers, negative numbers, or non-numeric values.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
+
+    let result = 0;
+
+    if (num < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    if(num < num2){
+        for(let n = num; n <= num2; n++){
+            result += n;
+        }
+        return result;
+    }
+
+    else if (num > num2){
+        for(let n = num; n >= num2; n--){
+            result += n;
+        }
+        return result;
+    }
 
 };
 
