7e5f774543ddc05e374afe05b048e194bee88f26
isi-code <irvinjose.silva@gmail.com>
2025-07-25 12:21:20 -0700

    Simplify approach using the filter method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,25 +1,8 @@
 const removeFromArray = function(array, ...remove) {
 
-    for (r of remove){
-        
-        if (array.includes(r)) {
+    let newArray = array.filter( element => !remove.includes(element))
 
-            let timesRemove = 0;
-        
-            array.forEach( element => {
-                if (element === r) 
-                    timesRemove += 1
-            });
-    
-            let indexToRemove =  array.indexOf(r)
-            
-            array.splice(indexToRemove, timesRemove)
-            
-        }
-        
-    }
-    return array
-
+    return newArray
 
 };
 
