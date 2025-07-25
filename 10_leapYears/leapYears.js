82e95ffbf7a0bfaf99a7f562f269a884b6026906
isi-code <irvinjose.silva@gmail.com>
2025-07-25 16:18:52 -0700

    Create a function that identifies leap years based on several rules

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (year % 4 === 0 && year % 100 !== 0){
+        return true
+    }
+    if ( year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true
+        }
+        else {
+             return false
+            }
+    }
+    else {
+        return false
+    }
 
 };
 
