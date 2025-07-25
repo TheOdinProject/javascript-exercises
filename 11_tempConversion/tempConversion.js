ae3f5ccb36292cff85b4de2a469324d69632f5d7
isi-code <irvinjose.silva@gmail.com>
2025-07-25 16:21:11 -0700

    Create two functions that converts temperature to Farenheit or Celsius degrees

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let formula = (temperature - 32) / (9 / 5)
+  let result =  parseFloat(formula.toFixed(1))
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let formula = (temperature * 9 / 5) + 32
+  let result =  parseFloat(formula.toFixed(1))
+  return result
 };
 
 // Do not edit below this line
