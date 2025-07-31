63459bb0e61671905d5f66aac4d2b56e94968ac2
Mayank Naruka <mayanknaruka47@gmail.com>
2025-07-31 21:55:17 +0530

    Update joinStrings.js

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -8,10 +8,17 @@
 */
 
 // Add your code right below, good luck!
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+// At first, greeting may look like this:
+// const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
+const fullName = firstName + " " + lastName 
+const age = thisYear - birthYear
 
-
-
+const greeting = "Hello My name is"  + fullName + " and I am" + age + "years old"  
 
 // Do not change this
 module.exports = {
