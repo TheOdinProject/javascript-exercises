457da7d46497f3571481a38384729810d6f2ad86
Matthew <12345689+05schmidtm18@gmail.com.github.com>
2025-07-31 21:57:07 -0400

    adding exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function () {
+    const [str, num] = arguments;
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += str;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
