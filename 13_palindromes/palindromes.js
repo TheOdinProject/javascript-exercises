42d0e9a93e0c61a5ebf68a188f1ed27e9746bf27
isi-code <irvinjose.silva@gmail.com>
2025-08-04 16:27:06 -0700

    Complete function that identifies palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let cleanWord = word.toLowerCase();
+    cleanWord = cleanWord.replace(/[^a-zA-Z0-9]/g, "");
+    
+    let letters = cleanWord.split("");
+    letters.reverse();
 
+    if (cleanWord === letters.join("")) return true
+
+    return false
 };
 
 // Do not edit below this line
