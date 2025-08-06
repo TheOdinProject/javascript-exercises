a8a6de02b878e48fb1b39bb1592d6b6437d42b17
isi-code <irvinjose.silva@gmail.com>
2025-08-06 15:00:53 -0700

    Alternative solution for finding the oldest person

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const currentYear = new Date().getFullYear;
+
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const currentAge = (currentPerson.yearOfDeath || currentYear) - currentPerson.yearOfBirth;
+        
+    return oldestAge < currentAge ? currentPerson : oldest
+    });
 };
 
 // Do not edit below this line
