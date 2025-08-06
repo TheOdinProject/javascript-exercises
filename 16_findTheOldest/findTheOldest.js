a073d8017b5ebb5ce2e494b5963e0c195e171845
Jay <jaygiri.gosai@yahoo.com>
2025-08-06 18:47:56 +0530

    Find oldest people

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  for (member of people) {
+    if ("yearOfDeath" in member)
+      member.age = member.yearOfDeath - member.yearOfBirth;
+    else member.age = new Date().getFullYear() - member.yearOfBirth;
+  }
+  people.sort((a, b) => b.age - a.age);
+  return people[0];
 };
 
 // Do not edit below this line
