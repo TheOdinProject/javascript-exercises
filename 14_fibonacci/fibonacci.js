dc519647ce8c7ba8b8dcfc1a98d5beb27641d94f
Jay <jaygiri.gosai@yahoo.com>
2025-08-06 18:15:29 +0530

    Implement Fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = parseInt(num);
+  if (num < 0) return "OOPS";
+  let a = 0,
+    b = 1;
+  for (let i = 0; i <= num; i++) {
+    if (i === num) return a; // print current Fibonacci number
+    let next = a + b;
+    a = b;
+    b = next;
+  }
 };
 
 // Do not edit below this line
