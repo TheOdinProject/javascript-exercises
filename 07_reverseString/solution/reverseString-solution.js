c52633d50616732ad35d2858623f8a4311f20eab
Sajwanhub <surisajwan1998@gmail.com>
2025-09-06 00:15:18 +0530

    reverseString: Make hint more generic  (#578)
    Provide alternative solution that also draws from the Loops and Arrays lesson.

--- 07_reverseString/solution/reverseString-solution.js
+++ 07_reverseString/solution/reverseString-solution.js
@@ -3,3 +3,17 @@ const reverseString = function (string) {
 };
 
 module.exports = reverseString;
+
+
+
+// Another approach could be to loop over the string in reverse direction to construct a new one
+/*
+  const reverseString = function (string) {
+    let reversedString = "";
+
+    for (let i = string.length -1; i >= 0; i--) {
+      reversedString += string[i];
+    }
+    return reversedString;
+  };
+*/ 
