a12a8b8dba24fb8b2d97a9f53be6c54f69c67bd6
vivekmaru36 <vivekmaru036@gmail.com>
2025-10-26 16:51:22 +0530

    05 join strings done

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,8 +9,14 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName ="Stevenson";
+const thisYear = 1965;
+const birthYear=1947;
 
+let greeting = "Hello! My name is "+firstName+" " +lastName+" and I am "+(thisYear-birthYear) +" years old."
 
+console.log(greeting);
 
 
 // Do not change this
