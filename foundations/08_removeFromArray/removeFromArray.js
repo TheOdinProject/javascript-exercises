f3388ac13ccd6285cfb5505a7928c02ca1c0d8eb
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2026-06-12 21:11:31 +0300

    Finish the '08_removeFromArray' exercise.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+// Advanced way to remove items from an array. The function takes an array as the first argument, followed by one or more additional arguments. The function should return a new array with the items removed.
+// const removeFromArray = function (array, ...args) {
+//   return array.filter((item) => !args.includes(item));
+// };
+
+// Easier way to remove items from an array. The function takes an array as the first argument, followed by one or more additional arguments. The function should return a new array with the items removed.
+const removeFromArray = function (array, ...args) {
+  const newArray = new Array();
+
+  array.forEach((item) => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    }
+  });
+
+  return newArray;
 };
 
 // Do not edit below this line
