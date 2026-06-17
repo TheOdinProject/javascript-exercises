1ae58c9779296cbe17bc82b75b65358a2553cea9
taha-mid <255703685+taha-mid@users.noreply.github.com>
2026-06-17 22:13:46 +0600

    solved the first five questions

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,17 +1,11 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+const firstName= "Carlos"
+const lastName= "Stevenson"
+const thisYear= 1965
+const birthYear= 1947
+const fullName = firstName + " " + lastName
+const age = thisYear- birthYear
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
-
-// Add your code right below, good luck!
-
-
-
-
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old."
 
 // Do not change this
 module.exports = {
