1689c212e7bcb942b2310b9101eed31ec8ee50ed
HMarsh1409 <hmarsh1409@gmail.com>
2026-09-06 12:14:08 +0100

    complete leapYears exercise

--- foundations/10_leapYears/leapYears.spec.js
+++ foundations/10_leapYears/leapYears.spec.js
@@ -1,22 +1,22 @@
-const leapYears = require('./leapYears')
+const leapYears = require("./leapYears");
 
-describe('leapYears', () => {
-  test('works with non century years', () => {
-    expect(leapYears(1996)).toBe(true);
-  });
-  test.skip('works with non century years', () => {
-    expect(leapYears(1997)).toBe(false);
-  });
-  test.skip('works with ridiculously futuristic non century years', () => {
-    expect(leapYears(34992)).toBe(true);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(1900)).toBe(false);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(1600)).toBe(true);
-  });
-  test.skip('works with century years', () => {
-    expect(leapYears(700)).toBe(false);
-  });
+describe("leapYears", () => {
+    test("works with non century years", () => {
+        expect(leapYears(1996)).toBe(true);
+    });
+    test("works with non century years", () => {
+        expect(leapYears(1997)).toBe(false);
+    });
+    test("works with ridiculously futuristic non century years", () => {
+        expect(leapYears(34992)).toBe(true);
+    });
+    test("works with century years", () => {
+        expect(leapYears(1900)).toBe(false);
+    });
+    test("works with century years", () => {
+        expect(leapYears(1600)).toBe(true);
+    });
+    test("works with century years", () => {
+        expect(leapYears(700)).toBe(false);
+    });
 });
