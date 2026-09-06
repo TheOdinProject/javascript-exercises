eef4b95c1ba221e7f2c71d2005c02468cf1975e5
HMarsh1409 <hmarsh1409@gmail.com>
2026-09-06 11:39:50 +0100

    complete repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if (times < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < times; i++) {
+        string += word;
+    }
+    return string;
 };
 
 // Do not edit below this line
