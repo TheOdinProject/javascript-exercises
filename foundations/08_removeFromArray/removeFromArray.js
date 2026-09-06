535210a8cf11da5198b493490111a418d30a9294
HMarsh1409 <hmarsh1409@gmail.com>
2026-09-06 11:56:13 +0100

    complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    const newArray = [];
+
+    arr.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
