831442574bc0cf0ae7f6bf418223135c0417aee9
Manu <mradul.krishna11@gmail.com>
2026-09-13 23:36:09 +0530

    Solve factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,9 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  if (!Number.isInteger(n) || n < 0) return;
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
 };
+
 
 // Do not edit below this line
 module.exports = factorial;
